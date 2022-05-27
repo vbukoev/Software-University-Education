34b4fe42b565a27bf3cea6c27ea937ceb72aa767
Vasil Bukoev <vbukoev@gmail.com>
2022-05-27 22:25:16 +0300

    Added new folder and the more exercise which i've done

---
+++ CSharp-Technology-FUNDAMENTALS/MORE EXERCISES FUNDAMENTALS/Data Types and Variables - More Exercise/MoreExercisesDataTypeFinder/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MoreExercisesDataTypeFinder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int valueInt = 0;
+            float valueFloat = 0.0f;
+            char valueChar;
+            bool valueBool;
+
+            while (input != "END")
+            {
+                if (int.TryParse(input, out valueInt))
+                {
+                    Console.WriteLine($"{input} is integer type");
+                }
+                else if (float.TryParse(input, out valueFloat))
+                {
+                    Console.WriteLine($"{input} is floating point type");
+                }
+                else if (char.TryParse(input, out valueChar))
+                {
+                    Console.WriteLine($"{input} is character type");
+                }
+                else if (bool.TryParse(input, out valueBool))
+                {
+                    Console.WriteLine($"{input} is boolean type");
+                }
+                else
+                {
+                    Console.WriteLine($"{input} is string type");
+                }
+                input = Console.ReadLine();
+            }
+
+        }
+    }
+}
