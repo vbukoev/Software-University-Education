4071e072589f910abece7530aeee8a7a51b6ebfb
Vasil Bukoev <vbukoev@gmail.com>
2022-06-01 21:33:51 +0300

    Update Program.cs

--- CSharp-Technology-FUNDAMENTALS/Arrays - Lab/Arrays - Lab/06. Even and Odd Subtraction/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/Arrays - Lab/Arrays - Lab/06. Even and Odd Subtraction/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _06._Even_and_Odd_Subtraction
 {
@@ -6,7 +7,16 @@ namespace _06._Even_and_Odd_Subtraction
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int evenSum = 0;
+            int oddSum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+            }
         }
     }
 }
