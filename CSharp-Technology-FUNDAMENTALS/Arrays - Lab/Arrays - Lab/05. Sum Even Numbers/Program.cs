15059304100f5541af93009863a2c7b434c74ef6
Vasil Bukoev <vbukoev@gmail.com>
2022-06-01 21:33:23 +0300

    Update Program.cs

--- CSharp-Technology-FUNDAMENTALS/Arrays - Lab/Arrays - Lab/05. Sum Even Numbers/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/Arrays - Lab/Arrays - Lab/05. Sum Even Numbers/Program.cs
@@ -7,19 +7,31 @@ namespace _05._Sum_Even_Numbers
     {
         static void Main(string[] args)
         {
-           int[] numbers = Console.ReadLine().Split()
-                .Select(int.Parse)
-                .ToArray();
+            int[] number = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
             int sum = 0;
-            for (int i = 0; i < numbers.Length; i++)
+            foreach (var item in number)
             {
-                int currNum = numbers[i];
-                if(currNum %2 == 0)
+                if (item % 2 == 0)
                 {
-                    sum += currNum;
+                    sum+=item;
                 }
             }
             Console.WriteLine(sum);
+
+            //int[] numbers = Console.ReadLine().Split()
+            //     .Select(int.Parse)
+            //     .ToArray();
+            // int sum = 0;
+            // for (int i = 0; i < numbers.Length; i++)
+            // {
+            //     int currNum = numbers[i];
+            //     if(currNum %2 == 0)
+            //     {
+            //         sum += currNum;
+            //     }
+            // }
+            // Console.WriteLine(sum);
         }
     }
 }
