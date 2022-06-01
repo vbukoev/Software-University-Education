39b7b8a09bc5ddf799d44bc159ffe3972e1195fa
Vasil Bukoev <vbukoev@gmail.com>
2022-06-01 20:45:14 +0300

    Update Program.cs

--- CSharp-Technology-FUNDAMENTALS/Arrays - Lab/Arrays - Lab/03. Rounding Numbers/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/Arrays - Lab/Arrays - Lab/03. Rounding Numbers/Program.cs
@@ -6,7 +6,18 @@ namespace _03._Rounding_Numbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] input = Console.ReadLine().Split(' ');
+            double[] numbers = new double[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                numbers[i] = double.Parse(input[i]);
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"{numbers[i]} => {Math.Round(numbers[i], MidpointRounding.AwayFromZero)}");
+            }
         }
     }
 }
