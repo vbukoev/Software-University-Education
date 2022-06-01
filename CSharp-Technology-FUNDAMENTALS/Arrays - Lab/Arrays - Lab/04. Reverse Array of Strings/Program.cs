bf21299d46789fb2c5ce71c5b13ff2bc003c1cb8
Vasil Bukoev <vbukoev@gmail.com>
2022-06-01 21:07:51 +0300

    Update program.cs

--- CSharp-Technology-FUNDAMENTALS/Arrays - Lab/Arrays - Lab/04. Reverse Array of Strings/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/Arrays - Lab/Arrays - Lab/04. Reverse Array of Strings/Program.cs
@@ -6,7 +6,20 @@ namespace _04._Reverse_Array_of_Strings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] elements = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < elements.Length / 2; i++)
+            {
+                string frontElement = elements[i];
+                string backElement = elements[(elements.Length - 1)- i];
+
+                string tempElement = frontElement;
+
+                elements[i] = backElement;
+                elements[(elements.Length - 1)- i] = frontElement;
+            }
+
+            Console.WriteLine(string.Join(" ", elements));
         }
     }
 }
