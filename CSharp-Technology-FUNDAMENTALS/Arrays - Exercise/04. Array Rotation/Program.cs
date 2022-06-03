9fe5a38540b8e56d49322f5d68a1058b5a89ee92
Vasil Bukoev <vbukoev@gmail.com>
2022-06-03 19:29:10 +0300

    Update Program.cs

--- CSharp-Technology-FUNDAMENTALS/Arrays - Exercise/04. Array Rotation/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/Arrays - Exercise/04. Array Rotation/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _04._Array_Rotation
 {
@@ -6,7 +7,19 @@ namespace _04._Array_Rotation
     {
         static void Main(string[] args)
         {
-            
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int rotations = int.Parse(Console.ReadLine());
+
+            for (int rotation = 0; rotation < rotations; rotation++)
+            {
+                int tempElement = array[0];
+                for (int operations = 0; operations < array.Length-1; operations++)
+                {
+                    array[operations] = array[operations + 1];
+                }
+                array[array.Length - 1] = tempElement;
+            }
+            Console.WriteLine(string.Join(" ", array));
         }
     }
 }
