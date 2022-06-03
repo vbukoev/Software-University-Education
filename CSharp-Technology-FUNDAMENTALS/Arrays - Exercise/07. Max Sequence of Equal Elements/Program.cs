c730da5ede319dbb2b2c3f3db583d1b9544aa556
Vasil Bukoev <vbukoev@gmail.com>
2022-06-03 23:29:08 +0300

    Update Program.cs

--- CSharp-Technology-FUNDAMENTALS/Arrays - Exercise/07. Max Sequence of Equal Elements/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/Arrays - Exercise/07. Max Sequence of Equal Elements/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _07._Max_Sequence_of_Equal_Elements
 {
@@ -6,7 +7,31 @@ namespace _07._Max_Sequence_of_Equal_Elements
     {
         static void Main(string[] args)
         {
-            
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int start = 0;
+            int count = 0;
+            int max = 0;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] == array[i+1])
+                {
+                    count++;
+                    if (count > max)
+                    {
+                        start = i - count;
+                        max = count;
+                    }                    
+                }
+                else
+                {
+                    count = 0; 
+                }
+            }
+            for (int i = start + 1; i <= start + max + 1; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
