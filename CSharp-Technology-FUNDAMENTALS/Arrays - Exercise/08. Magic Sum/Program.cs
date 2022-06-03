aeb9a4eae710d1ffc37410cbf6484478affe8545
Vasil Bukoev <vbukoev@gmail.com>
2022-06-03 19:05:24 +0300

    added tasks from ex 03-06-2022 fund

---
+++ CSharp-Technology-FUNDAMENTALS/Arrays - Exercise/08. Magic Sum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _08._Magic_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ CSharp-Technology-FUNDAMENTALS/Arrays - Exercise/09. Kamino Factory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _09._Kamino_Factory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
