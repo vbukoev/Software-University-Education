ea55405c32ee848626258c835b986c0337d9704e
Vasil Bukoev <vbukoev@gmail.com>
2022-06-03 20:31:44 +0300

    Update Program.cs

--- CSharp-Technology-FUNDAMENTALS/Arrays - Exercise/06. Equal Sum/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/Arrays - Exercise/06. Equal Sum/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _06._Equal_Sum
 {
@@ -6,7 +7,44 @@ namespace _06._Equal_Sum
     {
         static void Main(string[] args)
         {
-            
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int leftSum = 0;
+            int rightSum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers.Length == 1)
+                {
+                    Console.WriteLine(0);
+                    return;
+                }
+
+                leftSum = 0;
+                for (int sumLeft = i; sumLeft > 0; sumLeft--)
+                {
+                    int nextLeftElementPosition = sumLeft - 1;
+                    if (sumLeft > 0)
+                    {
+                        leftSum += numbers[nextLeftElementPosition];
+                    }
+                }
+                rightSum = 0;
+                for (int j = i; j < numbers.Length; j++)
+                {
+                    int nextElementPosition = j + 1;
+                    if (j < numbers.Length - 1)
+                    {
+                        rightSum += numbers[nextElementPosition];
+                    }
+                }
+
+                if (rightSum == leftSum)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
+            Console.WriteLine("no");
         }
     }
 }
