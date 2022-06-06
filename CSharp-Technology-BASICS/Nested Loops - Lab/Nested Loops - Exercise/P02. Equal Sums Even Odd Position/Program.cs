f1e0b14ceec939b25e2a7ad23039649ed440f55a
Vasil Bukoev <vbukoev@gmail.com>
2022-06-06 22:10:41 +0300

    tasks with nested loops

---
+++ CSharp-Technology-BASICS/Nested Loops - Lab/Nested Loops - Exercise/P02. Equal Sums Even Odd Position/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace P02._Equal_Sums_Even_Odd_Position
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            for (int i = start; i <= end; i++)
+            {
+                string currNum = i.ToString();
+                int oddSum = 0;
+                int evenSum = 0;
+                for (int j = 0; j < currNum.Length; j++)
+                {
+                    int currDigit = int.Parse(currNum[j].ToString());
+                    if (j % 2 == 0)
+                    {
+                        evenSum += currDigit;
+                    }
+                    else
+                    {
+                        oddSum += currDigit;
+                    }
+                }
+                if (evenSum == oddSum)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
---
+++ CSharp-Technology-BASICS/Nested Loops - Lab/Nested Loops - Lab/01. Clock/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01._Clock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int hours = 0; hours <= 23; hours++)
+            {
+                for (int minutes = 0; minutes <= 59; minutes++)
+                {                   
+                    Console.WriteLine($"{hours:d2}:{minutes:d2}");                    
+                }                     
+            }
+        }
+    }
+}
