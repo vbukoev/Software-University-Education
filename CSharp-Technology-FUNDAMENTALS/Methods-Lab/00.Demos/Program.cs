22bb5794ecb0049ffa97c58f65c8fe486f053356
Vasil Bukoev <vbukoev@gmail.com>
2022-06-08 22:11:03 +0300

    added a lab from 8-6-2022

---
+++ CSharp-Technology-FUNDAMENTALS/Methods-Lab/00.Demos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _00.Demos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintNumbers(1, 5);
+            PrintNumbers(2, 6);
+            PrintNumbers(3, 7);
+        }
+        static void PrintNumbers(int start, int end)
+        {
+            for (int i = start; i < end; i++)
+            {
+                Console.WriteLine("{0}", i );
+            }
+            Console.WriteLine();
+        }
+    }
+}
