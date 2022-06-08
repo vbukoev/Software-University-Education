ce715003496ce4ac5738729766a06f7eb9971232
Vasil Bukoev <vbukoev@gmail.com>
2022-06-08 22:13:29 +0300

    task 10 solution

---
+++ CSharp-Technology-FUNDAMENTALS/Methods-Lab/10. Multiply Evens by Odds/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _10._Multiply_Evens_by_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            number = Math.Abs(number);
+
+            int evenSum = GetSumOfEvenDigits(number);
+            int oddSum = GetSumOfOddDigits(number);
+            int result = GetMultipleOfEvenAndOddSum(evenSum, oddSum);
+            Console.WriteLine(result);
+        }
+        static int GetSumOfEvenDigits(int number)
+        {
+            int sum = 0;
+            int digit = number;
+            while (digit > 0)
+            {
+                int currDigit = digit % 10;
+                if (currDigit % 2 == 0)
+                {
+                    sum += currDigit;
+                }
+
+                digit /= 10;
+            }
+            return sum;
+        }
+        static int GetSumOfOddDigits(int number)
+        {
+            int sum = 0;
+            int digit = number;
+            while (digit > 0)
+            {
+                int currDigit = digit % 10;
+                if (currDigit % 2 != 0)
+                {
+                    sum += currDigit;
+                }
+
+                digit /= 10;
+            }
+            return sum;
+        }
+        static int GetMultipleOfEvenAndOddSum(int evenSum, int oddSum)
+        {
+            return evenSum * oddSum;
+        }
+    }
+}
