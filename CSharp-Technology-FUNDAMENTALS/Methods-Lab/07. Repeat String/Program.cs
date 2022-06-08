f3cc4f63f24994cd15b72bb15ffb5ae9bfe99244
Vasil Bukoev <vbukoev@gmail.com>
2022-06-08 22:12:44 +0300

    task 7 solution

---
+++ CSharp-Technology-FUNDAMENTALS/Methods-Lab/07. Repeat String/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _07._Repeat_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int times = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(RepeatString(input, times));
+
+        }
+        static string RepeatString(string input, int times)
+        {
+            string result = String.Empty;
+            for (int i = 0; i < times; i++)
+            {
+                result += input; 
+            }
+
+            return result;
+        }
+    }
+}
