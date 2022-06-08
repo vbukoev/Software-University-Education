a32f1e8c716b97fe761314fddff48e80f6f96293
Vasil Bukoev <vbukoev@gmail.com>
2022-06-08 22:12:54 +0300

    task 8 solution

---
+++ CSharp-Technology-FUNDAMENTALS/Methods-Lab/08. Math Power/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _08._Math_Power
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double @base = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+            Console.WriteLine(MathPower(@base, power));
+        }
+        static double MathPower(double @base, int power)
+        {
+            double result = 1;
+            for (int i = 0; i < power; i++)
+            {
+                result *= @base;
+            }
+            return result;
+        }
+    }
+}
