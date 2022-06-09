f9e73cf89750b9a0bc0794f43498efc5ac7feca8
Vasil Bukoev <vbukoev@gmail.com>
2022-06-09 21:46:07 +0300

    6th task

---
+++ CSharp-Technology-BASICS/Conditional statements- Lab/P06.SpeedInfo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace P06.SpeedInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            double v = double.Parse(Console.ReadLine());
+            //act
+            if (v <= 10)
+            {
+                Console.WriteLine("slow");
+            }
+            if ((v > 10) && (v <= 50))
+            {
+                Console.WriteLine("average");
+            }
+            if ((v > 50) && (v <= 150))
+            {
+                Console.WriteLine("fast");
+            }
+            if ((v > 150) && (v <= 1000))
+            {
+                Console.WriteLine("ultra fast");
+            }
+            if (v > 1000)
+            {
+                Console.WriteLine("extremely fast");
+            }
+        }
+    }
+}
