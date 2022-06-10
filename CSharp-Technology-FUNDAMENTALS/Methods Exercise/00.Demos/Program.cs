d924f35404f4ed7f550cda83bbb3af29c02de471
Vasil Bukoev <vbukoev@gmail.com>
2022-06-10 22:22:31 +0300

    adding a new task from the ex 10-6-2022

---
+++ CSharp-Technology-FUNDAMENTALS/Methods Exercise/00.Demos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _00.Demos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Print(message: "Heist People");
+            int a = 5;
+            int b = 5;
+            int result = SumNumbers(a,b);
+            Console.WriteLine(result);
+        }
+        static void Print(string message)
+        {
+            Console.WriteLine($"{message}");
+        }
+        static int SumNumbers(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+    }
+}
