0d5a46b7181f6a9614f69c51629d8ec781ba8ff8
Vasil Bukoev <vbukoev@gmail.com>
2022-06-10 22:23:58 +0300

    task 8

---
+++ CSharp-Technology-FUNDAMENTALS/Methods Exercise/08. Factorial Division/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _08._Factorial_Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            double result1 = Factoriel(firstNum);
+            double result2 = Factoriel(secondNum);
+            Console.WriteLine($"{result1/result2:f2}");
+        }
+
+        private static double Factoriel(int firstNum)
+        {
+            double result = 1;
+            while (firstNum!=1)
+            {
+                result *= firstNum;
+                firstNum--; 
+            }
+            return result;
+        }
+    }
+}
