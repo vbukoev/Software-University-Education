f8f8dfd02e0c45eca954f891145ab28059fcb8d3
Vasil Bukoev <vbukoev@gmail.com>
2022-06-10 22:23:52 +0300

    task 7

---
+++ CSharp-Technology-FUNDAMENTALS/Methods Exercise/07. NxN Matrix/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _07._NxN_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            MatrixCreator(input);
+        }
+
+        private static void MatrixCreator(int input)
+        {
+            for (int rows = 0; rows < input; rows++)
+            {
+                for (int col = 0; col < input; col++)
+                {
+                    Console.Write(input + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
