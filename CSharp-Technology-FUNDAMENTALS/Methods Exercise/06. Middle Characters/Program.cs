0bea5695393601be11d18a523a1d3eac68029ba3
Vasil Bukoev <vbukoev@gmail.com>
2022-06-10 22:23:41 +0300

    task 6

---
+++ CSharp-Technology-FUNDAMENTALS/Methods Exercise/06. Middle Characters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _06._Middle_Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            PrintTheMiddleCharacters(word);
+        }
+
+        private static void PrintTheMiddleCharacters(string word)
+        {
+            if (word.Length % 2 == 0)
+            {
+                Console.Write(word[word.Length / 2 - 1]);
+                Console.WriteLine(word[word.Length/2]);
+            }
+            else
+            {
+                Console.WriteLine(word[word.Length/2]);
+            }
+        }
+    }
+}
