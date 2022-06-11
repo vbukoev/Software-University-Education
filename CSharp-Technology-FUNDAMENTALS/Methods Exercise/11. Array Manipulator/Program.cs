213d03e8877713bc14d37bbdd52b6b3ffb7254ac
Vasil Bukoev <vbukoev@gmail.com>
2022-06-11 18:26:16 +0300

    Update Program.cs

--- CSharp-Technology-FUNDAMENTALS/Methods Exercise/11. Array Manipulator/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/Methods Exercise/11. Array Manipulator/Program.cs
@@ -14,6 +14,7 @@ namespace _11._Array_Manipulator
             {
                 if (command[0] == "exchange")
                 {
+
                     int givenIndex = int.Parse(command[1]);
                     arr = ExchangeArr(arr, int.Parse(command[1]));
                 }
