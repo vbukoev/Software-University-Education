5a47a6a932f2296da964643fedc84a4ad91d30ee
Vasil Bukoev <vbukoev@gmail.com>
2022-06-15 21:07:07 +0300

    Update Program.cs

--- CSharp-Technology-FUNDAMENTALS/Lists-Lab/Lists - Lab/06. List Manipulation Basics/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/Lists-Lab/Lists - Lab/06. List Manipulation Basics/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _06._List_Manipulation_Basics
 {
@@ -6,7 +8,39 @@ namespace _06._List_Manipulation_Basics
     {
         static void Main(string[] args)
         {
-            
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command=="end")
+                {
+                    break;
+                }
+                string[] tokens = command.Split();
+                string action = tokens[0];
+                switch (action)
+                {
+                    case "Add":
+                        int numberToAdd = int.Parse(tokens[1]);
+                        numbers.Add(numberToAdd);
+                        break;
+                    case "Remove":
+                        int numberToRemove = int.Parse(tokens[1]);
+                        numbers.Remove(numberToRemove);
+                        break;
+                    case "RemoveAt":
+                        int numberToRemoveAt = int.Parse(tokens[1]);
+                        numbers.RemoveAt(numberToRemoveAt);
+                        break;
+                    case "Insert":
+                        int numberToInsert = int.Parse(tokens[1]);
+                        int indexToInsert = int.Parse(tokens[2]);
+                        numbers.Insert(indexToInsert, numberToInsert);
+                        break;
+                }
+
+            }
+            Console.WriteLine(string.Join(" ", numbers));
         }
     }
 }
