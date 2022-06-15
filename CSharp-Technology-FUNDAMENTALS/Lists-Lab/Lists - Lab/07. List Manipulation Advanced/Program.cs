c1bff8a3869dc7608e229e9549af05152ce9d97f
Vasil Bukoev <vbukoev@gmail.com>
2022-06-15 20:47:45 +0300

    adding the 7th task

---
+++ CSharp-Technology-FUNDAMENTALS/Lists-Lab/Lists - Lab/07. List Manipulation Advanced/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _07._List_Manipulation_Advanced
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
