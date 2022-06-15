264c0a0adf92475db7978e7ea7f82115d016c6f9
Vasil Bukoev <vbukoev@gmail.com>
2022-06-15 21:23:36 +0300

    Update Program.cs

--- CSharp-Technology-FUNDAMENTALS/Lists-Lab/Lists - Lab/07. List Manipulation Advanced/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/Lists-Lab/Lists - Lab/07. List Manipulation Advanced/Program.cs
@@ -1,12 +1,75 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace _07._List_Manipulation_Advanced
+namespace _06._List_Manipulation_Basics
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "end")
+                {
+                    break;
+                }
+                string[] tokens = command.Split();
+                string action = tokens[0];
+                switch (action)
+                {
+                    case "Add":
+                        int numberToAdd = int.Parse(tokens[1]);
+                        numbers.Add(numberToAdd);
+                        break;
+                    case "Remove":
+                        int numberToRemove = int.Parse(tokens[1]);
+                        numbers.Remove(numberToRemove);
+                        break;
+                    case "RemoveAt":
+                        int numberToRemoveAt = int.Parse(tokens[1]);
+                        numbers.RemoveAt(numberToRemoveAt);
+                        break;
+                    case "Insert":
+                        int numberToInsert = int.Parse(tokens[1]);
+                        int indexToInsert = int.Parse(tokens[2]);
+                        numbers.Insert(indexToInsert, numberToInsert);
+                        break;
+                    case "Contains":
+                        PrintContains(int.Parse(tokens[1]), numbers);
+                        break;
+                    case "PrintEven":
+                        PrintEven(numbers);
+                        break;
+                }
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+         
+
+        static void PrintContains(int number, List<int> numbers)
+        {
+            if (numbers.Contains(number))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No such number");
+            }
+        }
+        static void PrintEven(List<int> numbers)
+        {
+            foreach (var number in numbers)
+            {
+                if (number%2==0)
+                {
+                    Console.WriteLine($"{number} ");
+                }
+            }
         }
     }
 }
