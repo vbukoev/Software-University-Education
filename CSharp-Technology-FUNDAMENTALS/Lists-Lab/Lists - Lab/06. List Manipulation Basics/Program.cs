49d23e9ddf748450a211f414c9afd49b19ed40e7
Vasil Bukoev <vbukoev@gmail.com>
2022-06-15 20:42:13 +0300

    added new task 6

---
+++ CSharp-Technology-FUNDAMENTALS/Lists-Lab/Lists - Lab/06. List Manipulation Basics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _06._List_Manipulation_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
