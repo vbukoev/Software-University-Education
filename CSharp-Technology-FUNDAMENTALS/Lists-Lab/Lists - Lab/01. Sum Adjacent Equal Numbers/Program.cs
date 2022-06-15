7708526135dce56a129b3420179b7cbfe96ddcb9
Vasil Bukoev <vbukoev@gmail.com>
2022-06-15 20:46:00 +0300

    Update Program.cs

--- CSharp-Technology-FUNDAMENTALS/Lists-Lab/Lists - Lab/01. Sum Adjacent Equal Numbers/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/Lists-Lab/Lists - Lab/01. Sum Adjacent Equal Numbers/Program.cs
@@ -7,7 +7,23 @@ namespace _01._Sum_Adjacent_Equal_Numbers
     {
         static void Main(string[] args)
         {
-             
+            List<double> numbers = Console.ReadLine()
+               .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+               .Select(double.Parse)
+               .ToList();
+
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    numbers[i] = numbers[i] + numbers[i + 1];
+                    numbers.Remove(numbers[i + 1]);
+
+                    i = -1;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
         }
     }
 }
