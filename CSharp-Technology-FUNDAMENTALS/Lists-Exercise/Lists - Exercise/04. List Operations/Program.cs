6148d0b1212602e25474edc80f9bdb0d64f09649
Vasil Bukoev <vbukoev@gmail.com>
2022-06-17 20:02:07 +0300

    5th task

--- CSharp-Technology-FUNDAMENTALS/Lists-Exercise/Lists - Exercise/04. List Operations/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/Lists-Exercise/Lists - Exercise/04. List Operations/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _04._List_Operations
 {
@@ -6,7 +7,62 @@ namespace _04._List_Operations
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                string[] tokens = command.Split();
+                if (tokens[0] == "Add")
+                {
+                    int number = int.Parse(tokens[1]);
+                    numbers.Add(number);
+                }
+                else if (tokens[0] == "Insert")
+                {
+                    int number = int.Parse(tokens[1]);
+                    int index = int.Parse(tokens[2]);
+                    if (index > numbers.Count - 1 || index < 0)
+                    {
+                        Console.WriteLine("Invalid index");
+                        continue;
+                    }
+                    numbers.Insert(index, number);
+                }
+                else if (tokens[0] == "Remove")
+                {
+                    int index = int.Parse(tokens[1]);
+                    if (index > numbers.Count - 1 || index < 0)
+                    {
+                        Console.WriteLine("Invalid index");
+                        continue;
+                    }
+                    numbers.RemoveAt(index);
+                }
+
+                else if(tokens[0]=="Shift")
+                {
+                    int cnt = int.Parse(tokens[2]);
+                    if (tokens[1]== "left")
+                    {
+                        for (int i = 0; i < cnt; i++)
+                        {
+                            numbers.Add(numbers[0]);
+                            numbers.RemoveAt(0);
+                        }
+                    }
+                    else if (tokens[1]== "right")
+                    {
+                        for (int i = 0; i < cnt; i++)
+                        {
+                            numbers.Insert(0, numbers[numbers.Count - 1]);
+                            numbers.RemoveAt(numbers.Count - 1);
+                        }
+                    }
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(String.Join(" ", numbers));
         }
     }
 }
