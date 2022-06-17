c09a19f3b30870b01b8bbb662083144cc881f9d6
Vasil Bukoev <vbukoev@gmail.com>
2022-06-17 19:04:17 +0300

    added new tasks 1 and 2 from ex 17-06-2022

---
+++ CSharp-Technology-FUNDAMENTALS/Lists-Exercise/Lists - Exercise/02.ChangeList/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.ChangeList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string line = Console.ReadLine();
+
+            while (line!="end")
+            {
+                string[] tokens = line.Split();
+                string cmd = tokens[0];
+                if (cmd == "Delete")
+                {
+                    int element = int.Parse(tokens[1]);
+                    numbers.RemoveAll(el => el == element);
+                }
+                else if (cmd =="Insert")
+                {
+                    int element = int.Parse(tokens[1]);
+                    int index = int.Parse(tokens[2]);
+                    numbers.Insert(index, element);
+                }
+                line = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
