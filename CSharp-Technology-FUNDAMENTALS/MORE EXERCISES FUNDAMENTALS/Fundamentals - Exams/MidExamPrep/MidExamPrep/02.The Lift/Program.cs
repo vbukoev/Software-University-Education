d3d0de9e5e83715996bfe95b9c818a4cb09c5cd2
Vasil Bukoev <vbukoev@gmail.com>
2022-06-20 19:15:47 +0300

    2nd task

--- CSharp-Technology-FUNDAMENTALS/MORE EXERCISES FUNDAMENTALS/Fundamentals - Exams/MidExamPrep/MidExamPrep/02.The Lift/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/MORE EXERCISES FUNDAMENTALS/Fundamentals - Exams/MidExamPrep/MidExamPrep/02.The Lift/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _02.The_Lift
 {
@@ -6,7 +8,30 @@ namespace _02.The_Lift
     {
         static void Main(string[] args)
         {
-            
+            int waitingPeople = int.Parse(Console.ReadLine());
+            List<int> liftWithWagons = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int maxPeople = 4;
+            for (int i = 0; i < liftWithWagons.Count; i++)
+            {
+                for (int j = liftWithWagons[i]; j < maxPeople; j++)
+                {
+                    liftWithWagons[i]++;
+                    waitingPeople--;
+
+                    if (waitingPeople == 0)
+                    {
+                        if (liftWithWagons.Sum() < maxPeople* liftWithWagons.Count)
+                        {
+                            Console.WriteLine("The lift has empty spots!");
+                        }
+                        Console.WriteLine(string.Join(' ', liftWithWagons));
+                        return;
+                    }                   
+                }
+            }
+            Console.WriteLine($"There isn't enough space! {waitingPeople} people in a queue!");
+            Console.WriteLine(string.Join(' ', liftWithWagons));
         }
     }
 }
