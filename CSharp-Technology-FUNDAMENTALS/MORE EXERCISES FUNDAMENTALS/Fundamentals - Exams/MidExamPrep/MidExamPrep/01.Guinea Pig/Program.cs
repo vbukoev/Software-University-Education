af04607be09b6e281ab3a27241619a9214490d2c
Vasil Bukoev <vbukoev@gmail.com>
2022-06-20 18:57:10 +0300

    submitting 1st task of mid prep exercise

---
+++ CSharp-Technology-FUNDAMENTALS/MORE EXERCISES FUNDAMENTALS/Fundamentals - Exams/MidExamPrep/MidExamPrep/01.Guinea Pig/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _01.Guinea_Pig
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double quantityFoodInKg = double.Parse(Console.ReadLine()) * 1000;
+            double quantityHayInKg = double.Parse(Console.ReadLine()) * 1000;
+            double quantityCoverInKg = double.Parse(Console.ReadLine()) * 1000;
+            double weightOfPigInKg = double.Parse(Console.ReadLine()) * 1000;
+
+            for (int i = 1; i <= 30; i++)
+            {
+                quantityFoodInKg -= 300;
+                if (i % 2 == 0)
+                {
+                    quantityHayInKg -= 0.05 * quantityFoodInKg;
+                }
+                if (i % 3 == 0)
+                {
+                    quantityCoverInKg -=  weightOfPigInKg/3;
+                }
+                if (quantityFoodInKg <= 0 || quantityCoverInKg <= 0 || quantityHayInKg <= 0)
+                {
+                    Console.WriteLine("Merry must go to the pet store!");
+                    return;
+                }
+            }
+            Console.WriteLine($"Everything is fine! Puppy is happy! Food: {(quantityFoodInKg/1000):f2}, Hay: {(quantityHayInKg/1000):f2}, Cover: {(quantityCoverInKg/1000):f2}.");
+        }
+    }
+}
