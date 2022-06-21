92b883b67fe28fe878b4e70eca0cfa887affe009
Vasil Bukoev <vbukoev@gmail.com>
2022-06-21 18:02:17 +0300

    adding one more task

---
+++ CSharp-Technology-FUNDAMENTALS/MORE EXERCISES FUNDAMENTALS/Fundamentals - Exams/MidExamPrep/MidExamPrep/01.SoftUniReception/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01.SoftUniReception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            int firstEfficiency = int.Parse(Console.ReadLine());
+            int secondEfficiency = int.Parse(Console.ReadLine());
+            int thirdEfficiency = int.Parse(Console.ReadLine());
+            int studentsCount = int.Parse(Console.ReadLine());
+            int neededTime = 0;
+            int totalEff = firstEfficiency + secondEfficiency + thirdEfficiency;
+
+            //logicOfTheOperation
+            while (studentsCount > 0)
+            {
+                studentsCount -= totalEff;
+                neededTime++;
+                if (neededTime % 4 == 0)
+                {
+                    neededTime++;
+                }
+            }
+           
+            //output 
+            Console.WriteLine($"Time needed: {neededTime}h.");
+        }
+    }
+}
--- CSharp-Technology-FUNDAMENTALS/MORE EXERCISES FUNDAMENTALS/Fundamentals - Exams/MidExamPrep/MidExamPrep/07.WorldTour/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/MORE EXERCISES FUNDAMENTALS/Fundamentals - Exams/MidExamPrep/MidExamPrep/07.WorldTour/Program.cs
@@ -72,44 +72,52 @@ namespace _07.WorldTour
     {
         static void Main(string[] args)
         {
-            string newLine = Console.ReadLine();
-            string command = string.Empty;
+            string text = Console.ReadLine();
+            string command = Console.ReadLine();
 
             while (command != "Travel")
-            {
-                command = Console.ReadLine();
+            {                
                 string[] tokens = command.Split(":", StringSplitOptions.RemoveEmptyEntries);
                 string action = tokens[0];
-                switch (action)
+                if (action == "Add Stop")
                 {
-                    case "Add Stop":
-                        int firstSymbol = int.Parse(tokens[1]);
-                        string name = tokens[2];
-                        if (firstSymbol >= 0 && firstSymbol < newLine.Length)
-                        {
-                            newLine.Insert(firstSymbol, name);
-                        }
-                        Console.WriteLine(newLine);
-                        break;
-                    case "Remove Stop":
-                        int startIndex = int.Parse(tokens[1]);
-                        int lastIndex = int.Parse(tokens[2]);
-                        if (startIndex >= 0 && startIndex < lastIndex && lastIndex < newLine.Length)
-                        {
-                            newLine.Remove(startIndex, lastIndex - startIndex);
-                        }
-                        Console.WriteLine(newLine);
-                        break;
-                    case "Switch":
-                        string old = tokens[1];
-                        string news = tokens[2];
-                        newLine.Replace(old, news);
-                        Console.WriteLine(newLine);
-                        break;
+                    int idx = int.Parse(tokens[1]);
+
+                    if (idx >= 0 && idx < text.Length)
+                    {
+                        string substr = tokens[2];
+                        text = text.Insert(idx, substr);
+                    }
+
+                    Console.WriteLine(text);
+                }
+                else if (action == "Remove Stop")
+                {
+                    int startIdx = int.Parse(tokens[1]);
+                    int endIdx = int.Parse(tokens[2]);
+
+                    if (startIdx >= 0 && endIdx < text.Length)
+                    {
+                        text = text.Remove(startIdx, endIdx - startIdx + 1);
+                    }
+
+                    Console.WriteLine(text);
+                }
+                else if (action == "Switch")
+                {
+                    string oldText = tokens[1];
+                    string newText = tokens[2];
+
+                    if (text.Contains(oldText))
+                    {
+                        text = text.Replace(oldText, newText);
+                    }
+
+                    Console.WriteLine(text);
                 }
                 command = Console.ReadLine();
             }
-            Console.WriteLine($"Ready for world tour! Planned stops: {newLine}");
+            Console.WriteLine($"Ready for world tour! Planned stops: {text}");
         }
     }
 }
