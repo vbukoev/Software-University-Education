0c4369fe669327438ea0aeb15c7ca97e291845c5
Vasil Bukoev <vbukoev@gmail.com>
2022-06-21 21:47:09 +0300

    using the regex class in this task

---
+++ CSharp-Technology-FUNDAMENTALS/MORE EXERCISES FUNDAMENTALS/Fundamentals - Exams/MidExamPrep/MidExamPrep/02.DestinationMapper/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02.DestinationMapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {             
+            string newText = Console.ReadLine();
+            string pattern = @"(=|\/)(?<l>[A-Z][A-Za-z]{2,})\1";
+            int count = 0;
+            Regex regex = new Regex(pattern, RegexOptions.Compiled);
+            List<string> sequence = new List<string>();
+            foreach (Match item in regex.Matches(newText))
+            {
+                count += item.Groups[2].Length;
+                string currDest = item.Groups[2].Value;
+                sequence.Add(currDest);
+            }
+            string dests = string.Join(", ", sequence);
+            Console.WriteLine($"Destinations: {dests}");
+            Console.WriteLine($"Travel Points: {count}");
+        }
+    }
+}
---
+++ CSharp-Technology-FUNDAMENTALS/MORE EXERCISES FUNDAMENTALS/Fundamentals - Exams/MidExamPrep/MidExamPrep/02.ShootfortheWin/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.ShootfortheWin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> sequence = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            string command = Console.ReadLine();
+            int cnt = 0;
+            while (command != "End")
+            {
+                int index = int.Parse(command);
+                if (index >= sequence.Count || index < 0 || sequence[index] == -1)
+                {
+                    command = Console.ReadLine();
+                    continue;
+                }
+                int old = sequence[index];
+                sequence[index] = -1;
+                cnt++;
+                for (int i = 0; i <= sequence.Count - 1; i++)
+                {
+                    if (sequence[i] == -1) continue;
+                    if (old < sequence[i])
+                    {
+                        int @new = sequence[i] - old;
+                        sequence[i] = @new;
+                    }
+                    else if (old >= sequence[i] && sequence[i] != -1)
+                    {
+                        int @new = old + sequence[i];
+                        sequence[i] = @new;
+                    }
+
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Shot targets: {cnt} -> {string.Join(' ', sequence)}");
+        }
+    }
+}
