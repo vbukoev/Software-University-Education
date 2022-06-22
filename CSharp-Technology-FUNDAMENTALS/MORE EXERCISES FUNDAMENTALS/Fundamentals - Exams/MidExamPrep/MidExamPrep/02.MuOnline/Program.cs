9e85ad9359b42565642155b5b1a57286aa85a976
Vasil Bukoev <vbukoev@gmail.com>
2022-06-22 22:06:00 +0300

    adding task 2 and 3 from mid exam prep tasks

--- CSharp-Technology-FUNDAMENTALS/MORE EXERCISES FUNDAMENTALS/Fundamentals - Exams/MidExamPrep/MidExamPrep/02.MuOnline/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/MORE EXERCISES FUNDAMENTALS/Fundamentals - Exams/MidExamPrep/MidExamPrep/02.MuOnline/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _02.MuOnline
 {
@@ -6,7 +8,67 @@ namespace _02.MuOnline
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            List<string> dungeonsRoom = Console.ReadLine().Split("|", StringSplitOptions.RemoveEmptyEntries).ToList();
+            int initialHealth = 100;
+            int initialBitcoins = 0;
+            string command = "";
+            int number = 0;
+            int rooms = 0;
+            List<string> currRoom = new List<string>();
+            for (int i = 0; i < dungeonsRoom.Count; i++)
+            {
+                currRoom = dungeonsRoom[i].Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+                command = currRoom[0];
+                number = int.Parse(currRoom[1]);
+                switch (command)
+                {
+                    case "potion":
+                        if (initialHealth == 100) rooms++;
+                        else if (initialHealth < 100)
+                        {
+                            int healthPoint = number;
+                            bool isEnoughHealth = number + initialHealth > 100;
+                            if (!isEnoughHealth)
+                            {
+                                initialHealth += number;
+                                Console.WriteLine($"You healed for {healthPoint} hp.");
+                                Console.WriteLine($"Current health: {initialHealth} hp.");
+                                rooms++;
+                            }
+                            else
+                            {
+                                int diff = 100 - initialHealth;
+                                initialHealth += diff;
+                                Console.WriteLine($"You healed for {diff} hp.");
+                                Console.WriteLine($"Current health: {initialHealth} hp.");
+                                rooms++;
+                            }
+                        }
+                        break;
+                    case "chest":
+                        int currBitCoin = number;
+                        Console.WriteLine($"You found {number} bitcoins.");
+                        initialBitcoins += currBitCoin;
+                        rooms++;
+                        break;
+                    default:
+                        bool isHealthZero = initialHealth - number <= 0;
+                        if (isHealthZero)
+                        {
+                            rooms++;
+                            Console.WriteLine($"You died! Killed by {command}.");
+                            Console.WriteLine($"Best room: {rooms}");
+                            return;
+                        }
+                        Console.WriteLine($"You slayed {command}.");
+                        initialHealth -= number;
+                        rooms++;
+                        break;
+                }
+            }
+            Console.WriteLine("You've made it!");
+            Console.WriteLine($"Bitcoins: {initialBitcoins}");
+            Console.WriteLine($"Health: {initialHealth}");
+        }    
     }
 }
---
+++ CSharp-Technology-FUNDAMENTALS/MORE EXERCISES FUNDAMENTALS/Fundamentals - Exams/MidExamPrep/MidExamPrep/03.Inventory/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.Inventory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> journal = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToList();
+            string command = Console.ReadLine();
+            while (command != "Craft!")
+            {
+                string[] tokens = command.Split(" - ").ToArray();
+                string action = tokens[0];
+                string item = tokens[1];
+                switch (action)
+                {
+                    case "Collect":
+                        if (!journal.Contains(item)) journal.Add(item);
+                        break;
+                    case "Drop":
+                        if (journal.Contains(item)) journal.Remove(item);
+                        break;
+                    case "Combine Items":
+                        string[] splitted = item.Split(":", StringSplitOptions.RemoveEmptyEntries);
+                        int index = journal.IndexOf(splitted[0]) + 1;
+                        if (journal.Contains(splitted[0])) journal.Insert(index, splitted[1]);
+                        break;
+                    case "Renew":
+                        if (journal.Contains(item))
+                        {
+                            index = journal.IndexOf(item);
+                            journal.Remove(item);
+                            journal.Add(item);
+                        }
+                        break;
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(String.Join(", ", journal));
+        }
+    }
+}
