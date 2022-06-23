c778691ccbb28399e2be16ac19f0f8aff0a13a8a
Vasil Bukoev <vbukoev@gmail.com>
2022-06-23 18:04:17 +0300

    adding 1st task of the day :)

---
+++ CSharp-Technology-FUNDAMENTALS/MORE EXERCISES FUNDAMENTALS/Fundamentals - Exams/MidExamPrep/MidExamPrep/01.BlackFlag/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _01.BlackFlag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int daysOfPlunder = int.Parse(Console.ReadLine());
+            double dailyPlunder = double.Parse(Console.ReadLine());
+            double expectedPlunder = double.Parse(Console.ReadLine());
+            double totalPlunder = 0;
+            for (int day = 1; day <= daysOfPlunder; day++)
+            {
+                totalPlunder += dailyPlunder;
+                if (day % 3 == 0)
+                {
+                    totalPlunder += dailyPlunder / 2;
+                }
+                if (day % 5 == 0)
+                {
+                    totalPlunder *= 0.7;
+                }
+            }
+            if (totalPlunder >= expectedPlunder)
+            {
+                Console.WriteLine($"Ahoy! {totalPlunder:f2} plunder gained.");
+            }
+            else
+            {
+                double left = totalPlunder/expectedPlunder;
+                double percentageLeft = left * 100;
+                Console.WriteLine($"Collected only {percentageLeft:f2}% of the plunder.");
+            }
+        }
+    }
+}
