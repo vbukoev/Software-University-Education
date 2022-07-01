2ceb4060455bd4155b7cdd063cb9ee049d29376b
Vasil Bukoev <vbukoev@gmail.com>
2022-07-01 18:53:18 +0300

    adding task 1

--- CSharp-Technology-FUNDAMENTALS/Objects and Classes - Exercise/01. Advertisement Message/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/Objects and Classes - Exercise/01. Advertisement Message/Program.cs
@@ -6,7 +6,28 @@ namespace _01._Advertisement_Message
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numOfMessages = int.Parse(Console.ReadLine());
+            string[] phrases = 
+            {
+                "Excellent product.", "Such a great product.", "I always use that product.", "Best product of its category.", "Exceptional product.", "I can't live without this product." 
+            };
+            string[] events = 
+            {
+                "Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!" 
+            };
+            string[] authors = { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
+            string[] cities = { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
+            Random random = new Random();
+            
+            for (int i = 0; i < numOfMessages; i++)
+            {
+                string phrase = phrases[random.Next(0, phrases.Length)];
+                string evenet = events[random.Next(0, events.Length)];
+                string author = authors[random.Next(0, authors.Length)];
+                string city = cities[random.Next(0, cities.Length)];
+                Console.WriteLine($"{phrase} {evenet} {author} {city}");
+        }
+            
         }
     }
 }
