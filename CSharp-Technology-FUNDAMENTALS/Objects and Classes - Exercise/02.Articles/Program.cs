5bd2b790eb056527dfbf1476b77653170c69bfe7
Vasil Bukoev <vbukoev@gmail.com>
2022-07-01 19:23:56 +0300

    adding task2

--- CSharp-Technology-FUNDAMENTALS/Objects and Classes - Exercise/02.Articles/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/Objects and Classes - Exercise/02.Articles/Program.cs
@@ -2,11 +2,52 @@
 
 namespace _02.Articles
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
+        {            
+            string[] currArt = Console.ReadLine().Split(", ");
+            int cntChanges = int.Parse(Console.ReadLine());
+            var art = new Article(currArt[0], currArt[1], currArt[2]);
+            for (int i = 0; i < cntChanges; i++)
+            {
+                string[] lines = Console.ReadLine().Split(": ");
+                string command = lines[0];
+                string argument = lines[1];
+                switch (command)
+                {
+                    case "Edit":
+                        art.Edit(argument);
+                        break;
+                    case "Change Author":
+                        art.ChangeAuthor(argument);
+                        break;
+                    case "Rename":
+                        art.Rename(argument);
+                        break;
+                }
+            }
+            Console.WriteLine(art);
+        }
+    }
+    class Article
+    {
+        public Article(string title, string content, string author)
         {
-            Console.WriteLine("Hello World!");
+            Author = author;
+            Title = title;
+            Content = content;
+        }
+        public string Title { get; set; } //getter - moje da se dostupi propurtito
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public void Rename(string title) => Title = title;
+        public void Edit(string content) => Content = content;
+        public void ChangeAuthor(string author) => Author = author;
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
         }
     }
 }
---
+++ CSharp-Technology-FUNDAMENTALS/Objects and Classes - Exercise/03.Articles2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03.Articles2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
