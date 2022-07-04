34025e4804777404d12fe1f421811cf16269deb6
Vasil Bukoev <vbukoev@gmail.com>
2022-07-04 21:02:33 +0300

    task 2 from more ex objects and classes

--- CSharp-Technology-FUNDAMENTALS/MORE EXERCISES FUNDAMENTALS/ObjectsAndClasses-MoreExercise/02/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/MORE EXERCISES FUNDAMENTALS/ObjectsAndClasses-MoreExercise/02/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace _02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            Family family = new Family();
+            int numOfMember = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numOfMember; i++)
+            {
+                family.AddMember(Console.ReadLine().Split());
+
+            }
+            Person theOldest = family.Oldest();
+            Console.WriteLine($"{theOldest.Name} {theOldest.Age}");
         }
+    }
+    class Person
+    { 
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+    class Family
+    {
+        public List<Person> FamilyPeople { get; set; } = new List<Person>();
+        public void AddMember(string[] info)
+        {
+            Person newMember = new Person(info[0], int.Parse(info[1]));
+            FamilyPeople.Add(newMember);
+        }
+        public Person Oldest() => FamilyPeople.OrderByDescending(member=>member.Age).First();        
     }
 }
