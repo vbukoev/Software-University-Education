19cedea4114c5a29e9380e8eddf56e5c99611143
Vasil Bukoev <vbukoev@gmail.com>
2022-07-06 19:52:15 +0300

    one more solution for the 1st task

--- CSharp-Technology-FUNDAMENTALS/AssociativeArrays-Lab/01. Count Real Numbers/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/AssociativeArrays-Lab/01. Count Real Numbers/Program.cs
@@ -12,9 +12,19 @@ namespace _01._Count_Real_Numbers
             var cnts = new SortedDictionary<double, int>();
             foreach (var number in numbers)
             {
-                if (cnts.ContainsKey(number)) cnts[number]++;
+                if (cnts.ContainsKey(number))
+                {
+                    int currOcc = cnts[number];
+                    currOcc += 1;
+                    cnts[number] = currOcc;
+                }
                 else cnts.Add(number, 1);
             }
+
+            //{
+            //    if (cnts.ContainsKey(number)) cnts[number]++;
+            //    else cnts.Add(number, 1);
+            //}
             foreach (var item in cnts) Console.WriteLine($"{item.Key} -> {item.Value}");
         }
     }
