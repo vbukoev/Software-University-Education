e59a67f77c622b720d45ca5a1ae12684e7a25e95
Vasil Bukoev <vbukoev@gmail.com>
2022-07-06 20:20:50 +0300

    3rd task

--- CSharp-Technology-FUNDAMENTALS/AssociativeArrays-Lab/03.WordSynonyms/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/AssociativeArrays-Lab/03.WordSynonyms/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _03.WordSynonyms
 {
@@ -6,6 +7,16 @@ namespace _03.WordSynonyms
     {
         static void Main(string[] args)
         {
+            int cnt = int.Parse(Console.ReadLine());
+            Dictionary<string, List<string>> synonyms = new Dictionary<string, List<string>>();
+            for (int i = 0; i < cnt; i++)
+            {
+                string wrd = Console.ReadLine();
+                string synonym = Console.ReadLine();
+                if (!synonyms.ContainsKey(wrd)) synonyms.Add(wrd, new List<string>());
+                synonyms[wrd].Add(synonym);
+            }
+            foreach (var item in synonyms) Console.WriteLine($"{item.Key} - {string.Join(", ", item.Value)}"); 
             
         }
     }
