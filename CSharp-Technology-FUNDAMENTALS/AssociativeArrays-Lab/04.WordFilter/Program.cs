0b6222359d68f8181145b48710c45d13933dd660
Vasil Bukoev <vbukoev@gmail.com>
2022-07-06 18:32:10 +0300

    program.cs

--- CSharp-Technology-FUNDAMENTALS/AssociativeArrays-Lab/04.WordFilter/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/AssociativeArrays-Lab/04.WordFilter/Program.cs
@@ -8,10 +8,7 @@ namespace _04.WordFilter
         static void Main(string[] args)
         {
             string[] words = Console.ReadLine().Split().Where(word => word.Length%2 == 0).ToArray();
-            foreach (var item in words)
-            {
-                Console.WriteLine(item);
-            }
+            foreach (var item in words) Console.WriteLine(item);
         }
     }
 }
