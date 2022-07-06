cf42b9fefd820fd9fcff32ee3b800146b4a257ba
Vasil Bukoev <vbukoev@gmail.com>
2022-07-06 19:57:37 +0300

    Program.cs

--- CSharp-Technology-FUNDAMENTALS/AssociativeArrays-Lab/01. Count Real Numbers/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/AssociativeArrays-Lab/01. Count Real Numbers/Program.cs
@@ -12,19 +12,9 @@ namespace _01._Count_Real_Numbers
             var cnts = new SortedDictionary<double, int>();
             foreach (var number in numbers)
             {
-                if (cnts.ContainsKey(number))
-                {
-                    int currOcc = cnts[number];
-                    currOcc += 1;
-                    cnts[number] = currOcc;
-                }
+                if (cnts.ContainsKey(number)) cnts[number]++;
                 else cnts.Add(number, 1);
             }
-
-            //{
-            //    if (cnts.ContainsKey(number)) cnts[number]++;
-            //    else cnts.Add(number, 1);
-            //}
             foreach (var item in cnts) Console.WriteLine($"{item.Key} -> {item.Value}");
         }
     }
