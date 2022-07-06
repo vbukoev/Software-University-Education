2e0d8844b634cfd3965453bb8f2d7bb863a70474
Vasil Bukoev <vbukoev@gmail.com>
2022-07-06 18:27:08 +0300

    submitting 4th task from the lab 06-07-2022

---
+++ CSharp-Technology-FUNDAMENTALS/AssociativeArrays-Lab/01. Count Real Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._Count_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ CSharp-Technology-FUNDAMENTALS/AssociativeArrays-Lab/04.WordFilter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.WordFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split().Where(word => word.Length%2 == 0).ToArray();
+            foreach (var item in words)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
