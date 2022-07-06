175f9c70bacb3b7488a028fee6672c05a16e7721
Vasil Bukoev <vbukoev@gmail.com>
2022-07-06 21:25:29 +0300

    2nd task added

--- CSharp-Technology-FUNDAMENTALS/AssociativeArrays-Lab/02.OddOccurrences/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/AssociativeArrays-Lab/02.OddOccurrences/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _02.OddOccurrences
 {
@@ -6,7 +7,16 @@ namespace _02.OddOccurrences
     {
         static void Main(string[] args)
         {
-            
+            string[] words = Console.ReadLine().Split();
+            Dictionary<string, int> cnt = new Dictionary<string, int>();
+            foreach (var item in words)
+            {
+                string wordInLCase = item.ToLower();
+                if (cnt.ContainsKey(wordInLCase)) cnt[wordInLCase]++;
+                else cnt.Add(wordInLCase, 1);                
+            }
+            foreach (var item1 in cnt)  
+                if (item1.Value % 2 != 0) Console.Write(item1.Key + " ");
         }
     }
 }
