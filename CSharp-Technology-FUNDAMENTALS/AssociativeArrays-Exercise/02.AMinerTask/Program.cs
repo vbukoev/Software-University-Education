b0026315ea2fcb23be501bfd74536def90ea24ba
Vasil Bukoev <vbukoev@gmail.com>
2022-07-07 15:11:57 +0300

    2nd task from the exercise from 08-07-2022

---
+++ CSharp-Technology-FUNDAMENTALS/AssociativeArrays-Exercise/02.AMinerTask/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02.AMinerTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new Dictionary<string, int>();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "stop") break;
+                int quantity = int.Parse(Console.ReadLine());
+                if (!dictionary.ContainsKey(input)) dictionary[input] = quantity;
+                else dictionary[input] += quantity;
+            }
+            foreach (var item in dictionary) Console.WriteLine($"{item.Key} -> {item.Value}");
+        }
+    }
+}
