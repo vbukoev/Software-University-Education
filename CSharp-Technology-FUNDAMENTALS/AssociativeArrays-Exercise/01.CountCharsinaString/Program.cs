df0799ac9524fb4c8f98fb6364f402ae2f712fcc
Vasil Bukoev <vbukoev@gmail.com>
2022-07-07 15:02:11 +0300

    1st task of 08-07-2022 exercise

---
+++ CSharp-Technology-FUNDAMENTALS/AssociativeArrays-Exercise/01.CountCharsinaString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.CountCharsinaString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<char, int> chars = new Dictionary<char, int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] != ' ')
+                {
+                    if (chars.ContainsKey(input[i])) chars[input[i]]++;
+                    else chars.Add(input[i], 1);
+                }
+                else continue;
+            }
+            chars.OrderByDescending(@char => @char.Value);
+            foreach (var newChar in chars) Console.WriteLine($"{newChar.Key} -> {newChar.Value}");            
+        }
+    }
+}
