5d6e80105f36dfe65ba5f7251e7cdef144e6c7b2
Vasil Bukoev <vbukoev@gmail.com>
2022-07-08 18:40:10 +0300

    Update Program.cs

--- CSharp-Technology-FUNDAMENTALS/AssociativeArrays-Exercise/01.CountCharsinaString/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/AssociativeArrays-Exercise/01.CountCharsinaString/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Linq; 
 
 namespace _01.CountCharsinaString
 {
--- CSharp-Technology-FUNDAMENTALS/AssociativeArrays-Exercise/03.LegendaryFarming/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/AssociativeArrays-Exercise/03.LegendaryFarming/Program.cs
@@ -1,143 +1,4 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-
-//namespace Legendary_Farming
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-
-//            var legendaryFarming = new Dictionary<string, int>()
-//            {
-//                {"shards", 0 },
-//                {"fragments", 0},
-//                {"motes", 0 }
-//            };
-
-//            bool finish = false;
-//            while (!finish)
-//            {
-
-//                string[] input = Console.ReadLine().Split();
-//                for (int i = 0; i < input.Length; i += 2)
-//                {
-//                    int valueItem = int.Parse(input[i]);
-//                    string nameItem = input[i + 1].ToLower(); ;
-
-//                    if (legendaryFarming.ContainsKey(nameItem))
-//                    {
-//                        legendaryFarming[nameItem] += valueItem;
-//                    }
-//                    else
-//                    {
-//                        legendaryFarming.Add(nameItem, valueItem);
-//                    }
-
-//                    bool validName = nameItem == "shards" || nameItem == "fragments" || nameItem == "motes";
-//                    if (legendaryFarming[nameItem] >= 250 && validName)
-//                    {
-//                        finish = true;
-//                        switch (nameItem)
-//                        {
-//                            case "shards": Console.WriteLine("Shadowmourne obtained!"); break;
-//                            case "fragments": Console.WriteLine("Valanyr obtained!"); break;
-//                            case "motes": Console.WriteLine("Dragonwrath obtained!"); break;
-//                        }
-//                        legendaryFarming[nameItem] -= 250;
-//                        break;
-//                    }
-//                }
-//            }
-
-
-//            var material = legendaryFarming.Where(x => x.Key == "shards" || x.Key == "fragments" || x.Key == "motes").ToDictionary(x => x.Key, x => x.Value);
-//            var junk = legendaryFarming.Where(x => x.Key != "shards" && x.Key != "fragments" && x.Key != "motes").OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
-
-//            material = material.OrderByDescending(x => x.Value).ThenBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
-
-//            foreach (var item in material)
-//            {
-//                Console.WriteLine(item.Key + ": " + item.Value);
-//            }
-//            foreach (var item in junk)
-//            {
-//                Console.WriteLine(item.Key + ": " + item.Value);
-//            }
-//        }
-//    }
-//}
-
-/////////////////////////////--------------------------------------------------------/////////////////////////////
-
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-
-//namespace _03.LegendaryFarming
-//{
-//    internal class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            var junks = new Dictionary<string, int>();
-//            var dictionary = new Dictionary<string, int>();
-//            dictionary["fragments"] = 0;
-//            dictionary["motes"] = 0;
-//            dictionary["shards"] = 0;
-//            bool isThereAWinner = false;
-
-//            while (!isThereAWinner)
-//            {
-//                var tokens = Console.ReadLine().Split();
-//                for (int i = 0; i < tokens.Length; i += 2)
-//                {
-//                    string type = tokens[i + 1].ToLower();
-//                    int quantity = int.Parse(tokens[i]);
-//                    if (dictionary.ContainsKey(type)) dictionary[type] += quantity;
-//                    else dictionary.Add(type, quantity);
-//                    bool validName = type == "shards" || type == "fragments" || type == "motes";
-//                    if (dictionary[type] >=250 && validName)
-//                    {
-//                        isThereAWinner = true;
-//                        switch (type)
-//                        {
-//                            case "shards":
-//                                Console.WriteLine("Shadowmourne obtained!");
-//                                break;
-//                            case "motes":
-//                                Console.WriteLine("Dragonwrath obtained!");
-//                                break;
-//                            case "fragments":
-//                                Console.WriteLine("Valanyr obtained!");
-//                                break;
-//                            default:
-//                                break;
-//                        }
-//                        dictionary[type] -= 250;
-//                        break;
-//                    }
-//                }
-//            }
-
-//            var material = dictionary.Where(x => x.Key == "shards" || x.Key == "fragments" || x.Key == "motes").ToDictionary(x => x.Key, x => x.Value);
-//            var junk = dictionary.Where(x => x.Key != "shards" && x.Key != "fragments" && x.Key != "motes").OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
-
-//            material = material.OrderByDescending(x => x.Value).ThenBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
-
-//            foreach (var item in material)
-//            {
-//                Console.WriteLine(item.Key + ": " + item.Value);
-//            }
-//            foreach (var item in junk)
-//            {
-//                Console.WriteLine(item.Key + ": " + item.Value);
-//            }
-//        }
-//    }
-//}
-using System;
+﻿using System;
 using System.Linq;
 using System.Collections.Generic;
 
@@ -147,78 +8,59 @@ namespace _03LegendaryFarming
     {
         static void Main(string[] args)
         {
-            var keyElements = new Dictionary<string, int>();
+            var dictionary = new Dictionary<string, int>();
+            dictionary["shards"] = 0;
+            dictionary["motes"] = 0;
+            dictionary["fragments"] = 0;
+            var junks = new Dictionary<string, int>();
+            bool isThereAWinner = false;
 
-            keyElements["fragments"] = 0;
-            keyElements["motes"] = 0;
-            keyElements["shards"] = 0;
-
-            var junkElements = new Dictionary<string, int>();
-
-            bool haveWinner = false;
-
-            while (haveWinner != true)
+            while (isThereAWinner != true)
             {
-                var tokens = Console.ReadLine()
-                    .ToLower()
-                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
-                    .ToArray();
+                var tokens = Console.ReadLine().ToLower().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
 
                 for (int i = 0; i < tokens.Length; i += 2)
                 {
                     string type = tokens[i + 1];
                     int quantity = int.Parse(tokens[i]);
 
-                    if (keyElements.ContainsKey(type))
+                    if (dictionary.ContainsKey(type))
                     {
-                        keyElements[type] += quantity;
+                        dictionary[type] += quantity;
 
-                        if (keyElements["motes"] >= 250)
+                        if (dictionary["motes"] >= 250)
                         {
                             Console.WriteLine("Dragonwrath obtained!");
-                            keyElements["motes"] -= 250;
-                            haveWinner = true;
+                            dictionary["motes"] -= 250;
+                            isThereAWinner = true;
                             break;
                         }
-                        else if (keyElements["fragments"] >= 250)
+                        else if (dictionary["fragments"] >= 250)
                         {
                             Console.WriteLine("Valanyr obtained!");
-                            keyElements["fragments"] -= 250;
-                            haveWinner = true;
+                            dictionary["fragments"] -= 250;
+                            isThereAWinner = true;
                             break;
                         }
-                        else if (keyElements["shards"] >= 250)
+                        else if (dictionary["shards"] >= 250)
                         {
                             Console.WriteLine("Shadowmourne obtained!");
-                            keyElements["shards"] -= 250;
-                            haveWinner = true;
+                            dictionary["shards"] -= 250;
+                            isThereAWinner = true;
                             break;
                         }
                     }
                     else
                     {
-                        if (!junkElements.ContainsKey(type))
-                        {
-                            junkElements[type] = quantity;
-                        }
-                        else
-                        {
-                            junkElements[type] += quantity;
-                        }
+                        if (!junks.ContainsKey(type)) junks[type] = quantity;
+                        else junks[type] += quantity;
                     }
                 }
-
             }
-
-            foreach (var kvp in keyElements.OrderByDescending(x => x.Value))
-            {
-                Console.WriteLine($"{kvp.Key}: {kvp.Value}");
-            }
-
-            foreach (var kvp in junkElements)
-            {
-                Console.WriteLine($"{kvp.Key}: {kvp.Value}");
-            }
+            foreach (var item in dictionary.Where(kvp => kvp.Value == 0).ToList()) dictionary.Remove(item.Key);
+            foreach (var item in junks.Where(kvp => kvp.Value == 0).ToList()) junks.Remove(item.Key);
+            foreach (var kvp in dictionary) Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+            foreach (var kvp in junks) Console.WriteLine($"{kvp.Key}: {kvp.Value}");
         }
     }
 }
