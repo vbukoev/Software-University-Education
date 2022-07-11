fdaf073dbdb17c34f47edcf534354b5d22d64d11
Vasil Bukoev <vbukoev@gmail.com>
2022-07-11 18:27:16 +0300

    1st task

--- CSharp-Technology-FUNDAMENTALS/Text Processing - Exercise/Text Processing - Exercise/01. Valid Usernames/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/Text Processing - Exercise/Text Processing - Exercise/01. Valid Usernames/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _01._Valid_Usernames
 {
@@ -6,7 +7,22 @@ namespace _01._Valid_Usernames
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var words = Console.ReadLine().Split(", ").ToArray();
+            for (int i = 0; i < words.Length; i++)
+            {
+                string word = words[i];
+                bool isValid = false;
+                if (word.Length >= 3 && word.Length <= 16)
+                {
+                    for (int j = 0; j < word.Length; j++)
+                    {
+                        char currChar = word[j];
+                        if (char.IsLetterOrDigit(currChar) || currChar == '-' || currChar == '_') isValid = true;
+                        else break;                        
+                    }
+                }
+                if (isValid) Console.WriteLine(word);
+            }
         }
     }
 }
