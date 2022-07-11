85f0363a9b3e8648c87de7f99cfdc78857e8df23
Vasil Bukoev <vbukoev@gmail.com>
2022-07-11 18:47:28 +0300

    3rd task 100/100

--- CSharp-Technology-FUNDAMENTALS/Text Processing - Exercise/Text Processing - Exercise/03.ExtractFile/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/Text Processing - Exercise/Text Processing - Exercise/03.ExtractFile/Program.cs
@@ -6,7 +6,14 @@ namespace _03.ExtractFile
     {
         static void Main(string[] args)
         {
-            
+            string pathToAFile = Console.ReadLine();
+            int startIndex = pathToAFile.LastIndexOf('\\') + 1;
+            string file = pathToAFile.Substring(startIndex);
+            int startIndexExtension = file.IndexOf('.') + 1;
+            string fileNAme = file.Substring(0, startIndexExtension - 1);
+            string fileExtension = file.Substring(startIndexExtension);
+            Console.WriteLine($"File name: {fileNAme}");
+            Console.WriteLine($"File extension: {fileExtension}");
         }
     }
 }
