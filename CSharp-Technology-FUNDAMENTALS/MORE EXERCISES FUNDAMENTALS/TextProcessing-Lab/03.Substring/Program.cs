8ba4d4483e40f8f32c115cdde07908c5c74624ce
Vasil Bukoev <vbukoev@gmail.com>
2022-07-13 19:24:59 +0300

    3

--- CSharp-Technology-FUNDAMENTALS/MORE EXERCISES FUNDAMENTALS/TextProcessing-Lab/03.Substring/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/MORE EXERCISES FUNDAMENTALS/TextProcessing-Lab/03.Substring/Program.cs
@@ -6,7 +6,15 @@ namespace _03.Substring
     {
         static void Main(string[] args)
         {
-            
+            string subStringToRemove = Console.ReadLine();
+            string wholeString = Console.ReadLine();
+            while (wholeString.Contains(subStringToRemove))
+            {
+                int startIndex = wholeString.IndexOf(subStringToRemove);
+                wholeString = wholeString.Remove(startIndex, subStringToRemove.Length);
+                
+            }
+            Console.WriteLine(wholeString);
         }
     }
 }
