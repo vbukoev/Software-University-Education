987614411a136556461c87994555364868bc2b2d
Vasil Bukoev <vbukoev@gmail.com>
2022-07-13 20:01:34 +0300

    Update Program.cs

--- CSharp-Technology-FUNDAMENTALS/MORE EXERCISES FUNDAMENTALS/TextProcessing-Lab/04.TextFilter/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/MORE EXERCISES FUNDAMENTALS/TextProcessing-Lab/04.TextFilter/Program.cs
@@ -6,7 +6,14 @@ namespace _04.TextFilter
     {
         static void Main(string[] args)
         {
-            
+            string[] banWords = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+            foreach (var bannedWord in banWords)
+            {
+                string replaced = new string('*', bannedWord.Length);
+                text = text.Replace(bannedWord, replaced);
+            }
+            Console.WriteLine(text);
         }
     }
 }
