a192fb54fab6246c20620bfd0597edb949f45a3d
Vasil Bukoev <vbukoev@gmail.com>
2022-07-13 20:12:04 +0300

    Update Program.cs

--- CSharp-Technology-FUNDAMENTALS/MORE EXERCISES FUNDAMENTALS/TextProcessing-Lab/05.Digits,LettersAndOther/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/MORE EXERCISES FUNDAMENTALS/TextProcessing-Lab/05.Digits,LettersAndOther/Program.cs
@@ -6,7 +6,20 @@ namespace _05.Digits_LettersAndOther
     {
         static void Main(string[] args)
         {
-            
+            string text = Console.ReadLine();
+            string digits = "";
+            string letters = "";
+            string others = "";
+            foreach (var item in text)
+            {
+                if (char.IsLetter(item)) letters += item;
+                else if (char.IsDigit(item)) digits += item;
+                else others += item;
+
+            }
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(others);
         }
     }
 }
