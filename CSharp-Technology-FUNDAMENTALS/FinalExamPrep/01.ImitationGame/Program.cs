1758168ca40baa552dd0153a2fc991e71d3c69e8
Vasil Bukoev <vbukoev@gmail.com>
2022-07-14 15:22:32 +0300

    Update Program.cs

--- CSharp-Technology-FUNDAMENTALS/FinalExamPrep/01.ImitationGame/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/FinalExamPrep/01.ImitationGame/Program.cs
@@ -30,7 +30,6 @@ namespace _01.ImitationGame
             }
             Console.WriteLine($"The decrypted message is: {message}");
         }
-
         static string Move(string message, string[] tokens)
         {
             int length = int.Parse(tokens[1]);
@@ -53,7 +52,5 @@ namespace _01.ImitationGame
             message = message.Replace(substring, replacemnet);
             return message;
         }
-
-
     }
 }
