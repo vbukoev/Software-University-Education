ee7b8f101212bc8e4af7ad6dfe70ea9b8065e016
Vasil Bukoev <vbukoev@gmail.com>
2022-07-14 15:25:28 +0300

    Update Program.cs

--- CSharp-Technology-FUNDAMENTALS/FinalExamPrep/01.ImitationGame/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/FinalExamPrep/01.ImitationGame/Program.cs
@@ -1,4 +1,48 @@
-﻿using System;
+﻿//using System;
+
+//namespace _01.ImitationGame
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            string message = Console.ReadLine();
+//            while (true)
+//            {
+//                string cmd= Console.ReadLine();
+//                if (cmd == "Decode") break;
+//                string[] tokens = cmd.Split('|');
+//                string action = tokens[0];
+//                switch (action)
+//                {
+//                    case "Move":
+//                        int length = int.Parse(tokens[1]);
+//                        string smthToMove = message.Substring(0, length);
+//                        message = message.Remove(0, length);
+//                        message += smthToMove;
+//                        break;
+//                    case "Insert":
+//                        int index = int.Parse(tokens[1]);
+//                        string value = tokens[2];
+//                        message = message.Insert(index, value);
+//                        break;
+//                    case "ChangeAll":
+//                        string substring = tokens[1];
+//                        string replacemnet = tokens[2];
+//                        message = message.Replace(substring, replacemnet);
+//                        break;
+//                    default:
+//                        break;
+//                }
+//            }
+//            Console.WriteLine($"The decrypted message is: {message}");
+//        }
+//    }
+//}
+
+                              //SECOND SOLUTION WITH METHODS//
+///////////////////////////////////////////////////////////////////////////////////////////////
+using System;
 
 namespace _01.ImitationGame
 {
@@ -9,7 +53,7 @@ namespace _01.ImitationGame
             string message = Console.ReadLine();
             while (true)
             {
-                string cmd= Console.ReadLine();
+                string cmd = Console.ReadLine();
                 if (cmd == "Decode") break;
                 string[] tokens = cmd.Split('|');
                 string action = tokens[0];
@@ -30,26 +74,29 @@ namespace _01.ImitationGame
             }
             Console.WriteLine($"The decrypted message is: {message}");
         }
-        static string Move(string message, string[] tokens)
+
+        private static string ChangeAll(string message, string[] tokens)
         {
-            int length = int.Parse(tokens[1]);
-            string smthToMove = message.Substring(0, length);
-            message = message.Remove(0, length);
-            message += smthToMove;
+            string substring = tokens[1];
+            string replacemnet = tokens[2];
+            message = message.Replace(substring, replacemnet);
             return message;
         }
-        static string Insert(string message, string[] tokens)
+
+        private static string Insert(string message, string[] tokens)
         {
             int index = int.Parse(tokens[1]);
             string value = tokens[2];
             message = message.Insert(index, value);
             return message;
         }
-        static string ChangeAll(string message, string[] tokens)
+
+        private static string Move(string message, string[] tokens)
         {
-            string substring = tokens[1];
-            string replacemnet = tokens[2];
-            message = message.Replace(substring, replacemnet);
+            int length = int.Parse(tokens[1]);
+            string smthToMove = message.Substring(0, length);
+            message = message.Remove(0, length);
+            message += smthToMove;
             return message;
         }
     }
