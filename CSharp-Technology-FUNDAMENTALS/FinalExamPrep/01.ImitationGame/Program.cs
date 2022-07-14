4475ca6ead21aaeb95f8786eaa86f089a4a0a2e5
Vasil Bukoev <vbukoev@gmail.com>
2022-07-14 15:18:22 +0300

    Update Program.cs

--- CSharp-Technology-FUNDAMENTALS/FinalExamPrep/01.ImitationGame/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/FinalExamPrep/01.ImitationGame/Program.cs
@@ -11,31 +11,49 @@ namespace _01.ImitationGame
             {
                 string cmd= Console.ReadLine();
                 if (cmd == "Decode") break;
-                string[] tokens = cmd.Split("|");
+                string[] tokens = cmd.Split('|');
                 string action = tokens[0];
                 switch (action)
                 {
                     case "Move":
-                        int length = int.Parse(tokens[1]);
-                        string smthToMove = message.Substring(0, length);
-                        message = message.Remove(0, length);
-                        message += smthToMove;
+                        message = Move(message, tokens);
                         break;
                     case "Insert":
-                        int index = int.Parse(tokens[1]);
-                        string value = tokens[2];
-                        message = message.Insert(0, value);
+                        message = Insert(message, tokens);
                         break;
                     case "ChangeAll":
-                        string substring = tokens[1];
-                        string replacemnet = tokens[2];
-                        message = message.Replace(substring, replacemnet);
+                        message = ChangeAll(message, tokens);
                         break;
                     default:
                         break;
                 }
             }
             Console.WriteLine($"The decrypted message is: {message}");
+        }
+
+        static string ChangeAll(string message, string[] tokens)
+        {
+            string substring = tokens[1];
+            string replacemnet = tokens[2];
+            message = message.Replace(substring, replacemnet);
+            return message;
+        }
+
+        static string Insert(string message, string[] tokens)
+        {
+            int index = int.Parse(tokens[1]);
+            string value = tokens[2];
+            message = message.Insert(0, value);
+            return message;
+        }
+
+        static string Move(string message, string[] tokens)
+        {
+            int length = int.Parse(tokens[1]);
+            string smthToMove = message.Substring(0, length);
+            message = message.Remove(0, length);
+            message += smthToMove;
+            return message;
         }
     }
 }
