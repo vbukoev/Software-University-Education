1b708afde80431c6ea806418c1c45a97dfdb30a9
Vasil Bukoev <vbukoev@gmail.com>
2022-07-20 20:12:29 +0300

    2nd task 100/100 submition

--- CSharp-Technology-FUNDAMENTALS/RegularExpressions-Lab/Regular Expressions - Lab/02.MatchPhoneNumber/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/RegularExpressions-Lab/Regular Expressions - Lab/02.MatchPhoneNumber/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace _02.MatchPhoneNumber
 {
@@ -6,7 +11,11 @@ namespace _02.MatchPhoneNumber
     {
         static void Main(string[] args)
         {
-                      
+            var phones = Console.ReadLine();
+            var regex = @"\+359([ -])2\1\d{3}\1\d{4}\b";            
+            var phoneMatch = Regex.Matches(phones, regex);
+            var matched = phoneMatch.Cast<Match>().Select(a => a.Value.Trim()).ToArray();
+            Console.WriteLine(String.Join(", ", matched));
         }
     }
 }
