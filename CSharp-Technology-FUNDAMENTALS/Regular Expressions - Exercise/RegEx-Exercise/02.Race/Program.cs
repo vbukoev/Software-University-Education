8f30538a6063a77e5bd6aeef1ee52269a2a7860b
Vasil Bukoev <vbukoev@gmail.com>
2022-07-22 19:40:32 +0300

    Update Program.cs

--- CSharp-Technology-FUNDAMENTALS/Regular Expressions - Exercise/RegEx-Exercise/02.Race/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/Regular Expressions - Exercise/RegEx-Exercise/02.Race/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace _02.Race
 {
@@ -6,7 +9,22 @@ namespace _02.Race
     {
         static void Main(string[] args)
         {
-            
+            string patternNamesOfPeople = new Regex(@"(?<name>[A-Za-z]+)(?<numbers>\d+)");
+            string patternDigits = new Regex(@"?<digits>\d+");
+            int sumDigits = 0;
+            var people = new Dictionary<string, int>();
+            var names = Console.ReadLine().Split(", ").ToList();
+            string input = Console.ReadLine();
+            while (input !=  "end of race")
+            {
+                MatchCollection matchedNames = patternNamesOfPeople.Matches(input);
+                MatchCollection matchedDigits = Regex.Matches(input, patternDigits);
+                string currName = string.Join("", matchedNames);
+                string currDigits = string.Join("", matchedDigits);
+                sumDigits = 0;
+                input = Console.ReadLine();
+            }
+
         }
     }
 }
