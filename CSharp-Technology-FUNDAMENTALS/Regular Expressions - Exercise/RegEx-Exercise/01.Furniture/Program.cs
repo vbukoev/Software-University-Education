578e9e3348774153d40ea9fd05247bbc7e9bae4a
Vasil Bukoev <vbukoev@gmail.com>
2022-07-22 19:06:50 +0300

    Update Program.cs

--- CSharp-Technology-FUNDAMENTALS/Regular Expressions - Exercise/RegEx-Exercise/01.Furniture/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/Regular Expressions - Exercise/RegEx-Exercise/01.Furniture/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace _01.Furniture
 {
@@ -6,7 +8,26 @@ namespace _01.Furniture
     {
         static void Main(string[] args)
         {
-            
+            string input = Console.ReadLine();
+            string regex = @">>(?<name>[A-Za-z\s]+)<<(?<price>\d+(.\d+)?)!(?<quantity>\d+)";
+            var list = new List<string>();
+            double total = 0;
+            while (input != "Purchase")
+            {
+                Match matches = Regex.Match(input, regex, RegexOptions.IgnoreCase);
+                if (matches.Success)
+                {
+                    var name = matches.Groups["name"].Value;
+                    var price = double.Parse(matches.Groups["price"].Value);
+                    var quantity = int.Parse(matches.Groups["quantity"].Value);
+                    list.Add(name);
+                    total += price*quantity;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine("Bought furniture:");
+            list.ForEach(Console.WriteLine);
+            Console.WriteLine($"Total money spend: {total:f2}");
         }
     }
 }
