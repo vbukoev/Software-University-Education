ed277f4deae7fcd318a8af67eb25a0d252167990
Vasil Bukoev <vbukoev@gmail.com>
2022-07-22 17:15:24 +0300

    update program.cs

---
+++ CSharp-Technology-FUNDAMENTALS/Regular Expressions - Exercise/RegEx-Exercise/03.SoftUniBarIncome/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03.SoftUniBarIncome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
