3e0f008ce22ebca4d52279b093bed27d72e9dac9
Vasil Bukoev <vbukoev@gmail.com>
2022-07-22 21:21:25 +0300

    6th task 100/100

--- CSharp-Technology-FUNDAMENTALS/Regular Expressions - Exercise/RegEx-Exercise/06.ExtractEmails/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/Regular Expressions - Exercise/RegEx-Exercise/06.ExtractEmails/Program.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿    using System;
+    using System.Linq;
+    using System.Text.RegularExpressions;
 
-namespace _06.ExtractEmails
-{
-    internal class Program
+    namespace _06.ExtractEmails
     {
-        static void Main(string[] args)
+        internal class Program
         {
-            
+            static void Main(string[] args)
+            {
+                string pattern = @"(^|\s)[A-Za-z0-9][\w*\-\.]*[A-Za-z0-9]@[A-Za-z]+([.-][A-Za-z]+)+\b";
+                string input = Console.ReadLine();
+                MatchCollection matches= Regex.Matches(input, pattern);
+                matches.ToList().ForEach(Console.WriteLine);
+            }
         }
     }
-}
