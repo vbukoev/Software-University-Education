0bcddb491d98a2515feeac9630f49aed557baf68
Vasil Bukoev <vbukoev@gmail.com>
2022-07-22 20:28:20 +0300

    update program.cs

--- CSharp-Technology-FUNDAMENTALS/Regular Expressions - Exercise/RegEx-Exercise/02.Race/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/Regular Expressions - Exercise/RegEx-Exercise/02.Race/Program.cs
@@ -9,22 +9,54 @@ namespace _02.Race
     {
         static void Main(string[] args)
         {
-            string patternNamesOfPeople = new Regex(@"(?<name>[A-Za-z]+)(?<numbers>\d+)");
-            string patternDigits = new Regex(@"?<digits>\d+");
+            Regex patternNamesOfPeople = new Regex(@"(?<name>[A-Za-z]+)");
+            string patternDigits = @"(?<numbers>\d+)";
             int sumDigits = 0;
             var people = new Dictionary<string, int>();
             var names = Console.ReadLine().Split(", ").ToList();
             string input = Console.ReadLine();
-            while (input !=  "end of race")
+            while (input != "end of race")
             {
                 MatchCollection matchedNames = patternNamesOfPeople.Matches(input);
                 MatchCollection matchedDigits = Regex.Matches(input, patternDigits);
                 string currName = string.Join("", matchedNames);
                 string currDigits = string.Join("", matchedDigits);
                 sumDigits = 0;
+                for (int i = 0; i < currDigits.Length; i++) 
+                { 
+                    sumDigits += int.Parse(currDigits[i].ToString()); 
+                }
+                if (names.Contains(currName))
+                {
+                    if (!people.ContainsKey(currName))  
+                    {
+                        people.Add(currName, sumDigits);
+                    }
+                    else
+                    {
+                        //updating the curr km of the run
+                        people[currName]+=sumDigits;
+                    }
+                }                
                 input = Console.ReadLine();
             }
-
+            //finding the winners of the run
+            var winners = people.OrderByDescending(x => x.Value).Take(3);
+            var first = winners.Take(1);
+            var second = winners.OrderByDescending(x=>x.Value).Take(2).OrderBy(x=>x.Value).Take(1);
+            var third = winners.OrderBy(x => x.Value).Take(1);
+            foreach (var firstName in first)
+            {
+                Console.WriteLine($"1st place: {firstName.Key}"); // 1st place
+            }
+            foreach (var secondName in second)
+            {
+                Console.WriteLine($"2nd place: {secondName.Key}"); // 2nd place
+            }
+            foreach (var thirdName in third)
+            {
+                Console.WriteLine($"3rd place: {thirdName.Key}"); // 3rd place
+            }
         }
     }
 }
--- CSharp-Technology-FUNDAMENTALS/Regular Expressions - Exercise/RegEx-Exercise/03.SoftUniBarIncome/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/Regular Expressions - Exercise/RegEx-Exercise/03.SoftUniBarIncome/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace _03.SoftUniBarIncome
 {
@@ -6,7 +7,32 @@ namespace _03.SoftUniBarIncome
     {
         static void Main(string[] args)
         {
-            
+            string pattern = @"^[^|$%.]*%(?<customer>[A-Z][a-z]+)%[^|$%.]*<(?<product>\w+)>[^|$%.]*\|(?<qtity>\d+)\|[^|$%.]*?(?<price>\d+\.?\d*)[^|$%.]*\$";
+
+            Regex regex = new Regex(pattern);
+            double income = 0;
+
+            while (true)
+            {
+                string line = Console.ReadLine();          //customer, product, count and a price:
+
+                if (line == "end of shift") break;              
+
+                Match match = regex.Match(line);
+
+                if (match.Success == false) continue; 
+
+                string customer = match.Groups["customer"].Value;
+                string product = match.Groups["product"].Value;
+                int quantity = int.Parse(match.Groups["qtity"].Value);
+                double price = (double.Parse)(match.Groups["price"].Value);
+
+                double totalCustomerIncome = price * quantity;
+                income += totalCustomerIncome;
+
+                Console.WriteLine($"{customer}: {product} - {totalCustomerIncome:f2}");
+            }
+            Console.WriteLine($"Total income: {income:f2}");
         }
     }
 }
