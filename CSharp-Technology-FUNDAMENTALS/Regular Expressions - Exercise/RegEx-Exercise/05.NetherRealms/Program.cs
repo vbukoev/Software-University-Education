673f5974bbbc24b395797b7d64bd48f13d0e9315
Vasil Bukoev <vbukoev@gmail.com>
2022-07-22 21:07:50 +0300

    5th task submission

--- CSharp-Technology-FUNDAMENTALS/Regular Expressions - Exercise/RegEx-Exercise/05.NetherRealms/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/Regular Expressions - Exercise/RegEx-Exercise/05.NetherRealms/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace _05.NetherRealms
 {
@@ -6,7 +8,45 @@ namespace _05.NetherRealms
     {
         static void Main(string[] args)
         {
-            
+            string input = Console.ReadLine();
+            string healthPattern = @"[^\+\-\*\/\.,0-9]";
+            string damagePattern = @"-?\d+\.?\d*";
+            string multiplyOrDividePattern = @"[\*\/]";
+            string splitPattern = @"[,\s]+";
+            string[] demons = Regex.Split(input, splitPattern).OrderBy(x=>x).ToArray();
+
+            for (int i = 0; i < demons.Length; i++)
+            {
+                string curDemon = demons[i];
+                var healthMatched = Regex.Matches(curDemon, healthPattern);
+                var health = 0;
+                foreach (Match match in healthMatched)
+                {
+                    char curChr = char.Parse(match.ToString());
+                    health += curChr;
+                }
+                double damage = 0;
+                var damageMatched = Regex.Matches(curDemon, damagePattern);
+                foreach (Match match in damageMatched)
+                {
+                    double curDamage = double.Parse(match.ToString());
+                    damage += curDamage;
+                }
+                var multiplyOrDivide = Regex.Matches(curDemon, multiplyOrDividePattern);
+                foreach (Match match in multiplyOrDivide)
+                {
+                    char curOpperator = char.Parse(match.ToString());
+                    if (curOpperator == '*')
+                    {
+                        damage *= 2;
+                    }
+                    else
+                    {
+                        damage /= 2;
+                    }
+                }
+                Console.WriteLine($"{curDemon} - {health} health, {damage:f2} damage");
+            }
         }
     }
 }
