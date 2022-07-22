ff549e38a359100df52a10ed30847141afd75616
Vasil Bukoev <vbukoev@gmail.com>
2022-07-22 21:37:42 +0300

    Update Program.cs

--- CSharp-Technology-FUNDAMENTALS/Regular Expressions - Exercise/RegEx-Exercise/04.StarEnigma/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/Regular Expressions - Exercise/RegEx-Exercise/04.StarEnigma/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace _04.StarEnigma
@@ -9,7 +10,47 @@ namespace _04.StarEnigma
     {
         static void Main(string[] args)
         {
-           
+            int n = int.Parse(Console.ReadLine());
+            List<string> attackedNames = new List<string>(); 
+            List<string> destroyedNames = new List<string>();
+            int attacked = 0;
+            int destroyed = 0;
+            for (int i = 0; i < n; i++)
+            {
+                string message = Console.ReadLine();
+                Regex regex = new Regex(@"[STARstar]");
+                MatchCollection matches = regex.Matches(message);
+                StringBuilder decoded = new StringBuilder();
+                int sum = 0;
+                foreach (Match match in matches) sum++;
+                foreach (var @char in message) decoded.Append((char)@char - sum);
+                Regex regex1 = new Regex(@"@(?<planet>[A-Za-z]+)[^\@,!:>]*[\-]*:(?<population>\d+)[^\@,!:>]*[\-]*!(?<attakType>[AD])![^\@,!:>]*[\-]*->(?<soldiers>\d+)");
+                MatchCollection matches1 = regex1.Matches(decoded.ToString());
+                foreach (Match match in matches1)
+                {
+                    string name = match.Groups["planet"].Value;
+                    int popultion = int.Parse(match.Groups["population"].Value);
+                    char attackType = char.Parse(match.Groups["attakType"].Value);
+                    int soldiers = int.Parse(match.Groups["soldiers"].Value);
+                    if (attackType == 'A')
+                    {
+                        attacked++;
+                        attackedNames.Add(name);
+                    }
+                    if (attackType == 'D')
+                    {
+                        destroyed++;
+                        destroyedNames.Add(name);
+                    }
+                }                
+            }
+            Console.WriteLine($"Attacked planets: {attacked}");
+            foreach (var nameOfTheAttack in attackedNames.OrderBy(x => x)) Console.WriteLine($"-> {nameOfTheAttack}");
+            Console.WriteLine($"Destroyed planets: {destroyed}");
+            foreach (var nameOfTheDestoy in destroyedNames.OrderBy(x => x)) Console.WriteLine($"-> {nameOfTheDestoy}");
+            {
+
+            }
         }
     }
 }
