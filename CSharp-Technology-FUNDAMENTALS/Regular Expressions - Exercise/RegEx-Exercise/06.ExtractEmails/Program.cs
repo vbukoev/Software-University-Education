a9abc10927db587bc7f58ffc71fc82c546294116
Vasil Bukoev <vbukoev@gmail.com>
2022-07-25 07:52:19 +0300

    Update Program.cs

--- CSharp-Technology-FUNDAMENTALS/Regular Expressions - Exercise/RegEx-Exercise/06.ExtractEmails/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/Regular Expressions - Exercise/RegEx-Exercise/06.ExtractEmails/Program.cs
@@ -10,7 +10,9 @@
             {
                 string pattern = @"(^|\s)[A-Za-z0-9][\w*\-\.]*[A-Za-z0-9]@[A-Za-z]+([.-][A-Za-z]+)+\b";
                 string input = Console.ReadLine();
+
                 MatchCollection matches= Regex.Matches(input, pattern);
+
                 matches.ToList().ForEach(Console.WriteLine);
             }
         }
