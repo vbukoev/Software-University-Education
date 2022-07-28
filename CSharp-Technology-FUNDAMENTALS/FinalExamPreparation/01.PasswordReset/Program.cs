21b0548cc95a559555e14910958006b2dd818547
Vasil Bukoev <vbukoev@gmail.com>
2022-07-28 17:05:10 +0300

    password reset 100/100

--- CSharp-Technology-FUNDAMENTALS/FinalExamPreparation/01.PasswordReset/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/FinalExamPreparation/01.PasswordReset/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace _01.PasswordReset
 {
@@ -6,7 +7,43 @@ namespace _01.PasswordReset
     {
         static void Main(string[] args)
         {
-            
+            var input = Console.ReadLine();
+            var sb = new StringBuilder();
+            var cmd = "";
+            while (true)
+            {
+                cmd = Console.ReadLine();
+                if (cmd == "Done") break;
+                var command = cmd.Split();
+                var action = command[0];
+                switch (action)
+                {
+                    case "TakeOdd":
+                        for (int i = 1; i < input.Length; i+=2) sb.Append(input[i]); //index "i" has to be with step of 2  
+                        input= sb.ToString();
+                        Console.WriteLine(input);
+                        break;
+                    case "Cut":
+                        var index = int.Parse(command[1]);
+                        var length = int.Parse(command[2]);
+                        input = input.Remove(index, length);
+                        Console.WriteLine(input);
+                        break;
+                    case "Substitute":
+                        var oldC = command[1];
+                        var newC = command[2];
+                        if (!input.Contains(oldC)) Console.WriteLine("Nothing to replace!");
+                        else
+                        {
+                            input = input.Replace(oldC, newC);
+                            Console.WriteLine(input);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine($"Your password is: {input}");
         }
     }
 }
