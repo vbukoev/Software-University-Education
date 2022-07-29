e9da9dbbec6c711f9862e70819e4965acef06223
Vasil Bukoev <vbukoev@gmail.com>
2022-07-29 18:07:20 +0300

    Update Program.cs

--- CSharp-Technology-FUNDAMENTALS/FinalExamPreparation/02.DestinationMapper/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/FinalExamPreparation/02.DestinationMapper/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace _02.DestinationMapper
 {
@@ -6,7 +8,20 @@ namespace _02.DestinationMapper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string newText = Console.ReadLine();
+            string pattern = @"(=|\/)(?<locationm>[A-Z][A-Za-z]{2,})\1";
+            int count = 0;
+            Regex regex = new Regex(pattern, RegexOptions.Compiled);
+            List<string> sequence = new List<string>();
+            foreach (Match item in regex.Matches(newText))
+            {
+                count += item.Groups[2].Length;
+                string currDest = item.Groups[2].Value;
+                sequence.Add(currDest);
+            }
+            string dests = string.Join(", ", sequence);
+            Console.WriteLine($"Destinations: {dests}");
+            Console.WriteLine($"Travel Points: {count}");
         }
     }
 }
