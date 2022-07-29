adbe7752b53fa80a462c41cb43d7689ac95f7ec2
Vasil Bukoev <vbukoev@gmail.com>
2022-07-29 19:31:39 +0300

    renaming variables :+1:

--- CSharp-Technology-FUNDAMENTALS/FinalExamPreparation/02.EmojiDetector/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/FinalExamPreparation/02.EmojiDetector/Program.cs
@@ -14,25 +14,25 @@ namespace _02.EmojiDetector
             BigInteger threshold = 1;           
             MatchCollection nums = numOfPattern.Matches(input);
             Regex pattern = new Regex(@"(?<chars>[:]{2}|[*]{2})(?<nameOfTheEmoji>[A-Z]{1}[a-z]{2,})\1");
-            MatchCollection emoji = pattern.Matches(input);
-            List<string> coolEmojios = new List<string>();
+            MatchCollection allEmojis = pattern.Matches(input);
+            List<string> coolEmojis = new List<string>();
 
             foreach (Match match in nums)
             {
                 var num = int.Parse(match.Value);
                 threshold = threshold * num;
             }           
-            foreach (Match item in emoji)
+            foreach (Match item in allEmojis)
             {
                 BigInteger cntOfCoolness = 0;
                 var itemGroup = item.Groups["nameOfTheEmoji"].Value;
                 for (int i = 0; i < itemGroup.Length; i++)
                     cntOfCoolness = cntOfCoolness + (int)itemGroup[i];
-                if (cntOfCoolness >= threshold) coolEmojios.Add(item.Value);
+                if (cntOfCoolness >= threshold) coolEmojis.Add(item.Value);
             }
             Console.WriteLine($"Cool threshold: {threshold}");
-            Console.WriteLine($"{emoji.Count} emojis found in the text. The cool ones are:");
-            Console.WriteLine(string.Join("\n", coolEmojios));
+            Console.WriteLine($"{allEmojis.Count} emojis found in the text. The cool ones are:");
+            Console.WriteLine(string.Join("\n", coolEmojis));
         }
     }
 }
