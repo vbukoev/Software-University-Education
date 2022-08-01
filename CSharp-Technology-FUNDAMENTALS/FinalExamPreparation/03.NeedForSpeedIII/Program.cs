83164f7e4af367845155a145708d5f5d628362cd
Vasil Bukoev <vbukoev@gmail.com>
2022-08-01 17:33:57 +0300

    -

--- CSharp-Technology-FUNDAMENTALS/FinalExamPreparation/03.NeedForSpeedIII/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/FinalExamPreparation/03.NeedForSpeedIII/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _03.NeedForSpeedIII
 {
@@ -6,7 +7,50 @@ namespace _03.NeedForSpeedIII
     {
         static void Main(string[] args)
         {
-            
+            var cars = new List<Car>();
+            var cnt = int.Parse(Console.ReadLine());
+            for (int i = 0; i < cnt; i++)
+            {
+                var carProps = Console.ReadLine().Split("|", StringSplitOptions.RemoveEmptyEntries);
+                Car car = new Car
+                {
+                    Brand = carProps[0],
+                    Mileage = int.Parse(carProps[1]),
+                    Fuel = int.Parse(carProps[2])
+                };
+                cars.Add(car);
+
+                while (true)
+                {
+                    var cmd = Console.ReadLine();
+                    if (cmd == "Stop") break;
+                    var tokens = cmd.Split(" : ");
+                    var action = tokens[0];
+                    switch (action)
+                    {
+                        case "Drive":
+
+                            break;
+
+                        case "Refuel":
+
+                            break;
+
+                        case "Revert":
+
+                            break;
+
+                        default: 
+                            break;
+                    }
+                }
+            }
         }
+    }
+    public class Car
+    {
+        public string Brand { get; set; }
+        public int Mileage { get; set; }
+        public int Fuel { get; set; }
     }
 }
