2a0de115363f7c13a2c130055d51610dcfe9efbf
Vasil Bukoev <vbukoev@gmail.com>
2022-08-02 13:42:36 +0300

    adding new task to the sln

---
+++ CSharp-Technology-FUNDAMENTALS/FinalExamPreparation/03.PlantDiscovery/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03.PlantDiscovery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
