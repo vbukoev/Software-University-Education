91d9fdbcca38a7893e8a08142a7324e335494301
Vasil Bukoev <vbukoev@gmail.com>
2022-08-03 10:50:36 +0300

    adding comments to the task

--- CSharp-Technology-FUNDAMENTALS/FinalExamPreparation/02.MirrorWords/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/FinalExamPreparation/02.MirrorWords/Program.cs
@@ -8,32 +8,32 @@ namespace _02.MirrorWords
     {
         static void Main(string[] args)
         {
-            var mirrorWords = new Dictionary<string, string>();
-            var input = Console.ReadLine();
-            Regex pattern = new Regex(@"(?<getAllValidCharsBeforeTheWord>([@#]))(?<getTheWordWithAtLeast3CharsInIt>([A-Za-z]{3,}))\1{2}(?<getsTheReversedWordWithAtLeast3CharsInIt>([A-Za-z]){3,})\1");
-            MatchCollection matches = pattern.Matches(input);
+            var mirrorWords = new Dictionary<string, string>(); // dictionary 
+            var input = Console.ReadLine(); //input from the console
+            Regex pattern = new Regex(@"(?<getAllValidCharsBeforeTheWord>([@#]))(?<getTheWordWithAtLeast3CharsInIt>([A-Za-z]{3,}))\1{2}(?<getsTheReversedWordWithAtLeast3CharsInIt>([A-Za-z]){3,})\1"); //the regex pattern
+            MatchCollection matches = pattern.Matches(input); // matchCollection
             if (matches.Count > 0) Console.WriteLine($"{matches.Count} word pairs found!"); //Checks if there are matches and prints their cnt
-            else Console.WriteLine("No word pairs found!");
+            else Console.WriteLine("No word pairs found!"); // prints on the console there aren't any pairs
             foreach (Match match in matches)
             {
-                var word = match.Groups["getTheWordWithAtLeast3CharsInIt"].Value;
-                var mirror = match.Groups["getsTheReversedWordWithAtLeast3CharsInIt"].Value;
-                var reverse = mirror;
-                var reverseMirror = reverse.ToCharArray();
-                Array.Reverse(reverseMirror);
-                var mirrorWord = new string(reverseMirror);
-                if (word == mirrorWord) mirrorWords.Add(word, mirror);
+                var word = match.Groups["getTheWordWithAtLeast3CharsInIt"].Value; // gets the value from the group where the word is 
+                var mirror = match.Groups["getsTheReversedWordWithAtLeast3CharsInIt"].Value; // gets the value from the group where the reversed word is
+                var reverse = mirror; // string reverse gets the reversed word and then it is going to be a char Array
+                var reverseMirror = reverse.ToCharArray(); // the char array
+                Array.Reverse(reverseMirror); // reversing the char array 
+                var mirrorWord = new string(reverseMirror); // "mirrorWord" gets the value from the charArr and it gets it as a string 
+                if (word == mirrorWord) mirrorWords.Add(word, mirror);// checks if the word is equal to the value of the mirrorWord and if it is we add it to the Dictionary 
             }
-            if (mirrorWords.Count == 0) Console.WriteLine("No mirror words!");
+            if (mirrorWords.Count == 0) Console.WriteLine("No mirror words!"); // checks if there are mirrorWords and if there aren't we print on the console that there are no mirror words
             else
             {
-                var cnt = 1;
+                var cnt = 1; // int count which is equal to 1 so that we don't have to increment the count rigth in the begining of the foreach loop
                 Console.WriteLine("The mirror words are:");
-                foreach (var word in mirrorWords)
+                foreach (var word in mirrorWords) 
                 {                    
-                    if (cnt == mirrorWords.Count) Console.Write($"{word.Key} <=> {word.Value}");
-                    else Console.Write($"{word.Key} <=> {word.Value}, ");
-                    cnt++;
+                    if (cnt == mirrorWords.Count) Console.Write($"{word.Key} <=> {word.Value}"); // checks if the count is equal to the Dictionary count and if it is it prints the word Key with its value
+                    else Console.Write($"{word.Key} <=> {word.Value}, "); //if it is not it prints 
+                    cnt++; //count increments by 1
                 }
             }
         }
