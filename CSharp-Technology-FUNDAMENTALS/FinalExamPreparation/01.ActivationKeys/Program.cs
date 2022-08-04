64e67fcab95780832fab14394d8c09f87dbe1db0
Vasil Bukoev <vbukoev@gmail.com>
2022-08-04 15:01:34 +0300

    Update Program.cs

--- CSharp-Technology-FUNDAMENTALS/FinalExamPreparation/01.ActivationKeys/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/FinalExamPreparation/01.ActivationKeys/Program.cs
@@ -19,58 +19,52 @@ namespace _01.ActivationKeys
                 var action = splitted[0];
                 switch (action)
                 {
-                   case "Contains":
-                        {
-                            var substring = splitted[1];
-                            if (actKey.Contains(substring)) Console.WriteLine($"{actKey} contains {substring}");
-                            else Console.WriteLine("Substring not found!");
-                            break;
-                        }
+                   case "Contains": 
+                   var substring = splitted[1];
+                        if (actKey.Contains(substring)) Console.WriteLine($"{actKey} contains {substring}");
+                        else Console.WriteLine("Substring not found!");
+                        break;
                     case "Flip":
+                    var cases = splitted[1];
+                        var start = int.Parse(splitted[2]);
+                        var end = int.Parse(splitted[3]);
+                        var cnt = end - start;
+                        substring = actKey.Substring(start, cnt);
+                        var toChar= actKey.ToArray();
+                        switch (cases)
                         {
-                            var cases = splitted[1];
-                            var cnt = int.Parse(splitted[3]) - int.Parse(splitted[2]);
-                            var substring = actKey.Substring(int.Parse(splitted[2]), int.Parse(splitted[3]));
-                            var toChar = actKey.ToArray();
-                            switch (cases)
-                            {
-                                case "Upper":
-                                    {
-                                        substring = substring.ToUpper();
-                                        var index = 0;
-                                        for (int i = int.Parse(splitted[2]); i < int.Parse(splitted[3]); i++)
-                                        {
-                                            toChar[i] = substring[index];
-                                            index++;
-                                        }
-                                        actKey = String.Join("", toChar);
-                                        break;
-                                    }
-                                case "Lower":
-                                    {
-                                        substring = substring.ToLower();
-                                        var index = 0;
-                                        for (int i = int.Parse(splitted[2]); i < int.Parse(splitted[3]); i++)
-                                        {
-                                            toChar[i] = substring[index];
-                                            index++;
-                                        }
-                                        actKey = String.Join("", toChar);
-                                        break;
-                                    }
-                                default:
-                                    break;
-                            }
-                            Console.WriteLine(actKey);
-                            break;
+                            case "Upper":
+                                substring = substring.ToUpper();
+                                var index = 0;
+                                for (int i = start; i < end; i++)
+                                {                                    
+                                    toChar[i] = substring[index];
+                                    index++;
+                                }
+                                actKey = String.Join("", toChar);
+                                break;
+                            case "Lower":
+                                substring = substring.ToLower();
+                                index = 0;
+                                for (int i = start; i < end; i++)
+                                {                                    
+                                    toChar[i] = substring[index];
+                                    index++;
+                                }
+                                actKey = String.Join("", toChar);
+                                break;
+                            default:
+                                break;
                         }
+                        Console.WriteLine(actKey);
+                        break;
                     case "Slice":
-                        {
-                            var cnt = int.Parse(splitted[2]) - int.Parse(splitted[1]);
-                            actKey = actKey.Remove(int.Parse(splitted[1]), cnt);
-                            Console.WriteLine(actKey);
-                            break;
-                        }
+                        start = int.Parse(splitted[1]);
+                        end = int.Parse(splitted[2]);
+                        cnt = end - start;
+                        actKey = actKey.Remove(start, cnt);
+                        Console.WriteLine(actKey);
+                        break;
                     default:
                         break;
                 }
