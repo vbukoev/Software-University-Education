c1a10e598deab7a039860d24c459931db129ec01
Vasil Bukoev <vbukoev@gmail.com>
2022-08-04 15:58:03 +0300

    -

--- CSharp-Technology-FUNDAMENTALS/FinalExamPreparation/03.PlantDiscovery/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/FinalExamPreparation/03.PlantDiscovery/Program.cs
@@ -6,7 +6,8 @@ namespace _03.PlantDiscovery
     {
         static void Main(string[] args)
         {
-            
+            //  2_147_483_647 int
+            //  4_294_967_295 uint
         }
     }
 }
