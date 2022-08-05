5aabc471b64a0a19dced3adbf9bc75bee9e5d9c9
Vasil Bukoev <vbukoev@gmail.com>
2022-08-05 13:31:41 +0300

    -

--- CSharp-Technology-FUNDAMENTALS/FinalExamPreparation/03.PlantDiscovery/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/FinalExamPreparation/03.PlantDiscovery/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _03.PlantDiscovery
 {
@@ -6,7 +7,27 @@ namespace _03.PlantDiscovery
     {
         static void Main()
         {
+            int n = int.Parse(Console.ReadLine());
+            var info = new Dictionary<string, (int rarity, List<double> rating)>();
+            for (int i = 0; i < n; i++)
+            {
+                var cmd = Console.ReadLine().Split("<->");
+                var plant = cmd[0];
+                var rarity = int.Parse(cmd[1]);
+                if (info.ContainsKey(plant))
+                {
+                    var newRarity = info[plant].rarity + rarity;
+                    info[plant] = (newRarity, new List<double>());
+                    continue;
+                }
+                info.Add(plant, (rarity, new List<double>()));
+            }            
+            while (true)
+            {
+                var command = Console.ReadLine();
+                if (command == "Exhibition") break;
 
+            }
         }
     }
 }
