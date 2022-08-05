758d285af64603be85d21bb6e8ff15d11ad5975c
Vasil Bukoev <vbukoev@gmail.com>
2022-08-05 13:38:30 +0300

    adding two cases

--- CSharp-Technology-FUNDAMENTALS/FinalExamPreparation/03.PlantDiscovery/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/FinalExamPreparation/03.PlantDiscovery/Program.cs
@@ -9,6 +9,7 @@ namespace _03.PlantDiscovery
         {
             int n = int.Parse(Console.ReadLine());
             var info = new Dictionary<string, (int rarity, List<double> rating)>();
+            char[] newCharArray = { ':', '-', ' ' };
             for (int i = 0; i < n; i++)
             {
                 var cmd = Console.ReadLine().Split("<->");
@@ -26,7 +27,34 @@ namespace _03.PlantDiscovery
             {
                 var command = Console.ReadLine();
                 if (command == "Exhibition") break;
+                var cmd = command.Split(newCharArray, StringSplitOptions.RemoveEmptyEntries);
+                var action = cmd[0];
+                var plant = cmd[1];
+                switch (action)
+                {
+                    case "Rate":
+                        if (info.ContainsKey(plant))
+                        {
+                            var rating = double.Parse(cmd[2]); //Example for why rating is a double number: rating = 1.22; rating = 9.54
+                            info[plant].rating.Add(rating);
+                        }
+                        else Console.WriteLine("error");
+                        break;
+                    case "Update":
+                        if (info.ContainsKey(plant))
+                        {
+                            var newRarity = int.Parse(cmd[2]);
+                            info[plant] = (newRarity, info[plant].rating);
+                        }
+                        else Console.WriteLine("error");
+                            break;
+                    case "Reset":
 
+                        break;
+
+                    default:
+                        break;
+                }
             }
         }
     }
