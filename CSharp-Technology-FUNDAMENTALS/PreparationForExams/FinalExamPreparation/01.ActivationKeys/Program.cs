9cbe616aa93e359b1cd1719343e4f8dfa0826453
Vasil Bukoev <vbukoev@gmail.com>
2022-08-07 12:09:07 +0300

    removed comments

--- CSharp-Technology-FUNDAMENTALS/PreparationForExams/FinalExamPreparation/01.ActivationKeys/Program.cs
+++ CSharp-Technology-FUNDAMENTALS/PreparationForExams/FinalExamPreparation/01.ActivationKeys/Program.cs
@@ -9,7 +9,7 @@ namespace _01.ActivationKeys
     {
         static void Main(string[] args)
         {
-            var actKey  = Console.ReadLine();
+            var actKey = Console.ReadLine();
             var cmd = "";
             while (true)
             {
@@ -19,25 +19,25 @@ namespace _01.ActivationKeys
                 var action = splitted[0];
                 switch (action)
                 {
-                   case "Contains": 
-                   var substring = splitted[1];
+                    case "Contains":
+                        var substring = splitted[1];
                         if (actKey.Contains(substring)) Console.WriteLine($"{actKey} contains {substring}");
                         else Console.WriteLine("Substring not found!");
                         break;
                     case "Flip":
-                    var cases = splitted[1];
+                        var cases = splitted[1];
                         var start = int.Parse(splitted[2]);
                         var end = int.Parse(splitted[3]);
                         var cnt = end - start;
                         substring = actKey.Substring(start, cnt);
-                        var toChar= actKey.ToArray();
+                        var toChar = actKey.ToArray();
                         switch (cases)
                         {
                             case "Upper":
                                 substring = substring.ToUpper();
                                 var index = 0;
                                 for (int i = start; i < end; i++)
-                                {                                    
+                                {
                                     toChar[i] = substring[index];
                                     index++;
                                 }
@@ -47,7 +47,7 @@ namespace _01.ActivationKeys
                                 substring = substring.ToLower();
                                 index = 0;
                                 for (int i = start; i < end; i++)
-                                {                                    
+                                {
                                     toChar[i] = substring[index];
                                     index++;
                                 }
@@ -71,5 +71,6 @@ namespace _01.ActivationKeys
             }
             Console.WriteLine($"Your activation key is: {actKey}");
         }
+
     }
 }
