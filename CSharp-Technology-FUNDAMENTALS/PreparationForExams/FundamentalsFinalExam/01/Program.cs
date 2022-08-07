cc9afce51b0ed6bee92ae5855fa76c4a8974ed32
Vasil Bukoev <vbukoev@gmail.com>
2022-08-07 11:13:09 +0300

    =

---
+++ CSharp-Technology-FUNDAMENTALS/PreparationForExams/FundamentalsFinalExam/01/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            while (true)
+            {
+                var cmd = Console.ReadLine();
+                if (cmd == "Done") break;
+                var tokens = cmd.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                var action = tokens[0];
+                switch (action)
+                {
+                    case "Change":
+                        {
+                            var nChar = char.Parse(tokens[1]);
+                            var replacement = char.Parse(tokens[2]);
+                            input = input.Replace(nChar, replacement);
+                            Console.WriteLine(input);
+                            break;
+                        }
+                    case "Includes":
+                        {
+                            var substring = tokens[1];
+                            if (!input.Contains(substring)) Console.WriteLine("False");
+                            else Console.WriteLine("True");
+                            break;
+                        }                        
+                    case "End":
+                        {
+                            var substring = tokens[1];
+                            var finalLength = input.Substring(input.Length - substring.Length);
+                            if (finalLength != substring) Console.WriteLine("False");
+                            else Console.WriteLine("True");                                       
+                            break;
+                        }
+                    case "Uppercase":
+                        {
+                            input = input.ToUpper();
+                            Console.WriteLine(input);
+                            break; 
+                        }                        
+                    case "FindIndex":
+                        {
+                            var newChar = char.Parse(tokens[1]);
+                            var indexOfChar = input.IndexOf(newChar);
+                            Console.WriteLine(indexOfChar);
+                            break;
+                        }
+                    case "Cut":
+                        {
+                            var start = int.Parse(tokens[1]);
+                            var cnt = int.Parse(tokens[2]);
+                            input = input.Substring(start, cnt);
+                            Console.WriteLine(input);
+                            break;
+                        }
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ CSharp-Technology-FUNDAMENTALS/PreparationForExams/FundamentalsFinalExam/03/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new Dictionary<string, int>();
+            var cmd = "";
+            while (true)
+            {
+                cmd = Console.ReadLine();
+                if (cmd == "Log out") break;
+                var tokens = cmd.Split(": ", StringSplitOptions.RemoveEmptyEntries);
+                var action = tokens[0];
+                var newUserName = tokens[1];
+                switch (action)
+                {
+                    case "New follower":
+                        NewFollower(dictionary, newUserName);
+                        break;
+
+                    case "Like":
+                        Like(dictionary, tokens, newUserName);
+                        break;
+
+                    case "Comment":
+                        Comment(dictionary, newUserName);
+                        break;
+
+                    case "Blocked":
+                        Blocked(dictionary, newUserName);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+            var followersCnt = dictionary.Keys.Count;
+            Console.WriteLine($"{followersCnt} followers");
+            foreach (var kvp in dictionary) Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+        }
+        static void NewFollower(Dictionary<string, int> dictionary, string newUserName)
+        {
+            if (!dictionary.ContainsKey(newUserName)) dictionary.Add(newUserName, 0);
+        }
+        static void Like(Dictionary<string, int> dictionary, string[] tokens, string newUserName)
+        {
+            var cnt = int.Parse(tokens[2]);
+            if (!dictionary.ContainsKey(newUserName)) dictionary.Add(newUserName, 0);
+            dictionary[newUserName] += cnt;
+        }
+        static void Comment(Dictionary<string, int> dictionary, string newUserName)
+        {
+            if (!dictionary.ContainsKey(newUserName)) dictionary.Add(newUserName, 0);
+            dictionary[newUserName] = dictionary[newUserName] + 1;
+        }
+        static void Blocked(Dictionary<string, int> dictionary, string newUserName)
+        {
+            if (!dictionary.ContainsKey(newUserName)) Console.WriteLine($"{newUserName} doesn't exist.");
+            else dictionary.Remove(newUserName);
+        }
+    }
+}
