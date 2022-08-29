31ae7ca60eb6a796b07dddaccad9f2266fae374e
Vasil Bukoev <vbukoev@gmail.com>
2022-08-29 17:54:31 +0300

    creating new App

---
+++ CSharp-Technology-FUNDAMENTALS/Regular Expressions - Exercise/ProjectApp/ConsoleApp4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
