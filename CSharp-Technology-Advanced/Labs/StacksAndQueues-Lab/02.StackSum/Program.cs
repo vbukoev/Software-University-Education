9399377d00313965114eba0037200ea5621f5701
Vasil Bukoev <vbukoev@gmail.com>
2022-09-13 19:35:46 +0300

    Update Program.cs

--- CSharp-Technology-Advanced/Labs/StacksAndQueues-Lab/02.StackSum/Program.cs
+++ CSharp-Technology-Advanced/Labs/StacksAndQueues-Lab/02.StackSum/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _02.StackSum
 {
@@ -6,7 +8,32 @@ namespace _02.StackSum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stack<int> stack = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToList());
+            var cmd = Console.ReadLine().ToLower();
+            while (cmd != "end")
+            {
+                var splitted = cmd.Split();
+                if (splitted[0] == "add")
+                {
+                    int firstNum = int.Parse(splitted[1]);
+                    int secondNum = int.Parse(splitted[2]);
+                    stack.Push(firstNum);
+                    stack.Push(secondNum);
+                }
+                if (splitted[0] == "remove")
+                {
+                    int cnt = int.Parse(splitted[1]);
+                    if (cnt <= stack.Count)
+                    {
+                        for (int i = 0; i < cnt; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                }
+                cmd = Console.ReadLine().ToLower();
+            }
+            Console.WriteLine(stack.Sum());
         }
     }
 }
