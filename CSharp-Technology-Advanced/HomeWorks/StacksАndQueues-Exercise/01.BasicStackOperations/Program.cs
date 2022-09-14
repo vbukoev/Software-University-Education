d336a6ea44d1f9c76ad6209759db6b1a3ea7150d
Vasil Bukoev <vbukoev@gmail.com>
2022-09-14 18:51:38 +0300

    Update Program.cs

--- CSharp-Technology-Advanced/HomeWorks/StacksАndQueues-Exercise/01.BasicStackOperations/Program.cs
+++ CSharp-Technology-Advanced/HomeWorks/StacksАndQueues-Exercise/01.BasicStackOperations/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _01.BasicStackOperations
 {
@@ -6,7 +8,28 @@ namespace _01.BasicStackOperations
     {
         static void Main(string[] args)
         {
-            
+            int[] cmdNums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int elementToPush = cmdNums[0];
+            int elementToPop = cmdNums[1];
+            int elementToPeek = cmdNums[2];
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < elementToPush; i++) //push of the stack
+            {
+                stack.Push(input[i]);
+            } 
+
+            for (int i = 1; i <= elementToPop; i++) //pop of the stack
+            {
+                stack.Pop();
+            }
+            if (stack.Contains(elementToPeek)) Console.WriteLine("true");
+            else
+            {
+                if (stack.Any()) Console.WriteLine(stack.Min());
+                else Console.WriteLine(0);
+            }
         }
     }
 }
