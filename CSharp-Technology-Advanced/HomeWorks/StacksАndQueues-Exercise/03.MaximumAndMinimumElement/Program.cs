2647c1d34225e4404aaeb20c8f17f9375f86aa28
Vasil Bukoev <vbukoev@gmail.com>
2022-09-14 19:06:30 +0300

    Update Program.cs

--- CSharp-Technology-Advanced/HomeWorks/StacksАndQueues-Exercise/03.MaximumAndMinimumElement/Program.cs
+++ CSharp-Technology-Advanced/HomeWorks/StacksАndQueues-Exercise/03.MaximumAndMinimumElement/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace _03.MaximumAndMinimumElement
 {
@@ -6,7 +9,35 @@ namespace _03.MaximumAndMinimumElement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var stack = new Stack<int>();
+            var sb = new StringBuilder();
+            var lines = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= lines; i++)
+            {
+                var tokens = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                int cmd = tokens[0];
+
+                switch (cmd)
+                {
+                    case 1:
+                        int elementPush = tokens[1];
+                        stack.Push(elementPush);
+                        break; 
+                    case 2:
+                        if (stack.Count > 0) stack.Pop();
+                        break;
+                    case 3:
+                        if (stack.Count > 0) sb.AppendLine(stack.Max().ToString());
+                        break;
+                    case 4:
+                        if (stack.Count > 0) sb.AppendLine(stack.Min().ToString());     
+                        break;
+                    default:
+                        break;
+                }
+            }        
+            sb.AppendLine(String.Join(", ", stack));
+            Console.WriteLine(sb);
         }
     }
 }
