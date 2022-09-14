911f7dd99757b562c06119e7584f0dd73f4601f3
Vasil Bukoev <vbukoev@gmail.com>
2022-09-14 18:57:21 +0300

    Update Program.cs

--- CSharp-Technology-Advanced/HomeWorks/StacksАndQueues-Exercise/02.BasicQueueOperations/Program.cs
+++ CSharp-Technology-Advanced/HomeWorks/StacksАndQueues-Exercise/02.BasicQueueOperations/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _02.BasicQueueOperations
 {
@@ -6,7 +8,37 @@ namespace _02.BasicQueueOperations
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cmdNums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int elementToEnqueue = cmdNums[0];
+            int elementToDequeue = cmdNums[1];
+            int elementToLookFor = cmdNums[2];
+
+            var queue = new Queue<int>();
+
+            for (int i = 0; i < elementToEnqueue; i++)
+            {
+                queue.Enqueue(input[i]);
+            }
+            for (int i = 0; i < elementToDequeue; i++)
+            {
+                queue.Dequeue();
+            }
+            if (queue.Contains(elementToLookFor))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                if (queue.Any())
+                {
+                    Console.WriteLine(queue.Min());
+                }
+                else
+                {
+                    Console.WriteLine(0);
+                }
+            }
         }
     }
 }
