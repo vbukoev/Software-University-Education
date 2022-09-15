4c70a41d981671024b8c29155b8cbb0d1d0d7b3c
Vasil Bukoev <vbukoev@gmail.com>
2022-09-15 13:21:49 +0300

    Update Program.cs

--- CSharp-Technology-Advanced/HomeWorks/StacksАndQueues-Exercise/09.SimpleTextEditor/Program.cs
+++ CSharp-Technology-Advanced/HomeWorks/StacksАndQueues-Exercise/09.SimpleTextEditor/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace _09.SimpleTextEditor
 {
@@ -6,7 +8,41 @@ namespace _09.SimpleTextEditor
     {
         static void Main(string[] args)
         {
-            
+            var cntOfCommand = int.Parse(Console.ReadLine()); // n
+            var textVersion = new Stack<string>();
+            var sb = new StringBuilder();
+            for (int i = 0; i < cntOfCommand; i++)
+            {
+                var tokens = Console.ReadLine().Split();
+                int cmd = int.Parse(tokens[0]);
+                switch (cmd)
+                {
+                    case 1:
+                        textVersion.Push(sb.ToString());
+                        string toAppend = tokens[1];
+                        sb.Append(toAppend);
+                        break;
+                    case 2:
+                        int cnt = int.Parse(tokens[1]);
+                        textVersion.Push(sb.ToString());
+                        sb.Remove(sb.Length - cnt, cnt);
+                        break;
+                    case 3:
+                        int index = int.Parse(tokens[1]);
+                        index--;
+                        if (index >= 0 && index < sb.Length)
+                        {
+                            Console.WriteLine(sb[index]);
+                        }
+                        break;
+                    case 4:
+                        sb.Clear();
+                        sb.Append(textVersion.Pop());
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
