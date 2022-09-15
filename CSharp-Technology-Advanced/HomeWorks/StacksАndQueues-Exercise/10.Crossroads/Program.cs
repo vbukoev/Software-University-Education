1a5164addf9b6af0bdb4f8f81f91d6388ebf1101
Vasil Bukoev <vbukoev@gmail.com>
2022-09-15 17:20:47 +0300

    Update Program.cs

--- CSharp-Technology-Advanced/HomeWorks/StacksАndQueues-Exercise/10.Crossroads/Program.cs
+++ CSharp-Technology-Advanced/HomeWorks/StacksАndQueues-Exercise/10.Crossroads/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _10.Crossroads
 {
@@ -6,7 +8,54 @@ namespace _10.Crossroads
     {
         static void Main(string[] args)
         {
-            
+            int greenLightSec = int.Parse(Console.ReadLine());
+            int freeWindowSec = int.Parse(Console.ReadLine());
+
+            var carQueue = new Queue<string>();
+            int passedCarCnt = 0;
+
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "END") break;
+                if (input == "green")
+                {
+                    int greenLight = greenLightSec;
+                    int freeWindow = freeWindowSec;
+                    while (carQueue.Any())
+                    {
+                        string currCar = carQueue.Peek();
+                        if (greenLight >= currCar.Length)
+                        {
+                            greenLight -= currCar.Length;
+                            carQueue.Dequeue();
+                            passedCarCnt++;
+                        }
+                        else if (greenLight < currCar.Length)
+                        {
+                            if (greenLight + freeWindow >= currCar.Length)
+                            {
+                                greenLight = 0;
+                                freeWindow -= currCar.Length;
+                                carQueue.Dequeue();
+                                passedCarCnt++;
+                            }
+                            else
+                            {
+                                int crashIndex = greenLight + freeWindow;
+                                if (crashIndex >= 0 && crashIndex < currCar.Length)
+                                {
+                                    Console.WriteLine("A crash happened!");
+                                    Console.WriteLine($"{currCar} was hit at {currCar[crashIndex]}");
+                                    return;
+                                }
+                            }
+                        }
+                        else if (greenLight <= 0) break;
+                    }
+                }                
+            }
         }
     }
 }
