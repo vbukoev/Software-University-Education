ddacb2e35b54dd9b72c29a3936eec0bfc37213cb
Vasil Bukoev <vbukoev@gmail.com>
2022-09-15 12:51:59 +0300

    Update Program.cs

--- CSharp-Technology-Advanced/HomeWorks/StacksАndQueues-Exercise/08.BalancedParenthesis/Program.cs
+++ CSharp-Technology-Advanced/HomeWorks/StacksАndQueues-Exercise/08.BalancedParenthesis/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _08.BalancedParenthesis
 {
@@ -6,7 +7,35 @@ namespace _08.BalancedParenthesis
     {
         static void Main(string[] args)
         {
-            
+            string input = Console.ReadLine();
+            var stack = new Stack<char>();
+            bool isBalanced = true;
+            foreach (var symbol in input)
+            {
+                if (symbol == '(' || symbol == '[' || symbol == '{')
+                {
+                    stack.Push(symbol);
+                }
+                else
+                {
+                    if (stack.Count == 0)
+                    {
+                        isBalanced = false;
+                        break; 
+                    }
+                    char lastSymbol = stack.Pop();
+                    if (lastSymbol == ')' && lastSymbol != '(')
+                    {
+                        isBalanced = false;
+                        break;
+                    }
+                    else if (lastSymbol == )
+                    {
+
+                    }
+                    
+                }
+            }
         }
     }
 }
