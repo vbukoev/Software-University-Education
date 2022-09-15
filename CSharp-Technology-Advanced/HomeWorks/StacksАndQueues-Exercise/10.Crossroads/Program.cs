8ff9737db818454fdb3d4b32f02bfb7e67ef393b
Vasil Bukoev <vbukoev@gmail.com>
2022-09-15 18:00:52 +0300

    removed comments

--- CSharp-Technology-Advanced/HomeWorks/StacksАndQueues-Exercise/10.Crossroads/Program.cs
+++ CSharp-Technology-Advanced/HomeWorks/StacksАndQueues-Exercise/10.Crossroads/Program.cs
@@ -1,140 +1,67 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-
-//namespace _10.Crossroads
-//{
-//    internal class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            int greenLightSec = int.Parse(Console.ReadLine());
-//            int freeWindowSec = int.Parse(Console.ReadLine());
-
-//            var carQueue = new Queue<string>();
-//            int passedCarCnt = 0;
-
-//            while (true)
-//            {
-//                string input = Console.ReadLine();
-//                if (input == "END") break;
-//                if (input == "green")
-//                {
-//                    int greenLight = greenLightSec;
-//                    int freeWindow = freeWindowSec;
-//                    while (carQueue.Any())
-//                    {
-//                        string currCar = carQueue.Peek();
-//                        if (greenLight >= currCar.Length)
-//                        {
-//                            greenLight -= currCar.Length;
-//                            carQueue.Dequeue();
-//                            passedCarCnt++;
-//                        }
-//                        else if (greenLight <= 0) break;
-//                        else if (greenLight < currCar.Length)
-//                        {
-//                            if (greenLight + freeWindow >= currCar.Length)
-//                            {
-//                                greenLight = 0;
-//                                freeWindow -= currCar.Length;
-//                                carQueue.Dequeue();
-//                                passedCarCnt++;
-//                            }
-//                            else
-//                            {
-//                                int crashIndex = greenLight + freeWindow;
-//                                if (crashIndex >= 0 && crashIndex < currCar.Length)
-//                                {
-//                                    Console.WriteLine("A crash happened!");
-//                                    Console.WriteLine($"{currCar} was hit at {currCar[crashIndex]}.");
-//                                    return;
-//                                }
-//                            }
-//                        }
-
-//                    }
-//                }
-//                else
-//                {
-//                    carQueue.Enqueue(input);
-//                }
-//            }
-//            Console.WriteLine("Everyone is safe.");
-//            Console.WriteLine($"{passedCarCnt} total cars passed the crossroads.");
-//        }
-//    }
-//}
-
-
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace _10._Crossroads
+namespace _10.Crossroads
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int greenLightDuration = int.Parse(Console.ReadLine());
-            int freeWindowDiration = int.Parse(Console.ReadLine());
+            int greenLightSec = int.Parse(Console.ReadLine());
+            int freeWindowSec = int.Parse(Console.ReadLine());
 
-
-            Queue<string> queueCars = new Queue<string>();
-
-            int passedCars = 0;
+            var carQueue = new Queue<string>();
+            int passedCarCnt = 0;
 
             while (true)
             {
-                string command = Console.ReadLine();
-
-                if (command == "END")
+                string input = Console.ReadLine();
+                if (input == "END") break;
+                if (input == "green")
                 {
-                    break;
-                }
-
-                if (command == "green")
-                {
-                    int totalSeconds = greenLightDuration + freeWindowDiration;
-                    int greenLight = greenLightDuration;
-                    int freeLight = freeWindowDiration;
-                    while (queueCars.Count > 0)
+                    int greenLight = greenLightSec;
+                    int freeWindow = freeWindowSec;
+                    while (carQueue.Any())
                     {
-                        string carToCross = queueCars.Peek();
-
-                        if (totalSeconds > freeWindowDiration)
+                        string currCar = carQueue.Peek();
+                        if (greenLight >= currCar.Length)
                         {
-                            if (carToCross.Length <= totalSeconds)
-                            {                                
-                                greenLight -= carToCross.Length;
-                                queueCars.Dequeue();
-                                passedCars++;
+                            greenLight -= currCar.Length;
+                            carQueue.Dequeue();
+                            passedCarCnt++;
+                        }
+                        else if (greenLight <= 0) break;
+                        else if (greenLight < currCar.Length)
+                        {
+                            if (greenLight + freeWindow >= currCar.Length)
+                            {
+                                greenLight = 0;
+                                freeWindow -= currCar.Length;
+                                carQueue.Dequeue();
+                                passedCarCnt++;
                             }
                             else
                             {
-                                string hittedCar = carToCross.Substring(totalSeconds, carToCross.Length - totalSeconds);
-
-                                Console.WriteLine("A crash happened!");
-                                Console.WriteLine($"{queueCars.Peek()} was hit at {hittedCar[0]}.");
-                                return;
+                                int crashIndex = greenLight + freeWindow;
+                                if (crashIndex >= 0 && crashIndex < currCar.Length)
+                                {
+                                    Console.WriteLine("A crash happened!");
+                                    Console.WriteLine($"{currCar} was hit at {currCar[crashIndex]}.");
+                                    return;
+                                }
                             }
                         }
-                        else
-                        {
-                            queueCars.Dequeue();
 
-                            continue;
-                        }
                     }
                 }
                 else
                 {
-                    queueCars.Enqueue(command);
+                    carQueue.Enqueue(input);
                 }
             }
-
             Console.WriteLine("Everyone is safe.");
-            Console.WriteLine($"{passedCars} total cars passed the crossroads.");
+            Console.WriteLine($"{passedCarCnt} total cars passed the crossroads.");
         }
     }
 }
