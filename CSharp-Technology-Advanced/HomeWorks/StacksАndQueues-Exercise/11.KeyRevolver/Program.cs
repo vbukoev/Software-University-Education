7f62a411243cb3f01facbcfe7534a75e8995ab67
Vasil Bukoev <vbukoev@gmail.com>
2022-09-15 21:04:35 +0300

    added ref to the bulletsShot variable

--- CSharp-Technology-Advanced/HomeWorks/StacksАndQueues-Exercise/11.KeyRevolver/Program.cs
+++ CSharp-Technology-Advanced/HomeWorks/StacksАndQueues-Exercise/11.KeyRevolver/Program.cs
@@ -9,7 +9,7 @@ namespace _11.KeyRevolver
         static void Main(string[] args)
         {
             int bulletPrice = int.Parse(Console.ReadLine());
-            int barelSize = int.Parse(Console.ReadLine());
+            int barrelSize = int.Parse(Console.ReadLine());
             int[] bulletsInfo = Console.ReadLine().Split().Select(int.Parse).ToArray();
             int[] locksInfo = Console.ReadLine().Split().Select(int.Parse).ToArray();
             int reward = int.Parse(Console.ReadLine());
@@ -20,19 +20,22 @@ namespace _11.KeyRevolver
 
             while (bullets.Any() && locks.Any()) //while there are bullets and locks left!
             {
-                int bulletSize = bullets.Pop(); // Remove it both cases 
-                int lockSize = locks.Peek(); // we only peek the value
-                bulletsShot++;
-                if (bulletSize>=lockSize) // we shot that lock 
-                {
-                    locks.Dequeue(); // remove the lock
-                    Console.WriteLine("Bang!");
-                }
-                else
-                {
-                    Console.WriteLine("Ping!");
-                }
+                StartShootingLocks(bullets, locks, ref bulletsShot, barrelSize);
             }
+        }
+        static void StartShootingLocks(Stack<int> bullets, Queue<int> locks, ref int bulletsShot, int barrelSize)
+        {
+            int bulletSize = bullets.Pop(); // Remove it both cases 
+            int lockSize = locks.Peek(); // we only peek the value
+            bulletsShot++;
+            if (bulletSize >= lockSize) // we shot that lock 
+            {
+                locks.Dequeue(); // remove the lock
+                Console.WriteLine("Bang!");
+            }
+            else Console.WriteLine("Ping!");
+
+            if (bulletsShot % barrelSize == 0 && bullets.Any()) Console.WriteLine("Reloading!");//if there are any bullets
         }
     }
 }
