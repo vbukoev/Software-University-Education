fd2f27fcd1646d2df8217121fc6785a6927c7b8d
Vasil Bukoev <vbukoev@gmail.com>
2022-09-15 20:58:06 +0300

    Update Program.cs

--- CSharp-Technology-Advanced/HomeWorks/StacksАndQueues-Exercise/11.KeyRevolver/Program.cs
+++ CSharp-Technology-Advanced/HomeWorks/StacksАndQueues-Exercise/11.KeyRevolver/Program.cs
@@ -8,7 +8,31 @@ namespace _11.KeyRevolver
     {
         static void Main(string[] args)
         {
+            int bulletPrice = int.Parse(Console.ReadLine());
+            int barelSize = int.Parse(Console.ReadLine());
+            int[] bulletsInfo = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] locksInfo = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int reward = int.Parse(Console.ReadLine());
+            Queue<int> locks = new Queue<int>(locksInfo);
+            Stack<int> bullets = new Stack<int>(bulletsInfo);
 
+            int bulletsShot = 0; //counter of bullets
+
+            while (bullets.Any() && locks.Any()) //while there are bullets and locks left!
+            {
+                int bulletSize = bullets.Pop(); // Remove it both cases 
+                int lockSize = locks.Peek(); // we only peek the value
+                bulletsShot++;
+                if (bulletSize>=lockSize) // we shot that lock 
+                {
+                    locks.Dequeue(); // remove the lock
+                    Console.WriteLine("Bang!");
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+                }
+            }
         }
     }
 }
