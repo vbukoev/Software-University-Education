cb138f811575f354b3713d360927193c41f586c4
Vasil Bukoev <vbukoev@gmail.com>
2022-09-16 09:51:17 +0300

    changed folders

---
+++ CSharp-Technology-Advanced/Labs/02MultidimensionalArrays-Lab/MultidimensionalArrays-Lab/01SumMatrixElements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01SumMatrixElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CSharp-Technology-Advanced/Labs/02MultidimensionalArrays-Lab/MultidimensionalArrays-Lab/06.Jagged-ArrayModification/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _06.Jagged_ArrayModification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
