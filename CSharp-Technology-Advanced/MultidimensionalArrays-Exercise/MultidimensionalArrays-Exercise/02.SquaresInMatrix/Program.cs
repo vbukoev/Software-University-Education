e70a8e3981d439a7f1ee914bfd6a6027b5518b1e
Vasil Bukoev <vbukoev@gmail.com>
2022-09-18 14:24:01 +0300

    submission of the second task of the exercise

--- CSharp-Technology-Advanced/MultidimensionalArrays-Exercise/MultidimensionalArrays-Exercise/02.SquaresInMatrix/Program.cs
+++ CSharp-Technology-Advanced/MultidimensionalArrays-Exercise/MultidimensionalArrays-Exercise/02.SquaresInMatrix/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _02.SquaresInMatrix
 {
@@ -6,7 +7,30 @@ namespace _02.SquaresInMatrix
     {
         static void Main(string[] args)
         {
-            
+            var sizes = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var rows = sizes[0];
+            var cols = sizes[1];
+            var matrix = new string[rows, cols]; // making the matrix
+            for (int row = 0; row < matrix.GetLength(0); row++) //filling the matrix 
+            {
+                var currRow = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currRow[col];
+                }
+            }
+            int cntOfSquares = 0;
+            for (int row = 0; row < matrix.GetLength(0)-1; row++) //going through the matrix and looking for squares
+            {
+                for (int col = 0; col < matrix.GetLength(1)-1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1] && matrix[row + 1, col] == matrix[row+1, col+1] && matrix[row, col] == matrix[row+1, col+1])
+                    {
+                        cntOfSquares++;
+                    }
+                }
+            }
+            Console.WriteLine(cntOfSquares);
         }
     }
 }
