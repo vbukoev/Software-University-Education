bcf4c62a6fadfeb2d7748b830e5235f7dd7a010f
Vasil Bukoev <vbukoev@gmail.com>
2022-09-18 13:58:41 +0300

    Update Program.cs

---
+++ CSharp-Technology-Advanced/MultidimensionalArrays-Exercise/MultidimensionalArrays-Exercise/01.DiagonalDifference/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.DiagonalDifference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizes = int.Parse(Console.ReadLine()); // N
+            var matrix = new int[sizes, sizes];
+            for (int row = 0; row < sizes; row++)
+            {
+                var currRow = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < currRow.Length; col++)
+                {
+                    matrix[row, col] = currRow[col];
+                }
+            }
+            int primaryDiagonal = 0;
+            int secondaryDiagonal = 0;
+            int currCol = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int col = row;
+                primaryDiagonal += matrix[row, col];
+            }
+            for (int row = matrix.GetLength(0)-1; row>=0; row--)
+            {
+                secondaryDiagonal += matrix[row, currCol];
+
+                currCol++;
+            }
+        }
+    }
+}
