a7a8dd526f01a5ae97f01af415a60660c791e462
Vasil Bukoev <vbukoev@gmail.com>
2022-09-19 18:43:02 +0300

    removed if statement which was useless

--- CSharp-Technology-Advanced/MultidimensionalArrays-Exercise/MultidimensionalArrays-Exercise/02.SquaresInMatrix/Program.cs
+++ CSharp-Technology-Advanced/MultidimensionalArrays-Exercise/MultidimensionalArrays-Exercise/02.SquaresInMatrix/Program.cs
@@ -20,17 +20,19 @@ namespace _02.SquaresInMatrix
                 }
             }
             int cntOfSquares = 0;
-            for (int row = 0; row < matrix.GetLength(0)-1; row++) //going through the matrix and looking for squares
-            {
-                for (int col = 0; col < matrix.GetLength(1)-1; col++)
+                for (int row = 0; row < matrix.GetLength(0) - 1; row++) //going through the matrix and looking for squares
                 {
-                    if (matrix[row, col] == matrix[row, col + 1] && matrix[row + 1, col] == matrix[row+1, col+1] && matrix[row, col] == matrix[row+1, col+1])
+                    for (int col = 0; col < matrix.GetLength(1) - 1; col++)
                     {
-                        cntOfSquares++;
+                        if (matrix[row, col] == matrix[row, col + 1] && matrix[row + 1, col] == matrix[row + 1, col + 1] && matrix[row, col] == matrix[row + 1, col + 1])
+                        {
+                            cntOfSquares++;
+                        }
                     }
                 }
-            }
+            
             Console.WriteLine(cntOfSquares);
         }
     }
 }
+
