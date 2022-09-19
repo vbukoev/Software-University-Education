3be375fa425cc0fbe7511cb329697eb55e8914a0
Vasil Bukoev <vbukoev@gmail.com>
2022-09-19 21:15:46 +0300

    10th task

--- CSharp-Technology-Advanced/MultidimensionalArrays-Exercise/MultidimensionalArrays-Exercise/10/Program.cs
+++ CSharp-Technology-Advanced/MultidimensionalArrays-Exercise/MultidimensionalArrays-Exercise/10/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _10
 {
@@ -6,7 +7,28 @@ namespace _10
     {
         static void Main(string[] args)
         {
-            
+            var dimensions = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+            int playerRow = 0;
+            int playerCol = 0;
+            var matrix = new char[rows, cols];
+            for (int row = 0; row < rows; row++)
+            {
+                var chars = Console.ReadLine();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = chars[col];
+                    if (matrix[row, col] == 'P')
+                    {
+                        playerRow = row;
+                        playerCol = col;
+                    }
+                }
+            }
         }
     }
 }
