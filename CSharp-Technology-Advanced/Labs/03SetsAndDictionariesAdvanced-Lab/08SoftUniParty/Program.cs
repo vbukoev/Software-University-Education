2092d19b4c9971b21653a2897d63546deabdc7b4
Vasil Bukoev <vbukoev@gmail.com>
2022-09-20 21:39:43 +0300

    Update Program.cs

--- CSharp-Technology-Advanced/Labs/03SetsAndDictionariesAdvanced-Lab/08SoftUniParty/Program.cs
+++ CSharp-Technology-Advanced/Labs/03SetsAndDictionariesAdvanced-Lab/08SoftUniParty/Program.cs
@@ -6,7 +6,50 @@ namespace _08SoftUniParty
     {
         static void Main(string[] args)
         {
-            
+            var vip = new HashSet<string>();
+            var guests = new HashSet<string>();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == "PARTY") break;
+                foreach (var symbol in input)
+                {
+                    if (char.IsDigit(symbol))
+                    {
+                        vip.Add(input);
+                    }
+                    else
+                    {
+                        guests.Add(input);
+                    }
+                    break;
+                }
+            }
+
+            while (true)
+            {
+                var command = Console.ReadLine();
+                if (command == "END") break;
+                if (vip.Contains(command))
+                {
+                    vip.Remove(command);
+                }
+                if (guests.Contains(command))
+                {
+                    guests.Remove(command);
+                }
+            }
+            int cnt = vip.Count + guests.Count;
+            Console.WriteLine(cnt);
+            foreach (var item in vip)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in guests)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
