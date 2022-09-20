b2779f94fc3bd1396c44df79a31652e9c3db9a93
Vasil Bukoev <vbukoev@gmail.com>
2022-09-20 17:06:07 +0300

    9th task submission

--- CSharp-Technology-Advanced/HomeWorks/02MultidimensionalArrays-Exercise/MultidimensionalArrays-Exercise/09Miner/Program.cs
+++ CSharp-Technology-Advanced/HomeWorks/02MultidimensionalArrays-Exercise/MultidimensionalArrays-Exercise/09Miner/Program.cs
@@ -35,7 +35,107 @@ namespace _09Miner
                 }
  
             }
-
+            int coalCnt = 0;
+            int r = minerIndex[0];
+            int c = minerIndex[1];
+            foreach (var command in cmd)
+            {
+                switch (command)
+                {
+                    case "up":
+                        if (r - 1 >= 0)
+                        {
+                            field[r][c] = "*";
+                            r--;
+                            switch (field[r][c])
+                            {
+                                case "c":
+                                    coalCnt++;
+                                    totalCoalCnt--;
+                                    break;
+                                case "e":
+                                    Console.WriteLine($"Game over! ({r}, {c})");
+                                    return;
+                                default:
+                                    break;
+                            }
+                            field[r][c] = "s";
+                        }
+                        else continue;
+                        break;
+                    case "right":
+                        if (c + 1 >= 0)
+                        {
+                            field[r][c] = "*";
+                            c++;
+                            switch (field[r][c])
+                            {
+                                case "c":
+                                    coalCnt++;
+                                    totalCoalCnt--;
+                                    break;
+                                case "e":
+                                    Console.WriteLine($"Game over! ({r}, {c})");
+                                    return;
+                                default:
+                                    break;
+                            }
+                            field[r][c] = "s";
+                        }
+                        else continue;
+                        break;
+                    case "down":
+                        if (r + 1 >= 0)
+                        {
+                            field[r][c] = "*";
+                            r++;
+                            switch (field[r][c])
+                            {
+                                case "c":
+                                    coalCnt++;
+                                    totalCoalCnt--;
+                                    break;
+                                case "e":
+                                    Console.WriteLine($"Game over! ({r}, {c})");
+                                    return;
+                                default:
+                                    break;
+                            }
+                            field[r][c] = "s";
+                        }
+                        else continue;
+                        break;
+                    case "left":
+                        if (c - 1 >= 0)
+                        {
+                            field[r][c] = "*";
+                            c--;
+                            switch (field[r][c])
+                            {
+                                case "c":
+                                    coalCnt++;
+                                    totalCoalCnt--;
+                                    break;
+                                case "e":
+                                    Console.WriteLine($"Game over! ({r}, {c})");
+                                    return;
+                                default:
+                                    break;
+                            }
+                            field[r][c] = "s";
+                        }
+                        else continue;
+                        break;
+                    default:
+                        break;
+                }
+                if (totalCoalCnt == 0)
+                {
+                    Console.WriteLine($"You collected all coals! ({r}, {c})");
+                    return;
+                }
+            }
+            Console.WriteLine($"{totalCoalCnt} coals left. ({r}, {c})");
         }
     }
 }
