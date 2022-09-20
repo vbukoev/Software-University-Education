d43ce90baeba6a625812692585e5bb29078e0cd9
Vasil Bukoev <vbukoev@gmail.com>
2022-09-20 19:50:33 +0300

    5th task submission

--- CSharp-Technology-Advanced/Labs/03SetsAndDictionariesAdvanced-Lab/05CitiesByContinentAndCoun/Program.cs
+++ CSharp-Technology-Advanced/Labs/03SetsAndDictionariesAdvanced-Lab/05CitiesByContinentAndCoun/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _05CitiesByContinentAndCoun
 {
@@ -6,7 +7,33 @@ namespace _05CitiesByContinentAndCoun
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, Dictionary<string, List<string>>> continents = new Dictionary<string, Dictionary<string, List<string>>>();
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split();
+                string continent = input[0];
+                string country = input[1];
+                string city = input[2];
+
+                if (!continents.ContainsKey(continent))
+                {
+                    continents.Add(continent, new Dictionary<string, List<string>>());
+                }
+                if (!continents[continent].ContainsKey(country))
+                {
+                    continents[continent].Add(country, new List<string>());
+                }
+                continents[continent][country].Add(city);
+            }
+            foreach (var continent in continents)
+            {
+                Console.WriteLine($"{continent.Key}:");
+                foreach (var country in continent.Value)
+                {
+                    Console.WriteLine($"{country.Key} -> {string.Join(", ",country.Value)}");
+                }
+            }
         }
     }
 }
