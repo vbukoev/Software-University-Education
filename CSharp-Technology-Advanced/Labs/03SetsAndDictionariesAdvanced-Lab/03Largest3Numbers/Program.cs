c453783e0ffcc2d3c076f068eef48867f17c334d
Vasil Bukoev <vbukoev@gmail.com>
2022-09-20 19:10:38 +0300

    3rd task submission

--- CSharp-Technology-Advanced/Labs/03SetsAndDictionariesAdvanced-Lab/03Largest3Numbers/Program.cs
+++ CSharp-Technology-Advanced/Labs/03SetsAndDictionariesAdvanced-Lab/03Largest3Numbers/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _03Largest3Numbers
 {
@@ -6,7 +8,11 @@ namespace _03Largest3Numbers
     {
         static void Main(string[] args)
         {
-            
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            numbers = numbers.OrderByDescending(x => x).Take(3).ToList();
+            int cnt = numbers.Count();
+            if (cnt>3) cnt = 3;
+            Console.WriteLine(String.Join(" ", numbers));
         }
     }
 }
