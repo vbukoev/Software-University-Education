e62fbdc256878ed9390996ea1c802af74f6c08d7
Vasil Bukoev <vbukoev@gmail.com>
2022-09-20 19:22:26 +0300

    Update Program.cs

--- CSharp-Technology-Advanced/Labs/03SetsAndDictionariesAdvanced-Lab/02AverageStudentGrades/Program.cs
+++ CSharp-Technology-Advanced/Labs/03SetsAndDictionariesAdvanced-Lab/02AverageStudentGrades/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _02AverageStudentGrades
 {
@@ -6,7 +8,22 @@ namespace _02AverageStudentGrades
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+            var students = new Dictionary<string, List<decimal>>();
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split().ToArray();
+                string name = input[0];
+                decimal mark = decimal.Parse(input[1]);
+                if (!students.ContainsKey(name)) students.Add(name, new List<decimal>());
+                students[name].Add(mark);
+            }
+            foreach (var item in students)
+            {
+                Console.Write($"{item.Key} -> {item.Value:f2}");
+                Console.WriteLine($"(avg: {item.Value.Average():f2})");
+            }
         }
     }
 }
+
