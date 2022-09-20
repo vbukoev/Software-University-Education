f65e04ec3a3d23a0716b4e0ec87057b37e28f9b1
Vasil Bukoev <vbukoev@gmail.com>
2022-09-20 15:42:27 +0300

    Update Program.cs

--- CSharp-Technology-Advanced/HomeWorks/02MultidimensionalArrays-Exercise/MultidimensionalArrays-Exercise/08Bombs/Program.cs
+++ CSharp-Technology-Advanced/HomeWorks/02MultidimensionalArrays-Exercise/MultidimensionalArrays-Exercise/08Bombs/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _08Bombs
 {
@@ -6,7 +7,23 @@ namespace _08Bombs
     {
         static void Main(string[] args)
         {
-            
+            int rowCols = int.Parse(Console.ReadLine());
+            var matrix = new int[rowCols, rowCols];
+            int alive = 0;
+            int sumAlive = 0;
+            for (int row = 0; row < rowCols; row++)
+            {
+                var currRow = Console.ReadLine().Split().Select(n => int.Parse(n)).ToArray();
+                for (int col = 0; col < rowCols; col++)
+                {
+                    matrix[row, col] = currRow[col];
+                }
+            }
+            var cmds = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            foreach (var item in cmds)
+            {
+                var coordinates = item.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            }
         }
     }
 }
