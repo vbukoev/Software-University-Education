9e7644aec5d182b3d8b8aebd8b7a99b27803b9c9
Vasil Bukoev <vbukoev@gmail.com>
2022-09-21 15:50:32 +0300

    2nd task submission

--- CSharp-Technology-Advanced/HomeWorks/03SetsАndDictionariesAdvanced-Exercise/02SetsOf/Program.cs
+++ CSharp-Technology-Advanced/HomeWorks/03SetsАndDictionariesAdvanced-Exercise/02SetsOf/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _02SetsOf
 {
@@ -6,7 +8,34 @@ namespace _02SetsOf
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+            int first = numbers[0];
+            int second = numbers[1];
+            var firstSet = new HashSet<int>();
+            var secondSet = new HashSet<int>();
+            var newSet = new HashSet<int>();
+            
+            for (int i = 0; i < first; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                firstSet.Add(num);
+            }
+
+            for (int i = 0; i < second; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                secondSet.Add(num);
+            }
+
+            foreach (var firstNum in firstSet)
+            {
+                foreach (var secondNum in secondSet)
+                {
+                    if (firstNum == secondNum) newSet.Add(firstNum); 
+                }
+            }
+            //Console.WriteLine(String.Join(" ", newSet));
+            foreach (var item in newSet) Console.Write(item + " ");
         }
     }
 }
