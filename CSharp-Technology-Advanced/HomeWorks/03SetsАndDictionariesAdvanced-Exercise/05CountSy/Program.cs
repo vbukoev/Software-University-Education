592484c0a5201d2fef608faa4756bc14da9227d1
Vasil Bukoev <vbukoev@gmail.com>
2022-09-21 16:29:12 +0300

    6th task

--- CSharp-Technology-Advanced/HomeWorks/03SetsАndDictionariesAdvanced-Exercise/05CountSy/Program.cs
+++ CSharp-Technology-Advanced/HomeWorks/03SetsАndDictionariesAdvanced-Exercise/05CountSy/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _05CountSy
 {
@@ -6,7 +8,23 @@ namespace _05CountSy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var text = new Dictionary<char, int>();
+            var input = Console.ReadLine();
+            foreach (var character in input)
+            {
+                if (!text.ContainsKey(character))
+                {
+                    text[character] = 1;
+                }
+                else
+                {
+                    text[character]++;
+                }
+            }
+            foreach (var item in text.OrderBy(x=>x.Key))
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
         }
     }
 }
