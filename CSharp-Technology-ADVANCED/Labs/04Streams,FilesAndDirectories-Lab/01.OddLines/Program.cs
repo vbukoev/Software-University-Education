cc065624a5a4ffd7543dfc060714e40911eb84ad
Vasil Bukoev <vbukoev@gmail.com>
2022-09-23 17:43:32 +0300

    Update Program.cs

--- CSharp-Technology-ADVANCED/Labs/04Streams,FilesAndDirectories-Lab/01.OddLines/Program.cs
+++ CSharp-Technology-ADVANCED/Labs/04Streams,FilesAndDirectories-Lab/01.OddLines/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace _01.OddLines
 {
@@ -6,7 +7,27 @@ namespace _01.OddLines
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = "01.Odd Lines";
+            string fileName = "input.txt";
+            string filePath = Path.Combine(path, fileName); // combining the name of the file with the path
+
+            using (var reader = new StreamReader(filePath))
+            {
+                using (var writter = new StreamWriter("output.txt"))
+                {
+                    int cnt = 0;
+                    string line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        if (cnt % 2 == 0)
+                        {
+                            writter.WriteLine(line);
+                        }
+                        cnt++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
         }
     }
 }
