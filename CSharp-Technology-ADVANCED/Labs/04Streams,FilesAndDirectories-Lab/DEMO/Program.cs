f2a87815a0d4d80e3fea6c5db3a11397b046e520
Vasil Bukoev <vbukoev@gmail.com>
2022-09-23 16:59:39 +0300

    adding the project files

---
+++ CSharp-Technology-ADVANCED/Labs/04Streams,FilesAndDirectories-Lab/DEMO/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace DEMO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = new StreamReader(".. / .. / .. / input.txt");
+            using (reader)
+            {
+                int counter = 1;
+                while (true)
+                {
+                    string line = reader.ReadLine();
+                    if (line == null)
+                        break;
+                    Console.WriteLine(++counter + ". " + line);
+                }
+            }
+
+        }
+    }
+}
