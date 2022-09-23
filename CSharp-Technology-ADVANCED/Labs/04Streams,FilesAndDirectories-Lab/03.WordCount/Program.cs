d6d0edbcc09cfceef5c60c57ff947065bd69acdb
Vasil Bukoev <vbukoev@gmail.com>
2022-09-23 21:22:27 +0300

    3rd task

--- CSharp-Technology-ADVANCED/Labs/04Streams,FilesAndDirectories-Lab/03.WordCount/Program.cs
+++ CSharp-Technology-ADVANCED/Labs/04Streams,FilesAndDirectories-Lab/03.WordCount/Program.cs
@@ -16,10 +16,33 @@
         }
 
         public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
-        {     
+        {
+            var words = File.ReadAllLines(wordsFilePath);
+            var wordCounts = new Dictionary<string, int>();
             var textLines = File.ReadAllLines(textFilePath);
-            var words = File.ReadAllLines(wordsFilePath).Split().ToArray();
-            var 
+            foreach (var word in words)
+            {
+                if (!wordCounts.ContainsKey(word))
+                {
+                    wordCounts.Add(word, 0);
+                }
+            }
+            foreach (var line in textLines)
+            {
+                var curLine = line.Split(new char[] { ' ', '.', ',', '!', '?', '-', '\'' });
+                foreach (var word in curLine)
+                {
+                    string lowerCase = word.ToLower();
+                    if (!wordCounts.ContainsKey(lowerCase))
+                    {
+                        wordCounts[lowerCase] += 1;
+                    }
+                }
+            }
+            foreach (var (word, count) in wordCounts.OrderByDescending(x=>x.Value))
+            {
+                File.AppendAllText(outputFilePath, $"{word} - {count}{Environment.NewLine}");
+            }
         }
     }
 }
--- CSharp-Technology-ADVANCED/Labs/04Streams,FilesAndDirectories-Lab/06SplitMergeBinaryFiles/Program.cs
+++ CSharp-Technology-ADVANCED/Labs/04Streams,FilesAndDirectories-Lab/06SplitMergeBinaryFiles/Program.cs
@@ -1,12 +1,59 @@
-﻿using System;
-
-namespace _06SplitMergeBinaryFiles
+﻿namespace SplitMergeBinaryFile
 {
-    internal class Program
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class SplitMergeBinaryFile
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string sourceFilePath = @"..\..\..\Files\example.png";
+            string joinedFilePath = @"..\..\..\Files\example-joined.png";
+            string partOnePath = @"..\..\..\Files\part-1.bin";
+            string partTwoPath = @"..\..\..\Files\part-2.bin";
+
+            SplitBinaryFile(sourceFilePath, partOnePath, partTwoPath);
+            MergeBinaryFiles(partOnePath, partTwoPath, joinedFilePath);
+        }
+
+        public static void SplitBinaryFile(string sourceFilePath, string partOneFilePath, string partTwoFilePath)
+        {
+            using (FileStream source = new FileStream(sourceFilePath, FileMode.Open))
+            {
+                using (FileStream partOne = new FileStream(partOneFilePath, FileMode.Create))
+                {
+                    int odd = sourceFilePath.Length % 2 == 1 ? 1 : 0;
+                    byte[] buffer = new byte[source.Length / 2 + odd];
+                    source.Read(buffer);
+                    partOne.Write(buffer);
+                }
+                using (FileStream partTwo = new FileStream(partTwoFilePath, FileMode.Create))
+                {                    
+                    byte[] buffer = new byte[source.Length / 2 ];
+                    source.Read(buffer);
+                    partTwo.Write(buffer);
+                }
+            }
+        }
+
+        public static void MergeBinaryFiles(string partOneFilePath, string partTwoFilePath, string joinedFilePath)
+        {
+            using (FileStream joined = new FileStream(joinedFilePath, FileMode.Open))
+            {
+                using (FileStream partOne = new FileStream(partOneFilePath, FileMode.Create))
+                {                    
+                    byte[] buffer = new byte[partOne.Length / 2 ];
+                    partOne.Read(buffer);
+                    partOne.Write(buffer);
+                }
+                using (FileStream partTwo = new FileStream(partTwoFilePath, FileMode.Create))
+                {
+                    byte[] buffer = new byte[partTwo.Length / 2];
+                    partTwo.Read(buffer);
+                    joined.Write(buffer);
+                }
+            }
         }
     }
 }
