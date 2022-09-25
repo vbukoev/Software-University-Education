a18356964b6db0ab3f13ab6aacd8e2497971f09a
Vasil Bukoev <vbukoev@gmail.com>
2022-09-25 15:56:00 +0300

    02CarExtension

--- CSharp-Technology-ADVANCED/Labs/06DefiningClasses-Lab/CarManufacturer/Car.cs
+++ CSharp-Technology-ADVANCED/Labs/06DefiningClasses-Lab/CarManufacturer/Car.cs
@@ -6,17 +6,35 @@ namespace CarManufacturer
 {
     public class Car
     {
-        string make;
-        string model;
-        int year;
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
         public string Make { get { return this.make; } set { this.make = value; } }
 
         public string Model { get { return this.model; } set { this.model = value; } }
 
         public int Year { get { return this.year; } set { this.year = value; } }
+        public double FuelQuantity { get { return this.fuelQuantity; } set { this.fuelQuantity = value; } }
+        public double FuelConsumption { get { return this.fuelConsumption; } set { this.fuelConsumption = value; } }     
         
-        
-        
-        
+        public void Drive(double distance)
+        {
+            var carContinue = FuelQuantity - fuelConsumption * distance>=0;
+            if (carContinue)
+            {
+                FuelQuantity -= this.FuelConsumption * distance;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:f2}";
+        }
     }
 }
