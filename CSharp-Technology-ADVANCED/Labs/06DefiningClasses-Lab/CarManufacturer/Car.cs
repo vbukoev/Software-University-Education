191bd3c11beab6dc341c546090d361c3998f6e80
Vasil Bukoev <vbukoev@gmail.com>
2022-09-25 16:43:44 +0300

    03CarConstructors added solution

--- CSharp-Technology-ADVANCED/Labs/06DefiningClasses-Lab/CarManufacturer/Car.cs
+++ CSharp-Technology-ADVANCED/Labs/06DefiningClasses-Lab/CarManufacturer/Car.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,23 +7,43 @@ namespace CarManufacturer
 {
     public class Car
     {
-        private string make;
-        private string model;
-        private int year;
-        private double fuelQuantity;
-        private double fuelConsumption;
+        string make;
+        string model;
+        int year;
+        double fuelQuantity;
+        double fuelConsumption;
 
-        public string Make { get { return this.make; } set { this.make = value; } }
+        public string Make { get => this.make; set => this.make = value; }
 
-        public string Model { get { return this.model; } set { this.model = value; } }
+        public string Model { get => this.model; set => this.model = value; }
 
-        public int Year { get { return this.year; } set { this.year = value; } }
-        public double FuelQuantity { get { return this.fuelQuantity; } set { this.fuelQuantity = value; } }
-        public double FuelConsumption { get { return this.fuelConsumption; } set { this.fuelConsumption = value; } }     
-        
+        public int Year { get => this.year; set => this.year = value; }
+        public double FuelQuantity { get => this.fuelQuantity; set => this.fuelQuantity = value; }
+        public double FuelConsumption { get => this.fuelConsumption; set => this.fuelConsumption = value; }
+        public Car()
+        {
+            this.make = "VW";
+            this.model = "Golf";
+            this.year = 2025;
+            this.fuelQuantity = 200;
+            this.fuelConsumption = 10;
+        }
+        public Car(string make, string model, int year) : this()
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+        }
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption) : this(make, model, year)
+        {
+            this.fuelQuantity = fuelQuantity;
+            this.fuelConsumption = fuelConsumption;
+        }
+
         public void Drive(double distance)
         {
-            var carContinue = FuelQuantity - fuelConsumption * distance>=0;
+
+            var carContinue = FuelQuantity - fuelConsumption * distance >= 0;
             if (carContinue)
             {
                 FuelQuantity -= this.FuelConsumption * distance;
@@ -32,9 +53,9 @@ namespace CarManufacturer
                 Console.WriteLine("Not enough fuel to perform this trip!");
             }
         }
-        public string WhoAmI()
+        public string GetInfo()
         {
-            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:f2}";
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:f2}L";
         }
     }
 }
--- CSharp-Technology-ADVANCED/Labs/06DefiningClasses-Lab/CarManufacturer/Program.cs
+++ CSharp-Technology-ADVANCED/Labs/06DefiningClasses-Lab/CarManufacturer/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+
+using System;
 
 namespace CarManufacturer
 {
@@ -6,7 +8,17 @@ namespace CarManufacturer
     {
         public static void Main(string[] args)
         {
-           
+            string make = Console.ReadLine();
+            string model = Console.ReadLine();
+            int year = int.Parse(Console.ReadLine());
+            double fuelQuantity = double.Parse(Console.ReadLine());
+            double fuelConsumption = double.Parse(Console.ReadLine());
+
+            Car first = new Car();
+            Car second = new Car(make, model, year);
+            Car third = new Car(make, model, year, fuelQuantity, fuelConsumption);
+
+
         }
     }
 }
