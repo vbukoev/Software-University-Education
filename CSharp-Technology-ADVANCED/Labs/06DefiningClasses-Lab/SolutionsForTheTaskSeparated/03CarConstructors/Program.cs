72d195273e4443edcd2bbe66955261c6e134b0be
Vasil Bukoev <vbukoev@gmail.com>
2022-09-25 18:16:51 +0300

    -

---
+++ CSharp-Technology-ADVANCED/Labs/06DefiningClasses-Lab/SolutionsForTheTaskSeparated/03CarConstructors/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string make = Console.ReadLine();
+            string model = Console.ReadLine();
+            int year = int.Parse(Console.ReadLine());
+            double fuelQuantity = double.Parse(Console.ReadLine());
+            double fuelConsumption = double.Parse(Console.ReadLine());
+
+            Car first = new Car();
+            Car second = new Car(make, model, year);
+            Car third = new Car(make, model, year, fuelQuantity, fuelConsumption);
+
+
+        }
+    }
+}
