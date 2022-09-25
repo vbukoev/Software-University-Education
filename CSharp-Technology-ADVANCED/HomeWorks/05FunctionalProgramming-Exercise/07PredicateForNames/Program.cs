a95dbb90b8118b34389d6baad9750360bdbefd41
Vasil Bukoev <vbukoev@gmail.com>
2022-09-25 10:39:15 +0300

    07PredicateForNames

---
+++ CSharp-Technology-ADVANCED/HomeWorks/05FunctionalProgramming-Exercise/07PredicateForNames/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _07PredicateForNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Func<string, bool> filter = x=>x.Length<=n;
+            var names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Where(filter).ToList();
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
