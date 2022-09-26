0ca6962aab70f569f1f8614f3c09532ad01d1f7b
Vasil Bukoev <vbukoev@gmail.com>
2022-09-26 18:56:47 +0300

    -

--- CSharp-Technology-ADVANCED/HomeWorks/04Streams,FilesAndDirectories-Exercises/Skeleton-Exercise/Skeleton/LineNumbers/LineNumbers.cs
+++ CSharp-Technology-ADVANCED/HomeWorks/04Streams,FilesAndDirectories-Exercises/Skeleton-Exercise/Skeleton/LineNumbers/LineNumbers.cs
@@ -18,6 +18,15 @@
         {
             int cnt = 1;
             var text = File.ReadAllLines(inputFilePath);
+
+            //for (int i = 0; i < text.Length; i++)
+            //{
+            //    int letters = text[i].Count(char.IsLetter);
+            //    int punctuations = line.Count(char.IsPunctuation);
+            //    File.AppendAllText(outputFilePath, $"Line {cnt}: {line} ({letters})({punctuations}){Environment.NewLine}");
+
+            //    cnt++;
+            //}
             foreach (var line in text)
             {
                 int letters = line.Count(char.IsLetter);
