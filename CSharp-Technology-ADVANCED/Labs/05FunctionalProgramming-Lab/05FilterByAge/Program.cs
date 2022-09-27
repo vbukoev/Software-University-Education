2954740d89d2a1506d7782c84f0b77c02e02e051
Vasil Bukoev <vbukoev@gmail.com>
2022-09-27 21:49:30 +0300

    05FilterAge lab task

--- CSharp-Technology-ADVANCED/Labs/05FunctionalProgramming-Lab/05FilterByAge/Program.cs
+++ CSharp-Technology-ADVANCED/Labs/05FunctionalProgramming-Lab/05FilterByAge/Program.cs
@@ -15,47 +15,53 @@ namespace _05FilterByAge
         static void Main(string[] args)
         {
             int peopleCnt = int.Parse(Console.ReadLine());
-            var people = new List<Person>();
+            Person[] people = new Person[peopleCnt];
+            Func<Person, string, int, bool> ageFilter = (p, c, a) => c == "older" ? p.Age >= a : p.Age < a;
+            Func<Person, string[], string> formatter = (p, f) =>
+            {
+                string fString = string.Empty;
+                if (f.Length == 2)
+                {
+                    if (f[0] == "name")
+                    {
+                        fString = "{0} - {1}";
+                    }
+                    else
+                    {
+                        fString = "{1} - {0}";
+                    }
+                }
+                else
+                {
+                    if (f[0] == "name")
+                    {
+                        fString = "{0}";
+                    }
+                    else
+                    {
+                        fString = "{1}";
+                    }
+                }
+                return string.Format(fString, p.Name, p.Age);
+            };
+
             for (int i = 0; i < peopleCnt; i++)
             {
-                var tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
-                string name = tokens[0];
-                int personAge = int.Parse(tokens[1]);
-                var person = new Person()
+                var tokens = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                people[i] = new Person()
                 {
-                    Age = personAge,
-                    Name = name
+                    Name = tokens[0],
+                    Age = int.Parse(tokens[1])
                 };
-                people.Add(person);
             }
             string condition = Console.ReadLine();
             int age = int.Parse(Console.ReadLine());
-            Func<Person, bool> filter = CreateFilter(condition, age);
-            string format = Console.ReadLine();
-            Func<Person, string> select = GetSelector(format);
-            var res = people.Where(filter)
-        }
+            string[] pattern = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
 
-        static Func<Person, string> GetSelector(string format)
-        {
-            switch (format)
-            {
-                case "name age": return p => $"{p.Name} - {p.Age}";
-                case "age": return p => $"{p.Age}";
-                case "name": return p => $"{p.Name}";                
-            }
-           
+            Console.WriteLine(string.Join(Environment.NewLine, people
+                .Where(p => ageFilter(p, condition, age)).Select(p => formatter(p, pattern))));
         }
-
-        public static Func<Person, bool> CreateFilter(string condition, int age)
-    {
-        switch (condition)
-        {
-            case "younger" return x => x < age; 
-                    case "older" return x => x >= age;
-            default: throw new ArgumentOutOfRangeException(condition);
-                break;
-        }
-    } 
     }
 }
+
