c84856cebb9de5963a8ab4c56c7e3c10cbac1579
Vasil Bukoev <vbukoev@gmail.com>
2022-09-27 19:08:34 +0300

    removed the array of numbers and added to the console writeline

--- CSharp-Technology-ADVANCED/Labs/05FunctionalProgramming-Lab/01SortEvenNumbers/Program.cs
+++ CSharp-Technology-ADVANCED/Labs/05FunctionalProgramming-Lab/01SortEvenNumbers/Program.cs
@@ -7,8 +7,7 @@ namespace _01SortEvenNumbers
     {
         static void Main(string[] args)
         {
-            int[] numbers = Console.ReadLine().Split(new String[] { ", " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Where(n=>n%2==0).OrderBy(n=>n).ToArray();
-            Console.WriteLine(string.Join(", ", numbers));
+            Console.WriteLine(string.Join(", ", Console.ReadLine().Split(new String[] { ", " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Where(n => n % 2 == 0).OrderBy(n => n).ToArray()));
         }
     }
 }
