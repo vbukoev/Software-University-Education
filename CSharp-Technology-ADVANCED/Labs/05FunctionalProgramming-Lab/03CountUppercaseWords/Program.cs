77e1c22883f68d64650e2af23f6264488ce9b864
Vasil Bukoev <vbukoev@gmail.com>
2022-09-27 18:46:09 +0300

    added a comment to the 03 task

--- CSharp-Technology-ADVANCED/Labs/05FunctionalProgramming-Lab/03CountUppercaseWords/Program.cs
+++ CSharp-Technology-ADVANCED/Labs/05FunctionalProgramming-Lab/03CountUppercaseWords/Program.cs
@@ -7,7 +7,7 @@ namespace _03CountUppercaseWords
     {
         static void Main(string[] args)
         {
-            Predicate<string> check = n => n[0] == n.ToUpper()[0]; // checks the words and gets only  uppercase letter
+            Predicate<string> check = n => n[0] == n.ToUpper()[0]; // checks the words and gets only   uppercase letter // lambda expression
             var words = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Where(x=>check(x)).ToArray();
             foreach (var word in words)
             {
