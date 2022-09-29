cfd9abee178263866f1c43ed86d8cd7b9435e3d8
Vasil Bukoev <vbukoev@gmail.com>
2022-09-29 15:30:20 +0300

    added the solution for the 06 task

--- CSharp-Technology-ADVANCED/HomeWorks/06DefiningClasses-Exercise/06SpeedRacing/Program.cs
+++ CSharp-Technology-ADVANCED/HomeWorks/06DefiningClasses-Exercise/06SpeedRacing/Program.cs
@@ -1,12 +1,14 @@
-﻿using System;
-
-namespace _06SpeedRacing
+﻿namespace DefiningClasses
 {
-    internal class Program
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
