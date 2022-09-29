b4fd194471ed14ca441683f72ae45e50a2a17523
Vasil Bukoev <vbukoev@gmail.com>
2022-09-29 20:13:29 +0300

    another solution for the task

--- CSharp-Technology-ADVANCED/HomeWorks/05FunctionalProgramming-Exercise/08ListOfPredicates/Program.cs
+++ CSharp-Technology-ADVANCED/HomeWorks/05FunctionalProgramming-Exercise/08ListOfPredicates/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace _08ListOfPredicates
 {
@@ -8,20 +9,47 @@ namespace _08ListOfPredicates
     {
         static void Main(string[] args)
         {
+            List<Predicate<int>> predicates = new List<Predicate<int>>();
             int range = int.Parse(Console.ReadLine());
-            var dividers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
-            var nums = new List<int>();
-            for (int i = 1; i <= range; i++)
-            {
-                nums.Add(i);
+            List<int> dividers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            int[] nums = Enumerable.Range(1, range).ToArray();
 
-            }
-            foreach (var item in dividers)
+            foreach (var divider in dividers)
             {
-                Func<int, bool> filter = x => x % item == 0;
-                nums = nums.Where(filter).ToList();
+                predicates.Add(p => p % divider == 0);
             }
-            Console.WriteLine(String.Join(" ", nums));
+            foreach (var num in nums)
+            {
+                bool isDivisable = true;
+                foreach (var predicate in predicates)
+                {
+                    if (!predicate(num))
+                    {
+                        isDivisable = false;
+                        break;
+                    }
+                }
+                if (isDivisable)
+                {
+                    Console.Write(num + " ");
+                }
+            }
+
+
+            //int range = int.Parse(Console.ReadLine());
+            //var dividers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            //var nums = new List<int>();
+            //for (int i = 1; i <= range; i++)
+            //{
+            //    nums.Add(i);
+
+            //}
+            //foreach (var item in dividers)
+            //{
+            //    Func<int, bool> filter = x => x % item == 0;
+            //    nums = nums.Where(filter).ToList();
+            //}
+            //Console.WriteLine(String.Join(" ", nums));
         }
     }
 }
