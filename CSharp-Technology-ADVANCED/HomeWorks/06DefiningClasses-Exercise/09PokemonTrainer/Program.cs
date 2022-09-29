8387202fc345a8a304152dae63ce3a2dde888246
Vasil Bukoev <vbukoev@gmail.com>
2022-09-29 17:56:02 +0300

    09PokemonTrainer

--- CSharp-Technology-ADVANCED/HomeWorks/06DefiningClasses-Exercise/09PokemonTrainer/Program.cs
+++ CSharp-Technology-ADVANCED/HomeWorks/06DefiningClasses-Exercise/09PokemonTrainer/Program.cs
@@ -1,12 +1,66 @@
-﻿using System;
-
-namespace _09PokemonTrainer
+﻿namespace DefiningClasses
 {
-    internal class Program
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var trainers = new Dictionary<string, Trainer>();            
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == "Tournament")
+                {
+                    break;
+                }
+                var tokens = input.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                var trainerName = tokens[0];
+                var pokemonName = tokens[1];
+                var pokemonElement = tokens[2];
+                int health = int.Parse(tokens[3]);
+                var pokemon = new Pokemon(pokemonName, pokemonElement, health);
+                if (!trainers.ContainsKey(trainerName))
+                {
+                    trainers.Add(trainerName, new Trainer(trainerName));
+                }
+                trainers[trainerName].AddPokemon(pokemon);
+
+            }
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == "End")
+                {
+                    break;
+                }
+                if (input == "Fire")
+                {
+                    foreach (var item in trainers.Values)
+                    {
+                        item.Check(input);
+                    }
+                }
+                else if (input == "Water")
+                {
+                    foreach (var item in trainers.Values)
+                    {
+                        item.Check(input);
+                    }
+                }
+                else if (input == "Electricity")
+                {
+                    foreach (var item in trainers.Values)
+                    {
+                        item.Check(input);
+                    }
+                }
+            }
+            var res = trainers.Values.OrderByDescending(x => x.Badges).ToList();
+            foreach (var item in res) Console.WriteLine(item);
         }
+
     }
 }
---
+++ CSharp-Technology-ADVANCED/HomeWorks/06DefiningClasses-Exercise/09PokemonTrainer/Trainer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Trainer
+    {
+        private List<Pokemon> pokemons;
+        public string Name { get; set; }
+        public int Badges { get; set; } 
+        public Trainer(string name)
+        {
+            Name = name;
+            Badges = 0;
+            pokemons = new List<Pokemon>();
+        }
+        public void AddPokemon(Pokemon pokemon)
+        {
+            pokemons.Add(pokemon);
+        }
+        public void Check(string element)
+        {
+            foreach (var item in pokemons)
+            {
+                if (item.Element == element)
+                {
+                    Badges++;
+                    break;
+                }
+                else
+                {
+                    item.Health = item.Health - 10;
+                }
+            }
+            for (int i = 0; i < pokemons.Count; i++)
+            {
+                Pokemon pokemon = pokemons[i];
+                if (pokemon.Health <= 0)
+                {
+                    pokemons.Remove(pokemon);
+                    i--;
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return $"{Name} {Badges} {pokemons.Count}";
+        }
+    }
+}
