f1e5f88dd2ef3ee361cbadfe21d91ef5de88bfe9
Vasil Bukoev <vbukoev@gmail.com>
2022-09-29 19:16:32 +0300

    removed empty lines

--- CSharp-Technology-ADVANCED/HomeWorks/05FunctionalProgramming-Exercise/05AppliedArithmetics/Program.cs
+++ CSharp-Technology-ADVANCED/HomeWorks/05FunctionalProgramming-Exercise/05AppliedArithmetics/Program.cs
@@ -34,55 +34,55 @@ namespace _05AppliedArithmetics
             //        }
             //}
 
-            Action<List<int>> subtract = numbers =>
-            {
-                for (int i = 0; i < numbers.Count; i++)
-                {
-                    numbers[i]--;
-                }
-            };
-            Action<List<int>> multiply = numbers =>
-            {
-                for (int i = 0; i < numbers.Count; i++)
-                {
-                    numbers[i]*=2;
-                }
-            };
-            Action<List<int>> add = numbers =>
-            {
-                for (int i = 0; i < numbers.Count; i++)
-                {
-                    numbers[i]++;
-                }
-            };
+            //Action<List<int>> subtract = numbers =>
+            //{
+            //    for (int i = 0; i < numbers.Count; i++)
+            //    {
+            //        numbers[i]--;
+            //    }
+            //};
+            //Action<List<int>> multiply = numbers =>
+            //{
+            //    for (int i = 0; i < numbers.Count; i++)
+            //    {
+            //        numbers[i]*=2;
+            //    }
+            //};
+            //Action<List<int>> add = numbers =>
+            //{
+            //    for (int i = 0; i < numbers.Count; i++)
+            //    {
+            //        numbers[i]++;
+            //    }
+            //};
 
-            Action<List<int>> print = numbers => Console.WriteLine(String.Join(" ", numbers));
-            List<int> nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
-            while (true)
-            {
-                var cmd = Console.ReadLine();
-                if (cmd == "end") break;
-                switch (cmd)
-                {
-                    case "add":
-                        add(nums);
-                        break;
+            //Action<List<int>> print = numbers => Console.WriteLine(String.Join(" ", numbers));
+            //List<int> nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            //while (true)
+            //{
+            //    var cmd = Console.ReadLine();
+            //    if (cmd == "end") break;
+            //    switch (cmd)
+            //    {
+            //        case "add":
+            //            add(nums);
+            //            break;
 
-                    case "subtract":
-                        subtract(nums);
-                        break;
+            //        case "subtract":
+            //            subtract(nums);
+            //            break;
                         
-                    case "multiply":
-                        multiply(nums);
-                        break;
+            //        case "multiply":
+            //            multiply(nums);
+            //            break;
 
-                    case "print":
-                        print(nums);
-                        break;
-                    default:
-                        break;
-                }
-            }
+            //        case "print":
+            //            print(nums);
+            //            break;
+            //        default:
+            //            break;
+            //    }
+            //}
         }
     }
 }
