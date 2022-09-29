28f7fb12bb76c687f8a5da1e844a39315fcea3cf
Vasil Bukoev <vbukoev@gmail.com>
2022-09-29 17:36:56 +0300

    08CarSalesman

---
+++ CSharp-Technology-ADVANCED/HomeWorks/06DefiningClasses-Exercise/08CarSalesman/Engine.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace DefiningClasses
+{
+    public class Engine
+    {
+        public string Model { get; set; }
+        public int Power { get; set; }
+        public int Displacement { get; set; }
+        public string Efficiency { get; set; }
+        public Engine(string model, int power)
+        {
+            this.Model = model;
+            this.Power = power;
+        }
+        public Engine(string model, int power, int displacement) : this(model, power)
+        {
+            this.Displacement = displacement;
+        }
+        public Engine(string model, int power, string efficiency) : this(model, power)
+        {
+            this.Efficiency = efficiency;
+        }
+        public Engine(string model, int power, int displacement, string efficiency):this(model, power, displacement)
+        {
+            this.Efficiency = efficiency;
+        }
+        public override string ToString()
+        {
+            var res = new StringBuilder();
+            res.AppendLine($" {this.Model}:");
+            res.AppendLine($"    Power: {this.Power}");
+            if (this.Displacement == 0) res.AppendLine($"    Displacement: n/a");
+            else res.AppendLine($"    Displacement: {this.Displacement}");
+            if (this.Efficiency == null) res.Append($"    Efficiency: n/a");
+            else res.Append($"    Efficiency: {this.Efficiency}");
+            return res.ToString();
+        }
+    }
+}
--- CSharp-Technology-ADVANCED/HomeWorks/06DefiningClasses-Exercise/08CarSalesman/Program.cs
+++ CSharp-Technology-ADVANCED/HomeWorks/06DefiningClasses-Exercise/08CarSalesman/Program.cs
@@ -1,12 +1,78 @@
-﻿using System;
-
-namespace _08CarSalesman
+﻿namespace DefiningClasses
 {
-    internal class Program
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Engine> engines = new List<Engine>();
+            List<Car> cars = new List<Car>();
+            var enginesCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < enginesCount; i++)
+            {
+                var tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                var model = tokens[0];
+                var power = int.Parse(tokens[1]);
+                var length = tokens.Length;
+                if (length == 2)
+                {
+                    engines.Add(new Engine(model, power));
+                }
+                else if (length == 3)
+                {
+                    if (int.TryParse(tokens[2], out int displacement))
+                    {
+                        engines.Add(new Engine(model, power, displacement));
+                    }
+                    else
+                    {
+                        engines.Add(new Engine(model, power, tokens[2]));
+                    }
+                }
+                else if (length == 4)
+                {
+                    int displacement = int.Parse(tokens[2]);
+                    var eff = tokens[3];
+                    engines.Add(new Engine(model, power, displacement, eff));
+                }
+            }
+
+            var cnt = int.Parse(Console.ReadLine());
+            for (int i = 0; i < cnt; i++)
+            {
+                var tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                var model = tokens[0];
+                var engineModel = tokens[1];
+                Engine engine = GetEngine(engines, engineModel);
+                var length = tokens.Length;
+                if (length == 2)
+                {
+                    cars.Add(new Car(model, engine));
+                }
+                else if (length == 3)
+                {
+                    if (int.TryParse(tokens[2], out int weight))
+                    {
+                        cars.Add(new Car(model, engine, weight));
+                    }
+                    else
+                    {
+                        cars.Add(new Car(model, engine, tokens[2]));
+                    }
+                }
+                else if (length == 4)
+                {
+                    int weight = int.Parse(tokens[2]);
+                    var color = tokens[3];
+                    cars.Add(new Car(model, engine, weight, color));
+                }
+            }            
+            foreach (var item in cars) Console.WriteLine(item);
+
         }
+        public static Engine GetEngine(List<Engine> engines, string engineModel) => engines.Where(x => x.Model == engineModel).FirstOrDefault(); 
     }
 }
