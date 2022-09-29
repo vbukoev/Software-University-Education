4d6cee47c0a7cdffece5708fba75d7d6139e6487
Vasil Bukoev <vbukoev@gmail.com>
2022-09-29 15:51:42 +0300

    submitting the 06 task SpeedRacing

--- CSharp-Technology-ADVANCED/HomeWorks/06DefiningClasses-Exercise/06SpeedRacing/Program.cs
+++ CSharp-Technology-ADVANCED/HomeWorks/06DefiningClasses-Exercise/06SpeedRacing/Program.cs
@@ -8,7 +8,46 @@
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            var cars = new List<Car>();
+            for (int i = 0; i < n; i++)
+            {
+                var tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string model = tokens[0];
+                double fuelAmount = double.Parse(tokens[1]);
+                double fuelConsumptionFor1km = double.Parse(tokens[2]);
+                if (!cars.Any(x=>x.Model.Contains(model)))
+                {
+                    var car = new Car(model, fuelAmount, fuelConsumptionFor1km, 0);
+                    cars.Add(car);
+                }
+            }
             
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input  == "End")
+                {
+                    break;
+                }
+                var command = input.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                var action = command[0];
+                switch (action)
+                {
+                    case "Drive":
+                        var model = command[1];
+                        var km = double.Parse(command[2]);
+                        var car = cars.Where(x => x.Model == model).FirstOrDefault();
+                        car.Drive(km);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            foreach (var item in cars)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
---
+++ CSharp-Technology-ADVANCED/HomeWorks/06DefiningClasses-Exercise/07RawData/Car.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public double FuelAmount { get; set; }
+        public double FuelConsumptionPerKilometer { get; set; }
+        public double Distance { get; set; }
+        public Car() { }
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer, double distance) 
+        {
+            this.Model = model;
+            this.FuelAmount = fuelAmount;
+            this.FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+            this.Distance = distance;
+        }
+        public void Drive(double km)
+        {
+            bool canMove = this.FuelAmount >= km * FuelConsumptionPerKilometer;
+            if (canMove)
+            {
+                this.FuelAmount = FuelAmount-  (km * FuelConsumptionPerKilometer);
+                this.Distance = Distance+  km;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+        public override string ToString()
+        {
+            return $"{this.Model} {this.FuelAmount:f2} {this.Distance}"; 
+        }
+    }
+}
