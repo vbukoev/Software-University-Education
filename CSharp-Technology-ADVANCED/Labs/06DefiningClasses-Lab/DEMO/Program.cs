c05a95669ee053de994b3ff8d87b4f1ff6a262d0
Vasil Bukoev <vbukoev@gmail.com>
2022-09-30 18:22:20 +0300

    added a scheme to a demo file

---
+++ CSharp-Technology-ADVANCED/Labs/06DefiningClasses-Lab/DEMO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DEMO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //     OOP -> Object oriented programming -> Everything is an object
+            //      |                          __________________________
+            //      |                          |      - STUDENT -       |
+            //      |                          |                        |
+            //      |                          |  +string username      |
+            //      |                          |  +int Age              |
+            //      |                          |  +double AverageGrade  |
+            //      |
+            //      V
+            //    Class
+            //      |
+            //      V
+            //    Object 
+        }
+    }
+}
