bc84e116fd8630b3362891d716cd31c39a71643d
Vasil Bukoev <vbukoev@gmail.com>
2022-09-30 20:55:58 +0300

    commit

---
+++ CSharp-Technology-ADVANCED/Labs/06DefiningClasses-Lab/Clock/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading; 
+namespace Clock
+{
+    class prExclass
+    {
+        public void ShowTime()
+        {
+            for (; ; )
+            {
+                Console.WriteLine(DateTime.Now.ToString());
+                Console.WriteLine("\a");
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+        }
+        static void Main(string[] args)
+        {
+            prExclass PC = new prExclass();
+            ThreadStart TS = new ThreadStart(PC.ShowTime);
+            Thread t = new Thread(TS);
+            t.Start();
+            Console.ReadLine();
+        }
+    }
+}
--- CSharp-Technology-ADVANCED/Labs/06DefiningClasses-Lab/DEMO/Program.cs
+++ CSharp-Technology-ADVANCED/Labs/06DefiningClasses-Lab/DEMO/Program.cs
@@ -1,24 +1,20 @@
-﻿using System;
+﻿/*
+  
+ 
+                OOP -> Object oriented programming -> Everything is an object
+                 |                          __________________________
+                 |                          |      - STUDENT -       |
+                 |                          |                        |
+                 |                          |  +string username      |
+                 |                          |  +int Age              |
+                 |                          |  +double AverageGrade  |
+                 |
+                 V
+               Class
+                 |
+                 V
+               Object 
 
-namespace DEMO
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            //   OOP -> Object oriented programming -> Everything is an object
-            //    |                          __________________________
-            //    |                          |      - STUDENT -       |
-            //    |                          |                        |
-            //    |                          |  +string username      |
-            //    |                          |  +int Age              |
-            //    |                          |  +double AverageGrade  |
-            //    |
-            //    V
-            //  Class
-            //    |
-            //    V
-            //  Object 
-        }
-    }
-}
+
+ 
+ */
