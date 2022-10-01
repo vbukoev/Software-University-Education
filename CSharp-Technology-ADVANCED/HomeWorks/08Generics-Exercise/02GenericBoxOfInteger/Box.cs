3ef6f836da030e5adde0f50d62cdf99557ce5373
Vasil Bukoev <vbukoev@gmail.com>
2022-10-01 14:34:58 +0300

    task submission

---
+++ CSharp-Technology-ADVANCED/HomeWorks/08Generics-Exercise/02GenericBoxOfInteger/Box.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01GenericBoxOfString
+{
+    public class Box<T>
+    {
+        private List<T> values;
+        public Box()
+        {
+            this.values = new List<T>();
+        }
+        public void Add(T value)
+        {
+            this.values.Add(value);
+        }
+        public override string ToString()
+        {
+            var res = new StringBuilder();
+            foreach (var item in this.values)
+            {
+                res.AppendLine($"{item.GetType()}: {item}");
+            }
+            return res.ToString();
+        }
+    }
+}
---
+++ CSharp-Technology-ADVANCED/HomeWorks/08Generics-Exercise/04GenericSwapMethodIntegers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _01GenericBoxOfString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var box = new Box<int>();
+            for (int i = 0; i < n; i++)
+            {
+                var input = int.Parse(Console.ReadLine());
+                box.Add(input);
+            }
+            var swap = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int first = swap[0];
+            int second = swap[1];
+            box.Swap(first, second);
+            Console.WriteLine(box);
+        }
+    }
+}
