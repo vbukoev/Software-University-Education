dc53f89ee1fa9a76f0b3a576a84bdf62ee7412ff
Vasil Bukoev <vbukoev@gmail.com>
2022-10-01 15:25:08 +0300

    08Threeuple

---
+++ CSharp-Technology-ADVANCED/HomeWorks/08Generics-Exercise/06GenericCountMethodDouble/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _01GenericBoxOfString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var box = new Box<double>();
+            for (int i = 0; i < n; i++)
+            {
+                var input = double.Parse(Console.ReadLine());
+                box.Add(input);
+            }
+            var value = double.Parse(Console.ReadLine());
+            var greater = box.GreaterCount(value);
+            Console.WriteLine(greater);
+        }
+    }
+}
---
+++ CSharp-Technology-ADVANCED/HomeWorks/08Generics-Exercise/07Tuple/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _07Tuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var data = Console.ReadLine().Split().ToArray();
+            var name = $"{data[0]} {data[1]}";
+            var adress = data[2];
+            var info = new Tuple<string, string>(name, adress);
+            Console.WriteLine(info);
+
+            var beerData = Console.ReadLine().Split().ToArray();
+            var beerName = beerData[0];
+            var littersBeer = int.Parse(beerData[1]);
+            var beerInfo = new Tuple<string, int>(beerName, littersBeer);
+            Console.WriteLine(beerInfo);
+            
+            var line = Console.ReadLine().Split().ToArray();
+            var firstLine = int.Parse(line[0]);
+            var secLine = double.Parse(line[1]);
+            var tuple = new Tuple<int, double>(firstLine, secLine);
+            Console.WriteLine(tuple);
+
+        }
+    }
+}
