735a7e0f6ba4741106d2a7836f3e09eb86bc2217
Vasil Bukoev <vbukoev@gmail.com>
2022-10-01 12:28:35 +0300

    GenericArrayCreator

---
+++ CSharp-Technology-ADVANCED/Labs/08Generics-Lab/GenericArrayCreator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = ArrayCreator.Create(5, "Pesho");
+            int[] integers = ArrayCreator.Create(110_122_222, 33);
+            foreach (var @string in strings) Console.Write(@string + " ");
+            Console.WriteLine();
+            foreach (long @integer in integers) Console.Write(@integer + " ");
+            Console.WriteLine();
+            for (int i = 0; i < strings.Length; i++) Console.Write (strings[i] + " ");
+             
+
+        }
+    }
+}
