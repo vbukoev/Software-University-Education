323c27a3ada47af9a2c9080bffc44f3f9045830a
Vasil Bukoev <vbukoev@gmail.com>
2022-10-01 12:45:44 +0300

    03 task and changed the name of the method

--- CSharp-Technology-ADVANCED/Labs/08Generics-Lab/GenericArrayCreator/Program.cs
+++ CSharp-Technology-ADVANCED/Labs/08Generics-Lab/GenericArrayCreator/Program.cs
@@ -10,7 +10,7 @@ namespace GenericArrayCreator
             int[] integers = ArrayCreator.Create(110_122_222, 33);
             foreach (var @string in strings) Console.Write(@string + " ");
             Console.WriteLine();
-            foreach (long @integer in integers) Console.Write(@integer + " ");
+            foreach (int @integer in integers) Console.Write(@integer + " ");
             Console.WriteLine();
             for (int i = 0; i < strings.Length; i++) Console.Write (strings[i] + " ");
              
---
+++ CSharp-Technology-ADVANCED/Labs/08Generics-Lab/GenericScale/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var a = Console.ReadLine();
+            var b = Console.ReadLine();
+            var scale = new EqualityScale<string>(a, b);
+            Console.WriteLine(scale.AreEqual());
+        }
+    }
+}
