7f4cfbda5a9c3f2a83e40b4c142cd5e33009ec52
Vasil Bukoev <vbukoev@gmail.com>
2022-10-02 16:12:52 +0300

    04 task submission

--- CSharp-Technology-ADVANCED/Labs/09IteratorsAndComparators-Lab/04BookComparator/Program.cs
+++ CSharp-Technology-ADVANCED/Labs/09IteratorsAndComparators-Lab/04BookComparator/Program.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿namespace IteratorsAndComparators
 
-namespace _04BookComparator
 {
-    internal class Program
+    using System;
+    public class StartUp
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
-        }
+            Book bookOne = new Book("Animal Farm", 2003, "George Orwell");//we get a new bookOne
+            Book bookTwo = new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustace");//we get a new bookTwo
+            Book bookThree = new Book("The Documents in the Case", 1930);//we get a new bookThree
+
+            Library library = new Library(bookOne, bookTwo, bookThree); // in the class library we store the book 
+            // with a foreach loop we are going to print the books to the console
+            foreach (var item in library)
+            {
+                Console.WriteLine(item);
+            }
+        } 
+
     }
 }
