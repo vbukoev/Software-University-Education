7ebadca6a0fa7aba48eedc1a9bb2a9330736538d
Vasil Bukoev <vbukoev@gmail.com>
2022-10-02 15:06:10 +0300

    submit the task in judge and submit it in github

---
+++ CSharp-Technology-ADVANCED/Labs/09IteratorsAndComparators-Lab/02LibraryIterator/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book
+    {
+        public string Title { get; private set; }
+        public int Year { get; private set; }
+        public List<string> Authors { get; private set; }
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = new List<string>(authors);
+        }
+    }
+}
---
+++ CSharp-Technology-ADVANCED/Labs/09IteratorsAndComparators-Lab/02LibraryIterator/Library.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+        public void Add(Book book)
+        {
+            books.Add(book);
+        }
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(this.books);
+        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private List<Book> books;
+            private int index;
+            public LibraryIterator(List<Book> books)
+            {
+                this.books = books;
+                this.index = -1;
+            }
+            public Book Current => this.books[this.index];
+            object IEnumerator.Current => Current;
+            public void Dispose() { }
+
+            bool IEnumerator.MoveNext()
+            {
+                index += 1;
+                if (index < books.Count)
+                {
+                    return true;
+                }
+                return false;
+            }
+
+            void IEnumerator.Reset()
+            {
+                index = -1;
+            }
+        }
+    }
+}
