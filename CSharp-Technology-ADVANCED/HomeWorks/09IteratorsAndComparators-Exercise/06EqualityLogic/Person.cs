c7f7bff78265d9dd8f959525925880a0e5396c1c
Vasil Bukoev <vbukoev@gmail.com>
2022-10-02 18:20:59 +0300

    06 and 07 task submitted

---
+++ CSharp-Technology-ADVANCED/HomeWorks/09IteratorsAndComparators-Exercise/06EqualityLogic/Person.cs
@@ -0,0 +1,31 @@
+﻿namespace IteratorsAndComparators
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    public class Person : IComparable<Person>
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }        
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public int CompareTo(Person other)
+        {
+            var result = Name.CompareTo(other.Name);
+            if (result == 0) return Age - other.Age;
+            return result;
+        }
+        public override bool Equals(object obj)
+        {
+            return Name == ((Person)obj).Name && Age == ((Person)obj).Age;
+        }
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() +  Age.GetHashCode() + (257 * 53);
+        }
+    }
+    }
+
---
+++ CSharp-Technology-ADVANCED/HomeWorks/09IteratorsAndComparators-Exercise/07CustomComparator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _07CustomComparator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var even = numbers.Where(x => x % 2 == 0).OrderBy(x=>x).ToArray();
+            var oods = numbers.Where(x => x % 2 != 0).OrderBy(x => x).ToArray();
+            var nums = even.Concat(oods).ToArray(); 
+            Console.WriteLine(string.Join(" ", nums));
+        }
+    }
+}
