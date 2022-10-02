9587dbe8c4a0e0dd7b86726ae05754554084e01c
Vasil Bukoev <vbukoev@gmail.com>
2022-10-02 17:22:48 +0300

    03 and 04 task submission

---
+++ CSharp-Technology-ADVANCED/HomeWorks/09IteratorsAndComparators-Exercise/03Stack/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace IteratorsAndComparators
+
+{
+using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+            var stack = new Stack<int>();
+            while (true)
+            {
+            var input = Console.ReadLine();
+                if (input == "END") break;
+                if (input.Contains("Push"))
+                {
+                    var nums = input.Split(new string[] { "Push", ", ", " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                    stack.Push(nums);
+                }
+                else if (input.Contains("Pop"))
+                {
+                    try { stack.Pop(); }
+                    catch (InvalidOperationException message) { Console.WriteLine(message.Message); }
+                }
+            }
+            foreach (var item in stack) Console.WriteLine(item); 
+            foreach (var item in stack) Console.WriteLine(item); 
+        }
+    }
+}
---
+++ CSharp-Technology-ADVANCED/HomeWorks/09IteratorsAndComparators-Exercise/04Froggy/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace IteratorsAndComparators
+
+{
+using System;
+    using System.Linq;
+    using System.Reflection;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var lake = new Lake(input);
+            foreach (var item in lake) Console.Write(item + ", ");
+        }
+    }
+}
