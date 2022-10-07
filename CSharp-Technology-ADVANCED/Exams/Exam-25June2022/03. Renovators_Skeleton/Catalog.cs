ff9f0c63ecb365db31b780db9ca3ea372601f7d7
Vasil Bukoev <vbukoev@gmail.com>
2022-10-07 15:59:11 +0300

    03 task

--- CSharp-Technology-ADVANCED/Exams/Exam-25June2022/03. Renovators_Skeleton/Catalog.cs
+++ CSharp-Technology-ADVANCED/Exams/Exam-25June2022/03. Renovators_Skeleton/Catalog.cs
@@ -2,11 +2,90 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Renovators
 {
     public class Catalog
     {
+        private string name;
+        private int needRenovators;
+        private string project;
+        private List<Renovator> renovators;
+        public Catalog(string name, int neededRenovators, string project)
+        {
+            this.Name = name;
+            this.NeededRenovators = neededRenovators;
+            this.Project = project;
+            this.renovators = new List<Renovator>();
+        }
+        
+        public IReadOnlyCollection<Renovator> Renovators => renovators;
+        public string Name { get; private set; }
+        public int NeededRenovators { get; private set; }
+        public string Project { get; private set; }
+        public int Count => this.Renovators.Count; 
+        public string AddRenovator(Renovator renovator)
+        {
+            if (string.IsNullOrEmpty(renovator.Name) || string.IsNullOrEmpty(renovator.Type))
+            {
+                return "Invalid renovator's information.";
+            }
+            if (this.Count == this.NeededRenovators)
+            {
+                return "Renovators are no more needed.";
+            }
+            if (renovator.Rate > 350)
+            {
+                return "Invalid renovator's rate.";
+            }
+            this.renovators.Add(renovator);
+            return $"Successfully added {renovator.Name} to the catalog.";
+        }
+        public bool RemoveRenovator(string name)
+        {
+            Renovator toBeRemoved = this.renovators.FirstOrDefault(x => x.Name == name);
+            if (toBeRemoved == null)
+            {
+                return false;
+            }
+            this.renovators.Remove(toBeRemoved);  
+            return true;
+        }
+        public int RemoveRenovatorBySpecialty(string type)
+        {
+            List<Renovator> toBeRemoved = this.renovators.FindAll(x => x.Type == type).ToList();
+            if (toBeRemoved.Count > 0)
+            {
+                foreach (var item in toBeRemoved)
+                {
+                    this.renovators.Remove(item);
+                }
+                return toBeRemoved.Count;
+            }
+            return 0;
+        }
+        public Renovator HireRenovator(string name)
+        {
+            Renovator sortByName = this.renovators.FirstOrDefault(x => x.Name == name);
+            if (sortByName == null)
+            {
+                return null;
+            }
+            this.Renovators.FirstOrDefault(x => x.Name == name).Hired = true;
+            return sortByName;
+        }
+        public List<Renovator> PayRenovators(int days)
+        {
+            List<Renovator> toBePaid = this.Renovators.Where(x => x.Days >= days).ToList();
+            return toBePaid;
+        }
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            var newCollection = this.Renovators.Where(x => x.Hired == false).Where(x=>x.Paid == false); //gets everybody who is not hired
+            sb.AppendLine($"Renovators available for Project {this.Project}:");
+            foreach (var item in newCollection) sb.AppendLine(item.ToString());
+            return sb.ToString().TrimEnd();
+        }
     }
 }
--- CSharp-Technology-ADVANCED/Exams/Exam-25June2022/03. Renovators_Skeleton/Renovator.cs
+++ CSharp-Technology-ADVANCED/Exams/Exam-25June2022/03. Renovators_Skeleton/Renovator.cs
@@ -1,12 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text; 
 
 namespace Renovators
 {
     public class Renovator
     {
+        private string name;
+        private string type;
+        private double rate;
+        private int days;
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public double Rate { get; set; }
+        public int Days { get; set; }
+        public bool Hired { get; set; } = false;
+        public bool Paid { get; set; } = false;
+        public Renovator(string name, string type, double rate, int days)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Rate = rate;
+            this.Days = days;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"-Renovator: {this.Name}");
+            sb.AppendLine($"--Specialty: {this.Type}");
+            sb.AppendLine($"--Rate per day: {this.Rate} BGN");
+            return sb.ToString().TrimEnd();
+        }
     }
+
 }
