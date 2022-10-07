5868a74b73eb65cabd7941103d09230b48333a98
Vasil Bukoev <vbukoev@gmail.com>
2022-10-07 20:32:59 +0300

    added comments

--- CSharp-Technology-ADVANCED/Labs/08Generics-Lab/01Box/Box.cs
+++ CSharp-Technology-ADVANCED/Labs/08Generics-Lab/01Box/Box.cs
@@ -12,11 +12,11 @@ using System.Collections.Generic;
         {
             this.elements = new List<T>();
         }
-        public void Add(T element)
+        public void Add(T element) //adding element of type "T"
         {
             this.elements.Add(element);
         }
-        public T Remove()
+        public T Remove() // we firstly take the first element and then we remove the topmost element and return last element (which is the element with the count of the elements minus 1)
         {
             T last = this.elements[this.Count - 1];
             this.elements.RemoveAt(this.Count - 1);
