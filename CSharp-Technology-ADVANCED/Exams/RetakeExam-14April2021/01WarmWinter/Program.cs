d6186180b975ee551b3e7c1fa0a676666fd9ee62
Vasil Bukoev <vbukoev@gmail.com>
2022-10-14 17:55:24 +0300

    01warm winter task submission

--- CSharp-Technology-ADVANCED/Exams/RetakeExam-14April2021/01WarmWinter/Program.cs
+++ CSharp-Technology-ADVANCED/Exams/RetakeExam-14April2021/01WarmWinter/Program.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _01WarmWinter
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stack<int> hats = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            Queue<int> scarfs = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            List<int> list = new List<int>();
+
+            while (true)
+            {
+                if (!hats.Any() || !scarfs.Any()) break;
+
+                int hat = hats.Peek();
+                int scarf = scarfs.Peek();
+
+                if (hat < scarf) hats.Pop();
+
+                else if (hat > scarf)
+                {
+                    list.Add(hat + scarf);
+                    hats.Pop();
+                    scarfs.Dequeue();
+                }
+
+                else if (hat == scarf)
+                {
+                    scarfs.Dequeue();
+                    hats.Pop();
+                    hats.Push(hat + 1);
+                }
+            }
+
+            var maxPriceSet = list.Max();
+            Console.WriteLine($"The most expensive set is: {maxPriceSet}");
+            Console.WriteLine(String.Join(" ", list));
         }
     }
 }
--- CSharp-Technology-ADVANCED/Exams/RetakeExam-14April2021/02SuperMario/Program.cs
+++ CSharp-Technology-ADVANCED/Exams/RetakeExam-14April2021/02SuperMario/Program.cs
@@ -2,11 +2,11 @@
 
 namespace _02SuperMario
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
