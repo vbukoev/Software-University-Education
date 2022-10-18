b25eaf11ad4e8a44223882a8b6b6dccf7498aa05
Vasil Bukoev <vbukoev@gmail.com>
2022-10-18 21:24:35 +0300

    added task projects

--- CSharp-Technology-ADVANCED/HomeWorks/BasicAlgorithms-Exercise/02RecursiveFactorial/Program.cs
+++ CSharp-Technology-ADVANCED/HomeWorks/BasicAlgorithms-Exercise/02RecursiveFactorial/Program.cs
@@ -6,7 +6,14 @@ namespace _02RecursiveFactorial
     {
         static void Main(string[] args)
         {
-
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Factorial(n));
+        }
+        static long Factorial(int num) 
+        {
+            if (num==0)  return 1;
+            
+            return num * Factorial(num-1);
         }
     }
 }
