eabef6868510fb052c2280c63f06a0bc817abfe5
Vasil Bukoev <vbukoev@gmail.com>
2022-10-18 20:53:18 +0300

    01 recursive array sum task

--- CSharp-Technology-ADVANCED/HomeWorks/BasicAlgorithms-Exercise/01RecursiveArraySum/Program.cs
+++ CSharp-Technology-ADVANCED/HomeWorks/BasicAlgorithms-Exercise/01RecursiveArraySum/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Linq;
 
 namespace _01RecursiveArraySum
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Console.WriteLine(Sum(array, 0));
+        }
+        private static int Sum(int[] array, int index)
+        {
+            if (index>=array.Length)
+            {
+                return 0;
+            }
+            return array[index] + Sum(array, index + 1);
         }
     }
 }
