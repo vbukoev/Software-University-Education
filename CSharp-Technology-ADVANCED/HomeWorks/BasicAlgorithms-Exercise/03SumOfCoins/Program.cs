14520df995bc00305ce7a86d84f76fb3be4ffd6c
Vasil Bukoev <vbukoev@gmail.com>
2022-10-19 21:08:16 +0300

    added project files

--- CSharp-Technology-ADVANCED/HomeWorks/BasicAlgorithms-Exercise/03SumOfCoins/Program.cs
+++ CSharp-Technology-ADVANCED/HomeWorks/BasicAlgorithms-Exercise/03SumOfCoins/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace _03SumOfCoins
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int finalSum = 18;
+            int currSum = 0;
+            int[] coins = { 10, 10, 5, 5, 2, 2, 1, 1 };
+            Queue<int> result = new Queue<int>();
+            for (int i = 0; i < coins.Length; i++)
+            {
+                if (currSum + coins[i] > finalSum) continue;
+                currSum += coins[i];
+                result.Enqueue(coins[i]);
+                if (currSum == finalSum)
+                {
+
+                }
+            }
+            Console.WriteLine("Sum not found");
         }
     }
 }
