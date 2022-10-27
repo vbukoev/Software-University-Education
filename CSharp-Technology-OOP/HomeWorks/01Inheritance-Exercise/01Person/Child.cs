153ac640408facce674e0980589be157b12ef851
Vasil Bukoev <vbukoev@gmail.com>
2022-10-27 19:28:00 +0300

    01 task submission

---
+++ CSharp-Technology-OOP/HomeWorks/01Inheritance-Exercise/01Person/Child.cs
@@ -0,0 +1,15 @@
+﻿namespace Person
+{
+    public class Child : Person
+    {
+        public Child(string name, int age) : base(name, age)
+        {
+
+        }
+        public override int Age
+        {
+            get { return base.Age; }
+            set { if(value<=15) base.Age = value; }
+        }
+    }
+}
---
+++ CSharp-Technology-OOP/HomeWorks/01Inheritance-Exercise/01Person/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Person
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            if (age > 15)
+            {
+                var person = new Person(name, age);
+                Console.WriteLine(person);
+            }
+            else
+            {
+                Child child = new Child(name, age);
+                Console.WriteLine(child);
+            }
+        }
+    }
+}
