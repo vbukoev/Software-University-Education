6ec1facaa33808e24a8d1328d0c3f58851efa874
Vasil Bukoev <vbukoev@gmail.com>
2022-10-30 17:57:06 +0200

    01 task submission

---
+++ CSharp-Technology-OOP/HomeWorks/02Encapsulation-Exercise/01ClassBoxData/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01ClassBoxData
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var length = double.Parse(Console.ReadLine());
+            var width = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine()); 
+            try
+            {
+                var box = new Box(length, width, height);
+                Console.WriteLine(box);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ CSharp-Technology-OOP/HomeWorks/02Encapsulation-Exercise/02AnimalFarm/Models/Validator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace AnimalFarm.Models
+{
+    public class Validator
+    {
+        public const int MinAge = 0;
+        public const int MaxAge = 15;
+        private const string NameCannotBeEmptyException = "Name cannot be empty.";
+        public static void ValidateName(string name)
+        {
+            if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException(NameCannotBeEmptyException);
+            }
+        }
+        public static void ValidateAge(int age)
+        {
+            if (age<MinAge || age> MaxAge)
+            {
+                throw new ArgumentException($"Age should be between {MinAge} and {MaxAge}.");
+            }
+        }
+    }
+}
