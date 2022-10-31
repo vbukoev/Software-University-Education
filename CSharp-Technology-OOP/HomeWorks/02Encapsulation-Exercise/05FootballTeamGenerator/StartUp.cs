18c470038ac90ae21fd624540f46d913ed1c61c1
Vasil Bukoev <vbukoev@gmail.com>
2022-10-31 21:19:18 +0200

    05 task submission 100/100

---
+++ CSharp-Technology-OOP/HomeWorks/02Encapsulation-Exercise/05FootballTeamGenerator/StartUp.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05FootballTeamGenerator
+{
+    public class StartUp
+    {
+        private static List<Team> teamList;
+        static void Main()
+        {
+            teamList = new List<Team>();
+            RunEngine();            
+        }
+        static void RunEngine()
+        {
+            string cmd;
+            
+            while ((cmd = Console.ReadLine()) != "END")
+            {
+                string[] cmdArgs = cmd.Split(';');
+                string cmdType = cmdArgs[0];
+                string teamName = cmdArgs[1];
+                try
+                {
+                    if (cmdType == "Team")
+                    {
+                        Team newTeam = new Team(teamName);
+                        teamList.Add(newTeam);
+                    }
+                    else if (cmdType == "Add") //adding player to a team
+                    {
+                        Team joiningTeam = teamList.FirstOrDefault(t => t.Name == teamName);
+                        if (joiningTeam == null)
+                        {
+                            throw new InvalidOperationException(string.Format(ExceptionMessages.InexistingTeamMessage, teamName));
+                        }
+                        string playerName = cmdArgs[2];
+                        int endurance = int.Parse(cmdArgs[3]);
+                        int sprint = int.Parse(cmdArgs[4]);
+                        int dribble = int.Parse(cmdArgs[5]);
+                        int passing = int.Parse(cmdArgs[6]);
+                        int shooting = int.Parse(cmdArgs[7]);
+                        Player joiningPlayer = new Player(playerName, endurance, sprint, dribble, passing, shooting);
+                        joiningTeam.AddPlayer(joiningPlayer);
+                    }
+                    else if (cmdType == "Remove")
+                    {
+                        string playerName = cmdArgs[2];
+                        Team removingTeam = teamList.FirstOrDefault(x => x.Name == teamName);
+                        if (removingTeam == null)
+                        {
+                            throw new InvalidOperationException(string.Format(ExceptionMessages.InexistingTeamMessage, teamName));
+                        }
+                        removingTeam.RemovePlayer(playerName);
+                    }
+                    else if (cmdType == "Rating")
+                    {
+                        Team teamToRate = teamList.FirstOrDefault(x => x.Name == teamName);
+                        if (teamToRate == null)
+                        {
+                            throw new InvalidOperationException(string.Format(ExceptionMessages.InexistingTeamMessage, teamName));
+                        }
+                        Console.WriteLine(teamToRate);
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (InvalidOperationException ioe)
+                {
+                    Console.WriteLine(ioe.Message);
+                }
+               
+            }
+        }
+    }
+}
---
+++ CSharp-Technology-OOP/HomeWorks/02Encapsulation-Exercise/05FootballTeamGenerator/Stats.cs
@@ -0,0 +1,100 @@
+﻿namespace _05FootballTeamGenerator
+{
+    using System;
+    public class Stats
+    {
+        private int endurance;
+        private int sprint;
+        private int dribble;
+        private int passing;
+        private int shooting;
+        public Stats(int endurance, int sprint, int dribble, int passing, int shooting)
+        {
+            this.Endurance = endurance;
+            this.Sprint = sprint;
+            this.Dribble = dribble;
+            this.Passing = passing;
+            this.Shooting = shooting;
+        }
+
+        public int Endurance
+        {
+            get
+            {
+                return this.endurance;
+            }
+            private set
+            {
+                if (this.isStatInvalid(value))
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidStatMessage, nameof(this.Endurance)));
+                }
+                this.endurance = value;
+            }
+        }
+        public int Sprint
+        {
+            get
+            {
+                return this.sprint;
+            }
+            private set
+            {
+                if (this.isStatInvalid(value))
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidStatMessage, nameof(this.Sprint)));
+                }
+                this.sprint = value;
+            }
+        }
+        public int Dribble
+        {
+            get
+            {
+                return this.dribble;
+            }
+            private set
+            {
+                if (this.isStatInvalid(value))
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidStatMessage, nameof(this.Dribble)));
+                }
+                this.dribble = value;
+            }
+        }
+        public int Passing
+        {
+            get
+            {
+                return this.passing;
+            }
+            private set
+            {
+                if (this.isStatInvalid(value))
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidStatMessage, nameof(this.Passing)));
+                }
+                this.passing = value;
+            }
+        }
+        public int Shooting
+        {
+            get
+            {
+                return this.shooting;
+            }
+            private set
+            {
+                if (this.isStatInvalid(value))
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidStatMessage, nameof(this.Shooting)));
+                }
+                this.shooting = value;
+            }
+        }
+        private bool isStatInvalid(int value) // returns true if the stat is invalid (<0 || > 100), if the stat is valid returns false;
+        {
+            return value < 0 || value > 100;
+        }
+    }
+}
