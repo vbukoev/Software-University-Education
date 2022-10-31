2f2ec886662b57cf8bc5fb51904a19d80cdbf781
Vasil Bukoev <vbukoev@gmail.com>
2022-10-31 09:30:26 +0200

    04 task 100/100 submission

---
+++ CSharp-Technology-OOP/HomeWorks/02Encapsulation-Exercise/04PizzaCalories/Dough.cs
@@ -0,0 +1,69 @@
+﻿using System;
+namespace _04PizzaCalories
+{
+    public class Dough
+    {
+        //White - 1.5
+        //Wholegrain - 1.0
+        //Crispy - 0.9
+        //Chewy - 1.1
+        //Homemade - 1.0
+        private const double White = 1.5;
+        private const double Wholegrain = 1.0;
+        private const double Crispy = 0.9;
+        private const double Chewy=1.1;
+        private const double Homemade = 1;
+        private string flourType;
+        private string backingTechnique;
+        private double grams;
+        private string FlourType
+        {
+            set
+            {
+                if (value.ToLower() != "white" && value.ToLower() != "wholegrain") throw new ArgumentException("Invalid type of dough.");
+                flourType = value;
+            }
+        }
+        private string BackingTechnique
+        {
+            set 
+            { 
+                if (value.ToLower() != "crispy" && value.ToLower() != "chewy" && value.ToLower() != "homemade") 
+                throw new ArgumentException("Invalid type of dough.");
+                backingTechnique = value;
+            }
+        }
+        private double Grams
+        {
+            set
+            {
+                if (value < 1|| value >200)
+                    throw new ArgumentException("Dough weight should be in the range [1..200].");
+                grams = value;
+            }
+        }
+        private double CaloriesPerGram
+        {
+            get
+            {
+                double getColories = 2;
+                if (this.flourType.ToLower() == "white") getColories *= White;
+                else if (this.flourType.ToLower() == "wholegrain") getColories *= Wholegrain;
+                if (backingTechnique.ToLower() == "crispy") getColories *= Crispy;  
+                else if (backingTechnique.ToLower() == "chewy") getColories *= Chewy;  
+                else if (backingTechnique.ToLower() == "homemade") getColories *= Homemade;  
+                return getColories;
+            }
+        }
+        public Dough(string flourType, string backingTechnique, double grams)
+        {
+            FlourType = flourType;
+            BackingTechnique = backingTechnique;
+            this.Grams = grams; 
+        }
+        public double GetCalories()
+        {
+            return grams * CaloriesPerGram;
+        }
+    }
+}
---
+++ CSharp-Technology-OOP/HomeWorks/02Encapsulation-Exercise/04PizzaCalories/Topping.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04PizzaCalories
+{
+    public class Topping
+    {
+        //Meat - 1.2;
+        //Veggies - 0.8;
+        //Cheese - 1.1;
+        //Sauce - 0.9;
+        private const double Meat = 1.2;
+        private const double Veggies = 0.8;
+        private const double Cheese = 1.1;
+        private const double Sauce = 0.9;
+        private string type;
+        private double grams;
+        private double Grams
+        {
+            set
+            {
+                if (value < 1 || value > 50)
+                    throw new ArgumentException($"{type} weight should be in the range [1..50].");
+                grams = value;
+            }
+        }        
+        private string Type
+        {
+            set
+            {
+                if (!new List<string> {"meat", "veggies", "cheese", "sauce"}.Contains(value.ToLower()))
+                    throw new ArgumentException($"Cannot place {value} on top of your pizza.");
+                type = value;
+            }
+        }
+        public double CaloriesPerGram
+        {
+            get
+            {
+                double calories = 2;
+                switch (type.ToLower())
+                {
+                    case "meat": calories *= Meat; break;
+                    case "veggies": calories *= Veggies; break;
+                    case "cheese": calories *= Cheese; break;
+                    case "sauce": calories *= Sauce; break;
+
+                }
+                return calories;
+            }
+        }
+        public Topping(string type, double grams)
+        {
+            Type = type;
+            Grams = grams; 
+        }
+        public double GetCalories()
+        {
+            return grams * CaloriesPerGram;
+        }
+    }
+}
