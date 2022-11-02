04f3ac5bc59bab9d90ed3ebcffa51877978b6080
Vasil Bukoev <vbukoev@gmail.com>
2022-11-02 20:59:05 +0200

    Cars task submission

---
+++ CSharp-Technology-OOP/Labs/03InterfacesAndAbstraction-Lab/02Cars/Seat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color; 
+        }
+
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public string Start { get; set; }
+        public string Stop { get; set; }
+        public override string ToString()
+        {
+            return $"{Color} Seat {Model}";
+        }
+    }
+}
