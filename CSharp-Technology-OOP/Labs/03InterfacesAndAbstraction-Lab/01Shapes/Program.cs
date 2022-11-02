d1b94416538b45b83e74f454c7cef38ecf6a6a8b
Vasil Bukoev <vbukoev@gmail.com>
2022-11-02 19:37:51 +0200

    added project files

---
+++ CSharp-Technology-OOP/Labs/03InterfacesAndAbstraction-Lab/01Shapes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01Shapes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
