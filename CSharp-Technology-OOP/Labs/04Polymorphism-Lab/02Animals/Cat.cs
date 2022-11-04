ec71dd62e06bd953fe4076f1130a2fc04039c713
Vasil Bukoev <vbukoev@gmail.com>
2022-11-04 20:41:16 +0200

    2nd task submission "Animals" task

---
+++ CSharp-Technology-OOP/Labs/04Polymorphism-Lab/02Animals/Cat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Cat : Animal
+    {
+        public Cat(string name, string favouriteFood) : base(name, favouriteFood)
+        {
+
+        }
+        public override string ExplainSelf()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(base.ExplainSelf());
+            sb.AppendLine("MEEOW");
+            return sb.ToString();
+        }
+    }
+}
