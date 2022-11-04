56904cdfd4e66d345b3ff0f573d6ca7001c67a02
Vasil Bukoev <vbukoev@gmail.com>
2022-11-04 21:06:29 +0200

    03 task from the lab submission

---
+++ CSharp-Technology-OOP/Labs/04Polymorphism-Lab/03Shapes/Exceptions/InvalidRadiusException.cs
@@ -0,0 +1,15 @@
+﻿namespace Shapes.Exceptions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+
+    public class InvalidRadiusException : Exception
+    {
+        private const string DEFAULT_MESSAGE = "Radius must be a positive number!";
+        public InvalidRadiusException() : base(DEFAULT_MESSAGE) 
+        { 
+        }
+    }
+}
---
+++ CSharp-Technology-OOP/Labs/04Polymorphism-Lab/03Shapes/Models/Circle.cs
@@ -0,0 +1,35 @@
+﻿namespace Shapes.Models
+{
+    using Shapes.Exceptions;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public class Circle : Shape
+    {
+        private double radius;
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public double Radius
+        {
+            get { return radius; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new InvalidRadiusException();
+                }
+                radius = value;
+            }
+        }
+
+        public override double CalculateArea()
+        => Math.PI * Math.Pow(this.Radius, 2);
+
+        public override double CalculatePerimeter()
+        => 2 * Math.PI * this.Radius;
+        public override string Draw()
+        => base.Draw() + $" {this.GetType().Name}";
+    }
+}
