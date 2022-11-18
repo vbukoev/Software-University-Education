ee78669a1760b5c6d865b00b63b3eacb1f87f280
Vasil Bukoev <vbukoev@gmail.com>
2022-11-18 09:38:12 +0200

    adding the lab for unit tests

---
+++ CSharp-OOP/Labs/08UnitTesting-Lab/01TestAxe/Skeleton.Tests/AxeTests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class AxeTests
+    {
+        [Test]
+        public void Test1()
+        {
+
+        }
+    }
+}
