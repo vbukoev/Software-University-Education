d1a41f8f07b1922c7a39b1c6ec18cf32bdea4124
Vasil Bukoev <vbukoev@gmail.com>
2022-11-25 18:07:10 +0200

    submission of the unit tests (last tasks)

--- CSharp-OOP/Exams/Exam-11December2021/03. Unit Tests_Skeleton/UnitTests-Skeleton/Gyms.Tests/GymsTests.cs
+++ CSharp-OOP/Exams/Exam-11December2021/03. Unit Tests_Skeleton/UnitTests-Skeleton/Gyms.Tests/GymsTests.cs
@@ -1,7 +1,108 @@
-﻿namespace Gyms.Tests
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using NUnit.Framework;
+
+namespace Gyms.Tests
 {
     public class GymsTests
     {
-        // Implement unit tests here
+        private const string AthleteName = "Athlete";
+        private const string GymName = "Gym";
+
+        private const int GymSize = 2;
+        private Athlete athlete;
+        private Gym gym;
+
+        [SetUp]
+        public void SetUp()
+        {
+            athlete = new Athlete(AthleteName);
+            gym = new Gym(GymName, GymSize);
+            gym.AddAthlete(athlete);
+        }
+
+        [Test]
+        public void AthleteConstructor()
+        {
+            Assert.True(athlete.FullName == AthleteName &&
+                        !athlete.IsInjured);
+        }
+
+        [Test]
+        public void GymConstructor()
+        {
+            gym = new Gym(GymName, GymSize);
+            Assert.True(gym.Name == GymName &&
+                        gym.Capacity == GymSize &&
+                        gym.Count == 0);
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        public void GymNameCannotBeNullOrEmpty(string name)
+        {
+            Assert.Throws<ArgumentNullException>(() => gym = new Gym(name, GymSize));
+        }
+
+        [Test]
+        public void GymCapacityCannotBeNegativeNumber()
+        {
+            Assert.Throws<ArgumentException>(() => gym = new Gym(GymName, -1));
+        }
+
+        [Test]
+        public void AddAthleteCorrectlyIncreasesTheCount()
+        {
+            Assert.AreEqual( gym.Count, 1);
+        }
+
+        [Test]
+        public void AddAthleteThrowsWhenThereIsANullGym()
+        {
+            var athlete = new Athlete("athlete");
+            var athlete2 = new Athlete("athlete2");
+            gym.AddAthlete(athlete);
+            Assert.Throws<InvalidOperationException>(()=>gym.AddAthlete(athlete2));
+        }
+
+        [Test]
+        public void RemoveAthleteDecreasesCount()
+        {
+            gym.RemoveAthlete(AthleteName);
+            Assert.AreEqual(gym.Count, 0);
+        }
+
+        [Test]
+        public void RemoveAthleteThrowsWhenThereIsNotAAthlete()
+        {
+            Assert.Throws<InvalidOperationException>(() => gym.RemoveAthlete("name"));
+        }
+
+        [Test]
+        public void InjureAthleteThrowsWhenThereIsNoAthlete()
+        {
+            Assert.Throws<InvalidOperationException>(() => gym.InjureAthlete("name"));
+        }
+
+        [Test]
+        public void InjureAthleteSetsTheBoolVariableInItToTrue()
+        {
+            Athlete athlete = gym.InjureAthlete(AthleteName);
+            Assert.IsTrue(athlete.IsInjured && athlete.FullName == AthleteName);
+        }
+
+        [Test]
+        public void ReportIsCorrect()
+        {
+            Athlete athlete2 = new Athlete("name");
+            List<Athlete> athletes = new List<Athlete>();
+            athletes.Add(this.athlete);
+            athletes.Add(athlete2);
+            gym.AddAthlete(athlete2);
+            gym.InjureAthlete(athlete2.FullName);
+            Assert.AreEqual($"Active athletes at {GymName}: {string.Join(", ", athletes.Where(x => !x.IsInjured).Select(x => x.FullName))}", gym.Report());
+        }
     }
 }
