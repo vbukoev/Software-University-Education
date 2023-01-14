3ff49f8a6e8308d4d1f838449cbba7d66aba6386
Vasil Bukoev <vbukoev@gmail.com>
2023-01-14 10:24:35 +0200

    adding the first task from the relations exercise

--- CSharp-MS-SQL/Homeworks/03TableRelations/01One-To-OneRelationship.sql
+++ CSharp-MS-SQL/Homeworks/03TableRelations/01One-To-OneRelationship.sql
@@ -4,8 +4,20 @@ CREATE TABLE [Passports](
 )
 
 CREATE TABLE [Persons](
-	[PersonID] INT PRIMARY KEY,
+	[PersonID] INT PRIMARY KEY IDENTITY,
 	[FirstName] NVARCHAR(30) NOT NULL,
 	[Salary] DECIMAL(8, 2) NOT NULL,
 	[PassportID] INT FOREIGN KEY REFERENCES [Passports]([PassportID]) UNIQUE NOT NULL
 )
+
+INSERT INTO [Passports]([PassportNumber])
+	VALUES
+('N34FG21B'),
+('K65LO4R7'),
+('ZE657QP2')
+
+INSERT INTO [Persons]([FirstName], [Salary], [PassportID])
+	VALUES
+('Roberto', 43300.00, 102),
+('Tom', 56100.00, 103),
+('Yana', 60200.00, 101)
