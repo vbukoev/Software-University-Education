362741a4cfaf91ec7a8804786c22893c2c958143
Vasil Bukoev <vbukoev@gmail.com>
2023-01-18 21:58:38 +0200

    adding the geography db tasks

---
+++ CSharp-MS-SQL/Homeworks/05SubqueriesAndJoins/14CountriesWithOrWithoutRivers.sql
@@ -0,0 +1,8 @@
+SELECT TOP(5) [c].[CountryName], [r].[RiverName]
+	FROM [Countries] AS [c]
+LEFT JOIN CountriesRivers as [cr]
+	ON [cr].[CountryCode] = [c].[CountryCode]
+LEFT JOIN [Rivers] AS [r]
+	ON [r].[Id] = [cr].[RiverId]
+	WHERE [c].[ContinentCode] = 'AF'
+ORDER BY [c].[CountryName]
