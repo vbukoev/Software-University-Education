42f0cc7bd7cb93fb18ba9e14626d42ad621f67db
Vasil Bukoev <vbukoev@gmail.com>
2023-01-27 12:58:44 +0200

    adding the remaining tasks to the project

---
+++ CSharp-MS-SQL/Homeworks/08TriggersAndTransactions/09DeleteEmployees.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Deleted_Employees
+(
+	[EmployeeId] INT PRIMARY KEY IDENTITY,
+	[FirstName] VARCHAR (50),
+	[LastName] VARCHAR(50),
+	[MiddleName] VARCHAR(50),
+	[JobTitle] VARCHAR (50),
+	[DepartmentId] INT,
+	[Salary] DECIMAL (18,2)
+)
+
+GO
+
+CREATE TRIGGER tr_AddToDeleted
+ON [Employees] FOR DELETE
+AS
+INSERT INTO [Deleted_Employees]
+SELECT [FirstName], [LastName], [MiddleName],[JobTitle], [DepartmentId], [Salary]
+FROM deleted
