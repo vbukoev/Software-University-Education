8832c932bb78641caa3448e32d412ec3fbb1f753
Vasil Bukoev <vbukoev@gmail.com>
2023-01-30 14:06:25 +0200

    adding the tasks from the same exam

---
+++ CSharp-MS-SQL/Exams/RetakeExam-8April2021/09UserPerEmployee.sql
@@ -0,0 +1,8 @@
+SELECT 
+	e.FirstName + ' ' + e.LastName AS FullName, 
+	COUNT(r.UserId) AS UsersCount
+FROM Employees AS e
+LEFT JOIN Reports AS r
+	ON e.Id = r.EmployeeId
+GROUP BY e.FirstName, e.LastName
+ORDER BY COUNT(r.UserId) DESC, FullName
