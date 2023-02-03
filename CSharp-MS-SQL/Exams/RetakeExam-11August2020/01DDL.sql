7bc32c208ec30187c2c4f35c9634badf81c82eaa
Vasil Bukoev <vbukoev@gmail.com>
2023-02-03 18:21:50 +0200

    adding the tasks from the exam 11 August 2020

---
+++ CSharp-MS-SQL/Exams/RetakeExam-11August2020/01DDL.sql
@@ -0,0 +1,59 @@
+CREATE TABLE Countries
+(
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] VARCHAR(50) UNIQUE
+)
+
+CREATE TABLE Customers
+(
+	Id INT PRIMARY KEY IDENTITY,
+	FirstName NVARCHAR(25),
+	LastName NVARCHAR(25),
+	Gender CHAR(1) CHECK(Gender IN ('M', 'F')),
+	Age INT,
+	PhoneNumber CHAR(10),
+	CountryId INT FOREIGN KEY REFERENCES Countries(Id)
+)
+
+CREATE TABLE Products
+(
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(25) UNIQUE,
+	[Description] NVARCHAR(250),
+	Recipe NVARCHAR(MAX),
+	Price DECIMAL(18, 2) CHECK(Price>=0)
+)
+
+CREATE TABLE Feedbacks
+(
+	Id INT PRIMARY KEY IDENTITY,
+	[Description] NVARCHAR(50),
+	Rate DECIMAL(4,2) CHECK(Rate BETWEEN 0 AND 10),
+	ProductId INT FOREIGN KEY REFERENCES Products(Id),
+	CustomerId INT FOREIGN KEY REFERENCES Customers(Id)
+)
+
+CREATE TABLE Distributors
+(
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(25) UNIQUE,
+	AddressText NVARCHAR(30),
+	Summary NVARCHAR(200),
+	CountryId INT FOREIGN KEY REFERENCES Countries(Id)
+)
+
+CREATE TABLE Ingredients
+(
+	Id INT PRIMARY KEY IDENTITY,
+	[Name] NVARCHAR(30),
+	[Description] NVARCHAR(200),
+	OriginCountryId INT FOREIGN KEY REFERENCES Countries(Id),
+	DistributorId INT FOREIGN KEY REFERENCES Distributors(Id)
+)
+
+CREATE TABLE ProductsIngredients
+(
+	ProductId INT FOREIGN KEY REFERENCES Products(Id) NOT NULL,
+	IngredientId INT FOREIGN KEY REFERENCES Ingredients(Id) NOT NULL,
+	PRIMARY KEY(ProductId, IngredientId)
+)
