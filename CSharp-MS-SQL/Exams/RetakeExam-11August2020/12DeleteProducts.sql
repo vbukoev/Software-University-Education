015732acd03b86f7541b02cdf4b09f429f442688
Vasil Bukoev <vbukoev@gmail.com>
2023-02-04 14:31:05 +0200

    adding the rest of the tasks from the exam

---
+++ CSharp-MS-SQL/Exams/RetakeExam-11August2020/12DeleteProducts.sql
@@ -0,0 +1,11 @@
+CREATE TRIGGER tr_Delete
+ON
+Products
+INSTEAD OF DELETE
+AS
+BEGIN
+DECLARE @productId INT = (SELECT Id FROM deleted)
+DELETE FROM Feedbacks WHERE ProductId = @productId
+DELETE FROM ProductsIngredients WHERE ProductId = @productId
+DELETE FROM Products WHERE Id = @productId
+END
