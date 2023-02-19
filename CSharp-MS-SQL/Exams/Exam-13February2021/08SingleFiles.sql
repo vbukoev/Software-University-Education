69d5aada66eaee29259b0ebdbe224c19322cabcf
Vasil Bukoev <vbukoev@gmail.com>
2023-02-19 13:58:38 +0200

    made changes to these two tasks

--- CSharp-MS-SQL/Exams/Exam-13February2021/08SingleFiles.sql
+++ CSharp-MS-SQL/Exams/Exam-13February2021/08SingleFiles.sql
@@ -1,4 +1,3 @@
 SELECT Id, [Name], CAST(Size AS VARCHAR) + 'KB' AS Size
 	FROM Files
-	WHERE Id NOT IN
-	(SELECT ParentId FROM Files WHERE ParentId IS NOT NULL)
+	WHERE Id NOT IN (SELECT ParentId FROM Files WHERE ParentId IS NOT NULL)
