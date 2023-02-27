20ca0749dd4371e1127abcef7a4487f4278aac3b
Vasil Bukoev <vbukoev@gmail.com>
2023-02-27 20:46:36 +0200

    adding the tasks from the linq exercise

---
+++ CSharp-EntityFrameworkCore/05LINQ/03SongsAboveDuration/MusicHub/Data/Models/SongPerformer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicHub.Data.Models
+{
+    public class SongPerformer
+    {
+        [ForeignKey(nameof(Song))]
+        public int SongId { get; set; }
+        public virtual Song Song { get; set; }
+
+
+        [ForeignKey(nameof(Performer))]
+        public int PerformerId { get; set; }
+        public virtual Performer Performer { get; set; }
+    }
+}
