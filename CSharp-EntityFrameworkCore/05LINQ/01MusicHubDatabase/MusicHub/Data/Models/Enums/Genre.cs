27f24058b9879a93417f5b520c584af3b4835cc0
Vasil Bukoev <vbukoev@gmail.com>
2023-02-28 21:19:52 +0200

    adding the first task from the linq exercise

---
+++ CSharp-EntityFrameworkCore/05LINQ/01MusicHubDatabase/MusicHub/Data/Models/Enums/Genre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicHub.Data.Models.Enums
+{
+    public enum Genre
+    {
+        Blues, 
+        Rap, 
+        PopMusic, 
+        Rock, 
+        Jazz
+    }
+}
