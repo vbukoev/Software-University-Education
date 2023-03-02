5863237db5364e3f0b9ad38ebfb7b70eff5dfa2e
vbukoev <vbukoev@gmail.com>
2023-03-02 14:05:14 +0200

    adding the rest of the task from the exercise

---
+++ CSharp-EntityFrameworkCore/06AdvancedQuerying/16RemoveBooks/BookShop/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using BookShop.Models.Enums;
+
+namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+            //if the upper line is not commented it will print these unused lines :
+            //    "BookShop database created successfully."
+            //    "Sample data inserted successfully."
+
+            Console.WriteLine(RemoveBooks(db));
+        }
+
+        public static int RemoveBooks(BookShopContext context)
+        {
+            var books = context
+                .Books
+                .Where(x => x.Copies < 4200)
+                .ToArray();
+
+            context.RemoveRange(books);
+
+            context.SaveChanges();
+
+            return books.Count();
+        }
+    }
+}
