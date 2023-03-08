60e1b19b59ffc1e135a5073c08ac7ddabb588685
Vasil Bukoev <vbukoev@gmail.com>
2023-03-08 17:32:19 +0200

    update program.cs

--- CSharp-EntityFrameworkCore/07JSObjectNotation-JSON/01. Import Users_Skeleton (CarDealer)/CarDealer/Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CarDealer.Data
-{
-    public static class Configuration
-    {
-        public const string ConnectionString = @"Server=.;Database=CarDealer;Integrated Security=True;Encrypt=False";
-    }
-}
