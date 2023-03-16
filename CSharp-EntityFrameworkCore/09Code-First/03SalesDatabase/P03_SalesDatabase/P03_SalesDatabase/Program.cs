228419c27107cc75dd77c7db645b2e78a7e73e2f
Vasil Bukoev <vbukoev@gmail.com>
2023-03-16 17:10:56 +0200

    creating the sales database file

---
+++ CSharp-EntityFrameworkCore/09Code-First/03SalesDatabase/P03_SalesDatabase/P03_SalesDatabase/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace P03_SalesDatabase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
