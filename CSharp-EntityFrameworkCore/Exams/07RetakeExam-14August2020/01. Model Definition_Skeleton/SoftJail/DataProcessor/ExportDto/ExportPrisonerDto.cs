41828ec08c9d5a31db333d83b860eb8e363cf4cf
Vasil Bukoev <vbukoev@gmail.com>
2023-03-18 20:49:33 +0200

    adding the last task from the exam
    export the results

---
+++ CSharp-EntityFrameworkCore/Exams/07RetakeExam-14August2020/01. Model Definition_Skeleton/SoftJail/DataProcessor/ExportDto/ExportPrisonerDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SoftJail.DataProcessor.ExportDto
+{
+    [XmlType("Prisoner")]
+    public class ExportPrisonerDto
+    {
+        [XmlElement("Id")]
+        public int Id { get; set; }
+
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [XmlElement("IncarcerationDate")]
+        public string IncarcerationDate { get; set; }
+
+        [XmlArray("EncryptedMessages")]
+        public ExportMessageDto[] EncryptedMessages { get; set; }
+    }
+
+    [XmlType("Message")]
+    public class ExportMessageDto
+    {
+        [XmlElement("Description")]
+        public string Description { get;set; }
+    }
+}
