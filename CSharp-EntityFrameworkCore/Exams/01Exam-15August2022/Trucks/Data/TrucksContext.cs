adb7b6214ba2b38fbec8f1b44d363c36f2791424
vbukoev <vbukoev@gmail.com>
2023-03-25 11:24:48 +0200

    import model configuration of the exam 15Aug2022

---
+++ CSharp-EntityFrameworkCore/Exams/01Exam-15August2022/Trucks/Data/TrucksContext.cs
@@ -0,0 +1,42 @@
+﻿using Trucks.Data.Models;
+
+namespace Trucks.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class TrucksContext : DbContext
+    {
+        public TrucksContext()
+        { 
+        }
+
+        public TrucksContext(DbContextOptions options)
+            : base(options) 
+        { 
+        }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<ClientTruck> ClientsTrucks { get; set; }
+        public DbSet<Despatcher> Despatchers { get; set; }
+        public DbSet<Truck> Trucks { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ClientTruck>(e =>
+                {
+                    e.HasKey(ct => new { ct.TruckId, ct.ClientId });
+                }
+            );
+        }
+    }
+}
