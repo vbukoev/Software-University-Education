74ed9befada714171a2b6d75f0db3b5611775c43
vbukoev <vbukoev@gmail.com>
2023-04-06 18:23:08 +0300

    Create BoardgameSeller.cs

---
+++ CSharp-EntityFrameworkCore/Exams/00Exam-01April2023/Boardgames/Data/Models/BoardgameSeller.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Boardgames.Data.Models
+{
+    public class BoardgameSeller
+    {
+        [Required]
+        [ForeignKey(nameof(Boardgame))]
+        public int BoardgameId { get; set; }
+        public Boardgame Boardgame { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Seller))]
+        public int SellerId { get; set; }
+        public Seller Seller { get; set; }
+    }
+}
