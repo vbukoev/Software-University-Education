dfbc483784b8c63a5f89f78a21d874b4797067fd
vbukoev <vbukoev@gmail.com>
2023-04-12 10:22:35 +0300

    Create ImportSellerDto.cs

---
+++ CSharp-EntityFrameworkCore/Exams/00Exam-01April2023/Boardgames/DataProcessor/ImportDto/ImportSellerDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boardgames.DataProcessor.ImportDto
+{
+    public class ImportSellerDto
+    {
+        [Required]
+        [MinLength(5)]
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(30)]
+        public string Address { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        [Required]
+        [RegularExpression("^www\\.[A-Za-z\\d-]+\\.com$")]
+        public string Website { get; set; }
+
+        [Required]
+        public List<int> Boardgames { get; set; }
+    }
+}
