0126a16bd2879e9683de628a664d37306fc009ca
vbukoev <vbukoev@gmail.com>
2023-04-13 20:36:40 +0300

    Create Serializer.cs

---
+++ CSharp-EntityFrameworkCore/Exams/00Exam-01April2023/Boardgames/DataProcessor/Serializer.cs
@@ -0,0 +1,86 @@
+﻿using System.Text;
+using System.Xml.Serialization;
+using Boardgames.DataProcessor.ExportDto;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace Boardgames.DataProcessor
+{
+    using Boardgames.Data;
+
+    public class Serializer
+    {
+        public static string ExportCreatorsWithTheirBoardgames(BoardgamesContext context)
+        {
+            var creatorDtos = context.Creators
+                .Where(c => c.Boardgames.Any())
+                .Include(c => c.Boardgames)
+                .ToArray()
+                .Select(c => new ExportCreatorDto()
+                {
+                    BoardgamesCount = c.Boardgames.Count,
+                    CreatorName = $"{c.FirstName} {c.LastName}",
+                    Boardgames = c.Boardgames
+                        .Select(b => new ExportBoardgameDto()
+                        {
+                            BoardgameName = b.Name,
+                            BoardgameYearPublished = b.YearPublished,
+                        })
+                        .OrderBy(b => b.BoardgameName)
+                        .ToArray()
+                })
+                .OrderByDescending(c => c.Boardgames.Count())
+                .ThenBy(c => c.CreatorName)
+                .ToArray();
+
+            return Serialize<ExportCreatorDto[]>(creatorDtos, "Creators");
+        }
+
+        public static string ExportSellersWithMostBoardgames(BoardgamesContext context, int year, double rating)
+        {
+            var exportSellers = context.Sellers
+                .Where(s => s.BoardgamesSellers.Any(bs => bs.Boardgame.YearPublished >= year && bs.Boardgame.Rating <= rating))
+                .Include(s => s.BoardgamesSellers)
+                .ThenInclude(bs => bs.Boardgame)
+                .ToArray()
+                .Select(s => new
+                {
+                    s.Name,
+                    s.Website,
+                    Boardgames = s.BoardgamesSellers
+                        .Where(bs => bs.Boardgame.YearPublished >= year && bs.Boardgame.Rating <= rating)
+                        .Select(bs => new
+                        {
+                            bs.Boardgame.Name,
+                            bs.Boardgame.Rating,
+                            bs.Boardgame.Mechanics,
+                            Category = bs.Boardgame.CategoryType.ToString()
+                        })
+                        .OrderByDescending(b => b.Rating)
+                        .ThenBy(b => b.Name)
+                        .ToArray()
+                })
+                .OrderByDescending(s => s.Boardgames.Count())
+                .ThenBy(s => s.Name)
+                .Take(5)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(exportSellers, Formatting.Indented);
+        }
+
+        private static string Serialize<T>(T dataTransferObjects, string xmlRootAttributeName)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T), new XmlRootAttribute(xmlRootAttributeName));
+
+            StringBuilder sb = new StringBuilder();
+            using var write = new StringWriter(sb);
+
+            XmlSerializerNamespaces xmlNamespaces = new XmlSerializerNamespaces();
+            xmlNamespaces.Add(string.Empty, string.Empty);
+
+            serializer.Serialize(write, dataTransferObjects, xmlNamespaces);
+
+            return sb.ToString();
+        }
+    }
+}
