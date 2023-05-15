11447cdfaefbed7b4ef773dcda9db8a18dc57f7c
vbukoev <vbukoev@gmail.com>
2023-05-15 20:11:29 +0300

    adding the tasks from 3 to 11 from the lab js fundamentals

---
+++ JS-Front-End/Labs/01. JS Syntax Fundamentals - Lab/03. Excellent grade.js
@@ -0,0 +1,10 @@
+function solve(num){
+    if(num>=5.50){
+        console.log('Excellent');
+    }
+    else{
+        console.log('Not excellent');
+    }
+}
+
+//solve(4.35);
---
+++ JS-Front-End/Labs/01. JS Syntax Fundamentals - Lab/09. Numbers from 1 to 5.js
@@ -0,0 +1,6 @@
+function solve(){
+    for(let i=1; i<=5; i++){
+        console.log(i)
+    }
+}
+//solve()
