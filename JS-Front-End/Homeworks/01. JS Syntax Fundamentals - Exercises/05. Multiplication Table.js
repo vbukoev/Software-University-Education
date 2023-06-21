ddc95a9ad6e9fa74dcb2b14d82eacb4991a6dbf5
vbukoev <vbukoev@gmail.com>
2023-06-21 22:22:08 +0300

    Delete 05. Multiplication Table.js

--- JS-Front-End/Homeworks/01. JS Syntax Fundamentals - Exercises/05. Multiplication Table.js
+++
@@ -1,9 +0,0 @@
-function solve(number){
-    for(let i = 1; i <= 10; i++){
-        console.log(`${number} X ${i} = ${number * i}`);
-    } 
-}
-
-//solve(5);
-//console.log('-----------');
-//solve(2);
