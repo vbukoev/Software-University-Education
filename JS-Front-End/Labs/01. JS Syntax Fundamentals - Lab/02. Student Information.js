c55c45f6706c1e85d0be78eab3ffcbf84aa76b5e
vbukoev <vbukoev@gmail.com>
2023-06-24 16:54:10 +0300

    Delete 02. Student Information.js

--- JS-Front-End/Labs/01. JS Syntax Fundamentals - Lab/02. Student Information.js
+++
@@ -1,5 +0,0 @@
-function solve(name, age, grade){
-    console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`);
-}
-
-//solve('Steve', 16, 2.1426) -> this line is being used only for debugging!
