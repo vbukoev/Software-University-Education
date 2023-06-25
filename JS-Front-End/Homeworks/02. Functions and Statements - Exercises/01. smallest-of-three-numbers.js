83a94866693d94e081ead60ec3a603efb50c1797
vbukoev <vbukoev@gmail.com>
2023-06-25 10:50:03 +0300

    Create 01. smallest-of-three-numbers.js

---
+++ JS-Front-End/Homeworks/02. Functions and Statements - Exercises/01. smallest-of-three-numbers.js
@@ -0,0 +1,10 @@
+// function smallestOfThree(firstNum, secondNum, thirdNum){
+//     let min = Math.min(firstNum, secondNum, thirdNum);
+//     return min;
+// }
+
+const smallestOfThree = (firstNum, secondNum, thirdNum) => Math.min(firstNum, secondNum, thirdNum);
+
+// console.log(smallestOfThree(2,
+// 5,
+// 3))
