3b1283286e3550ab17985d8db74500a1189e25ba
vbukoev <vbukoev@gmail.com>
2023-07-01 13:19:00 +0300

    Create 09. make-a-dictionary.js

---
+++ JS-Front-End/Homeworks/03. Objects and Classes - Exercises/09. make-a-dictionary.js
@@ -0,0 +1,20 @@
+function dictionary(list){
+    let res = {};
+    for(item of list){
+        json = JSON.parse(item);
+        res[Object.keys(json)[0]] = Object.values(json)[0]
+    }
+    for(item of Object.entries(res).sort(([keyA], [keyB]) => keyA.localeCompare(keyB))){
+        console.log(`Term: ${item[0]} => Definition: ${item[1]}`)
+    }
+}
+
+//dictionary(
+//    [
+//        '{"Coffee":"A hot drink made from the roasted and ground seeds (coffee beans) of a tropical shrub."}',
+//        '{"Bus":"A large motor vehicle carrying passengers by road, typically one serving the public on a fixed route and for a fare."}',
+//        '{"Boiler":"A fuel-burning apparatus or container for heating water."}',
+//        '{"Tape":"A narrow strip of material, typically used to hold or fasten something."}',
+//        '{"Microphone":"An instrument for converting sound waves into electrical energy variations which may then be amplified, transmitted, or recorded."}'
+//    ]
+//)
