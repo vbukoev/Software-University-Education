71a2705bff2c104c2c1377ef6f4e3ed0d4af6345
vbukoev <vbukoev@gmail.com>
2023-07-05 15:05:40 +0300

    Update solution.js

--- JS-Front-End/More exercises/04. DOM and Events - More Exercises/15. JavaScript-Quiz/solution.js
+++ JS-Front-End/More exercises/04. DOM and Events - More Exercises/15. JavaScript-Quiz/solution.js
@@ -1,3 +1,28 @@
 function solve() {
-  //TODO...
+  let correctAnswers = ['onclick', 'JSON.stringify()', 'A programming API for HTML and XML documents'];
+  let rightAnswers = 0;
+  let i = 0;
+
+  Array.from(document.querySelectorAll('.answer-text')).map((x) => x.addEventListener('click', e=>{
+
+    if(correctAnswers.includes(e.target.textContent)){
+      rightAnswers++;
+    }
+
+    let currentSection = document.querySelectorAll('section')[i];
+    currentSection.style.display = 'none';
+
+    if(document.querySelectorAll('section')[i + 1] !== undefined){
+      let next = document.querySelectorAll('section')[i + 1];
+      next.style.display = 'block';
+      i++;
+    } else{
+      document.querySelector('#results').style.display = 'block';
+      if(rightAnswers === 3){
+        document.querySelector('#results h1').textContent = "You are recognized as top JavaScript fan!"       
+      } else{
+        document.querySelector('#results h1').textContent = `You have ${rightAnswers} right answers`
+      }
+    }
+  }))
 }
