bb2b1154b6b144fd54774100daa04fca73ce821e
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-01-24 16:14:23 +0200

    Update ForumAppDbContext.cs

--- CSharp-Web/CSharpWebFund-ForumApp-January2024/ForumApp/Data/ForumAppDbContext.cs
+++ CSharp-Web/CSharpWebFund-ForumApp-January2024/ForumApp/Data/ForumAppDbContext.cs
@@ -1,6 +1,22 @@
 ﻿namespace Forum.App.Data
 {
-    public class ForumAppDbContext
+    using Models;
+    using Microsoft.EntityFrameworkCore;
+    using Configuration;
+
+    public class ForumAppDbContext : DbContext
     {
+        public ForumAppDbContext(DbContextOptions<ForumAppDbContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+        public DbSet<Post> Posts { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new PostEntityConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
