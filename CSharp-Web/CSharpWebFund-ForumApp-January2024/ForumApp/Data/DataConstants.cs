99f2be19930441435f133ae184a74819b446deae
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-01-24 15:47:17 +0200

    Create DataConstants.cs

---
+++ CSharp-Web/CSharpWebFund-ForumApp-January2024/ForumApp/Data/DataConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace Forum.App.Data
+{
+    public class DataConstants
+    {
+        public class Post
+        {
+            public const int TitleMaxLength = 50;
+            public const int ContentMaxLength = 1500;
+        }
+    }
+}
