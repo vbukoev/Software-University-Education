9434071923310bde1b72fd3647cb98ac5fa80f71
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-01-24 16:14:51 +0200

    Delete ErrorViewModel.cs

--- CSharp-Web/CSharpWebFund-MVCIntro-Exercise-May2023/ChatApp/ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ChatApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
