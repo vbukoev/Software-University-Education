1652fc5fc0605d1fb58bfa5a0725681b2086a582
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-01-24 16:14:19 +0200

    Create PostEntityConfiguration.cs

---
+++ CSharp-Web/CSharpWebFund-ForumApp-January2024/ForumApp/Data/Configuration/PostEntityConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Forum.App.Data.Seeding;
+
+namespace Forum.App.Data.Configuration
+{
+    using Models;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    public class PostEntityConfiguration : IEntityTypeConfiguration<Post>
+    {
+        private readonly PostSeeder postSeeder;
+
+        public PostEntityConfiguration()
+        {
+            this.postSeeder = new PostSeeder();
+        }
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            builder.HasData(this.postSeeder.GeneratePosts().ToArray());
+        }
+    }
+}
