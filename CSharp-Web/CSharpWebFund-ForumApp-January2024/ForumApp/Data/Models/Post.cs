bd84d7d20328820504e9dd1da73a284c2fd2823e
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-01-24 15:47:21 +0200

    Create Post.cs

---
+++ CSharp-Web/CSharpWebFund-ForumApp-January2024/ForumApp/Data/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Forum.App.Data.DataConstants.Post;
+namespace Forum.App.Data.Models
+{
+    public class Post
+    {
+        public int Id { get; init; }
+        [Required]
+        [MaxLength(TitleMaxLength)]
+        public string Title { get; set; } = null!;
+        [Required]
+        [MaxLength(ContentMaxLength)]
+        public string Content { get; set; } = null!;
+    }
+}
