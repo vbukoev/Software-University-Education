74cda554ff15084fb6b4dfa527d5ac8eecc0b18d
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-01-26 17:48:07 +0200

    Create PostSeeder.cs

---
+++ CSharp-Web/CSharpWebFund-ForumApp-January2024/ForumApp.Data/Seeding/PostSeeder.cs
@@ -0,0 +1,37 @@
+﻿using ForumApp.Data.Models;
+
+namespace ForumApp.Data.Seeding
+
+{
+
+    class PostSeeder
+    {
+        internal Post[] GeneratePosts()
+        {
+            ICollection<Post> posts = new HashSet<Post>();
+            Post currPost;
+            currPost = new Post()
+            {
+                Title = "My First Post",
+                Content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ullamcorper, risus quis fermentum lacinia, dui nisi posuere nisl, eget dictum.",
+            };
+            posts.Add(currPost);
+
+            currPost = new Post()
+            {
+                Title = "My Second Post",
+                Content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu lacinia turpis. Quisque eu dapibus sem. Mauris pharetra ultrices leo.",
+            };
+            posts.Add(currPost);
+            
+            currPost = new Post()
+            {
+                Title = "My Third Post",
+                Content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacinia nibh a risus hendrerit tincidunt. Donec porta nulla arcu, at.",
+            };
+            posts.Add(currPost);
+
+            return posts.ToArray();
+        }
+    }
+}
