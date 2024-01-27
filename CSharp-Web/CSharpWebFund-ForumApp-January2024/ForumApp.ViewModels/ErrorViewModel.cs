2c2d778990a26cf65d03d86046dc3381dffb7d88
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-01-27 10:19:37 +0200

    Delete ErrorViewModel.cs

--- CSharp-Web/CSharpWebFund-ForumApp-January2024/ForumApp.ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ForumApp.ViewModels
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
