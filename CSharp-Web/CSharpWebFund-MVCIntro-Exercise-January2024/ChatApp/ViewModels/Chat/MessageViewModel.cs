5d2663717899d5f20eaa33a65cb4cd0e3a6fbb80
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-01-28 10:01:41 +0200

    Create MessageViewModel.cs

---
+++ CSharp-Web/CSharpWebFund-MVCIntro-Exercise-January2024/ChatApp/ViewModels/Chat/MessageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatApp.Models.Chat
+{
+    public class MessageViewModel
+    {
+        [Required]
+        public string Sender { get; set; } = null!;
+
+        [Required]
+        [MaxLength(255)]
+        public string MessageText { get; set; } = null!;
+    }
+}
