1d20bc79d8a2d6d5f8d01df7e808466dbc3b6498
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-01-28 10:01:30 +0200

    Create ChatController.cs

---
+++ CSharp-Web/CSharpWebFund-MVCIntro-Exercise-January2024/ChatApp/Controllers/ChatController.cs
@@ -0,0 +1,45 @@
+﻿using ChatApp.Models.Chat;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Controllers
+{
+    public class ChatController : Controller
+    {
+        private static readonly List<KeyValuePair<string, string>> messages =
+            new List<KeyValuePair<string, string>>();
+
+        
+        public IActionResult Show()
+        {
+            if(messages.Count < 1)
+            {
+                return View(new ChatViewModel());
+            }
+
+            var chatViewModel = new ChatViewModel()
+            {
+                AllMessages = messages.Select(m => new MessageViewModel()
+                {
+                    Sender = m.Key,
+                    MessageText = m.Value,
+                }).ToList()
+                
+            };
+            return View(chatViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Send(ChatViewModel chatViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.RedirectToAction("Show");
+            }
+            KeyValuePair<string, string> currentMessage = new KeyValuePair<string, string>(chatViewModel.CurrentMessage.Sender, chatViewModel.CurrentMessage.MessageText);
+            messages
+                .Add(currentMessage);
+
+            return RedirectToAction("Show");
+        }
+    }
+}
