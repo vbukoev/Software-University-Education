7f28f6b057aa06b1b3c2e2de684d2584142ff951
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-01-29 11:14:22 +0200

    Create ChatViewModel.cs

---
+++ CSharp-Web/CSharpWebFund-MVCIntro-Exercise-January2024/ChatApp/ViewModels/Chat/ChatViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ChatApp.Models.Chat
+{
+    public class ChatViewModel
+    {
+        public ChatViewModel() 
+        {
+            this.AllMessages = new HashSet<MessageViewModel>();
+        }
+        public MessageViewModel CurrentMessage { get; set; } = null!;
+
+        public ICollection<MessageViewModel> AllMessages { get; set; } 
+    }
+}
