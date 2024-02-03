1b60a9225ee033e7634f7a4032a77cad87667938
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-03 11:17:25 +0200

    Create ProductController.cs

---
+++ CSharp-Web/CSharpWebFund-MVCIntro-Exercise-January2024/MVCIntroDemo/Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using MVCIntroDemo.ViewModels.Product;
+using Newtonsoft.Json;
+using System.Text;
+using System.Text.Json;
+using System.Xml;
+using static MVCIntroDemo.Seeding.ProductData;
+namespace MVCIntroDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult All()
+        {
+            return View(products);
+        }
+        public IActionResult ById(string id)
+        {
+            ProductViewModel? product = products
+                .FirstOrDefault(p => p.Id.ToString().Equals(id));
+
+            if (product == null)
+            {
+                return RedirectToAction("All");
+            }
+
+            return View(product);
+        }
+
+        public IActionResult AllAsJson()
+        {
+            return Json(products, new JsonSerializerOptions()
+            {
+                WriteIndented = true,
+            });
+        }
+
+        public IActionResult DownloadProductsInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var product in products)
+            {
+                sb
+                    .AppendLine($"Product with Id: {product.Id}")
+                    .AppendLine($"## Product Name: {product.Name}")
+                    .AppendLine($"## Price: {product.Price:f2}")
+                    .AppendLine("-----------------------------");
+            }
+            Response.Headers.Add(HeaderNames.ContentDisposition, "attachment;filename=products.txt");
+            return File(Encoding.UTF8.GetBytes(sb.ToString()), "text/plain");
+        }
+    }
+}
