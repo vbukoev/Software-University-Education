9627d51b205a1589a70a163615dc80704f98f6f9
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-03 15:55:07 +0200

    Create IdentityUserBook.cs

---
+++ CSharp-Web/CSharpWebFund-ExamPrep-May2023/Library/Data/Models/IdentityUserBook.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Data.Models
+{
+    [Comment("User Books")]
+    public class IdentityUserBook
+    {
+        [Comment("Book Collector")]
+        public string CollectorId { get; set; } = null!;
+        [Comment("Collector")]
+        [ForeignKey(nameof(CollectorId))]
+        public IdentityUser Collector { get; set; } = null!;
+        [Comment("Book Id")]
+        public int BookId { get; set; }
+
+        [Comment("Book")]
+        [ForeignKey(nameof(BookId))]
+        public Book Book { get; set; } = null!;
+    }
+}
