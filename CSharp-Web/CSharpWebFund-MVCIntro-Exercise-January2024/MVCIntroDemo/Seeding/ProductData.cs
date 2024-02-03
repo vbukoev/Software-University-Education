8d9cca6711ebae69264a1932a2fac1d2609e4866
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-03 11:17:40 +0200

    Create ProductData.cs

---
+++ CSharp-Web/CSharpWebFund-MVCIntro-Exercise-January2024/MVCIntroDemo/Seeding/ProductData.cs
@@ -0,0 +1,29 @@
+﻿using MVCIntroDemo.ViewModels.Product;
+
+namespace MVCIntroDemo.Seeding
+{
+    public static class ProductData
+    {
+        public static IEnumerable<ProductViewModel> products = new List<ProductViewModel>()
+        {
+            new ProductViewModel()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Cheese",
+                Price = 7.00m
+            },
+            new ProductViewModel()
+            {
+            Id = Guid.NewGuid(),
+                Name = "Ham",
+                Price = 5.50m
+            },
+            new ProductViewModel()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Bread",
+                Price = 1.50m
+            }
+        };
+    }
+}
