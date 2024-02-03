ce090aa3361b334dfb1662d900d0679cf0c39504
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-03 15:55:02 +0200

    Create AddBookViewModel.cs

---
+++ CSharp-Web/CSharpWebFund-ExamPrep-May2023/Library/Models/AddBookViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+    public class AddBookViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 10)]
+        public string Title { get; set; } = null!;
+        [Required]
+        [StringLength(50, MinimumLength = 5)]
+        public string Author { get; set; } = null!;
+        [Required(AllowEmptyStrings = false)]
+        public string Url { get; set; } = null!;
+        [Required]
+        public string Rating { get; set; } = null!;
+        [Required]
+        [StringLength(5000, MinimumLength = 5)]
+        public string Description { get; set; } = null!;
+        [Range(1, int.MaxValue)]
+        public int CategoryId { get; set; }
+
+        public IEnumerable<CategoryViewModel> Categories { get; set; } = new List<CategoryViewModel>();
+    }
+}
