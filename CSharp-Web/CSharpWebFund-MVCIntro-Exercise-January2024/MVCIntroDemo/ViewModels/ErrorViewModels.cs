1f3465fc2a366e1ddb77df8e1c77468aa1afa9db
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-03 11:17:43 +0200

    Create ErrorViewModels.cs

---
+++ CSharp-Web/CSharpWebFund-MVCIntro-Exercise-January2024/MVCIntroDemo/ViewModels/ErrorViewModels.cs
@@ -0,0 +1,8 @@
+﻿namespace MVCIntroDemo.ViewModels
+{
+    public class ErrorViewModels
+    {
+        public string? RequestId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
