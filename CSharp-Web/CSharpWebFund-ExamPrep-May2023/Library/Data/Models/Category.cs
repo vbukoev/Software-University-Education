4b2db226084c4ca34d0fe895c4a3f45252a24d9f
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-05 18:36:14 +0200

    Create Category.cs

---
+++ CSharp-Web/CSharpWebFund-ExamPrep-May2023/Library/Data/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Data.Models
+{
+    [Comment("Categories for the Library")]
+    public class Category
+    {
+        [Comment("Category's unique identifier")]
+        [Key]
+        public int Id { get; set; }
+
+        [Comment("Name of the category")]
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Comment("Books in the category")]
+        public List<Book> Books { get; set; } = new List<Book>();
+    }
+}
+
+//Has Id – a unique integer, Primary Key
+//    • Has Name – a string with min length 5 and max length 50 (required)
+//    • Has Books – a collection of type Books
