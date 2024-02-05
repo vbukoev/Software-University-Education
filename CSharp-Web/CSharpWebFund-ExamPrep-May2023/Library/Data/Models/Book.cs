981724960041eeab2567d14f22e848a787615f4c
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-05 18:36:32 +0200

    Update Book.cs

--- CSharp-Web/CSharpWebFund-ExamPrep-May2023/Library/Data/Models/Book.cs
+++ CSharp-Web/CSharpWebFund-ExamPrep-May2023/Library/Data/Models/Book.cs
@@ -28,7 +28,7 @@ namespace Library.Data.Models
         public string Description { get; set; }
 
         [Comment("URL of the book's cover image")]
-        [Required]
+        [Required(AllowEmptyStrings = false)]
         public string ImageUrl { get; set; } = null!;
 
         [Comment("Rating of the book")]
@@ -49,13 +49,4 @@ namespace Library.Data.Models
     }
 }
 
-//      Has Id – a unique integer, Primary Key
-//    • Has Title – a string with min length 10 and max length 50 (required)
-//    • Has Author – a string with min length 5 and max length 50 (required)
-//    • Has Description – a string with min length 5 and max length 5000 (required)
-//    • Has ImageUrl – a string (required)
-//    • Has Rating – a decimal with min value 0.00 and max value 10.00 (required)
-//    • Has CategoryId – an integer, foreign key (required)
-//    • Has Category – a Category (required)
-//    • Has UsersBooks – a collection of type IdentityUserBook
 
