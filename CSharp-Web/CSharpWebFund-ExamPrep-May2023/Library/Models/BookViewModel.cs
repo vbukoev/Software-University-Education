821cc6190cb5b59997d7d032b0b45dfef0a4c4b5
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-05 18:36:38 +0200

    Create BookViewModel.cs

---
+++ CSharp-Web/CSharpWebFund-ExamPrep-May2023/Library/Models/BookViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+    public class BookViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 10)]
+        public string Title { get; set; } = null!;
+        [Required]
+        [StringLength(50, MinimumLength = 5)]
+        public string Author { get; set; } = null!;
+        [Required]
+        [MinLength(5)]
+        public string ImageUrl { get; set; } = null!;
+        [Required]
+        public decimal Rating { get; set; }
+        [Required]
+        [StringLength(5000, MinimumLength = 5)]
+        public string Description { get; set; } = null!;
+        [Range(1, int.MaxValue)]
+        public int CategoryId { get; set; } 
+    }
+}
