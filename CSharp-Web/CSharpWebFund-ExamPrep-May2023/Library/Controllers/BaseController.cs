d871ceea94d9bc9f3eb02c8b9cb1fd16981bdeb7
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-06 20:54:40 +0200

    Create BaseController.cs

---
+++ CSharp-Web/CSharpWebFund-ExamPrep-May2023/Library/Controllers/BaseController.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Controllers
+{
+    [Authorize]
+    public class BaseController : Controller
+    {
+        protected string? GetUserId()
+        {
+            string id = String.Empty;
+
+            if (User != null)
+            {
+                id = User?.FindFirstValue(ClaimTypes.NameIdentifier);
+            }
+            return id;
+        }
+    }
+}
