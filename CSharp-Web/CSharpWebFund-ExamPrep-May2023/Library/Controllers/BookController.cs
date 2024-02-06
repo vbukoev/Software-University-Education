c8a7e1f77d7eb22ff66a16a7d674102f46a95aa4
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-06 20:54:42 +0200

    Create BookController.cs

---
+++ CSharp-Web/CSharpWebFund-ExamPrep-May2023/Library/Controllers/BookController.cs
@@ -0,0 +1,89 @@
+﻿using Library.Contracts;
+using Library.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Controllers
+{
+    public class BookController : BaseController
+    {
+        private readonly IBookService bookService;
+
+        public BookController(IBookService bookService)
+        {
+            this.bookService = bookService;
+        }
+        public async Task<IActionResult> All()
+        {
+            var model  = await bookService.GetAllBooksAsync();
+            return View(model);
+        }
+
+        public async Task<IActionResult> Mine()
+        {
+            var model = await bookService.GetMyBooksAsync(GetUserId());
+            return View(model);
+        }
+
+        public async Task<IActionResult> AddToCollection(int id)
+        {
+            var book = await bookService.GetBookByIdAsync(id);
+
+            if (book == null)
+            {
+                RedirectToAction(nameof(All));
+            }
+
+            var userId = GetUserId();
+
+            await bookService.AddBookToCollectionAsync(userId, book);
+
+            return RedirectToAction(nameof(All));
+        }
+
+        public async Task<IActionResult> RemoveFromCollection(int id)
+        {
+            var book = await bookService.GetBookByIdAsync(id);
+
+            if (book == null)
+            {
+               return RedirectToAction(nameof(Mine));
+            }
+
+            var userId = GetUserId();
+
+            await bookService.RemoveBookFromCollectionAsync(userId, book);
+
+            return RedirectToAction(nameof(Mine));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            AddBookViewModel model =  await bookService.GetNewAddBookModelAsync();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddBookViewModel model)
+        {
+            decimal rating;
+            if (!decimal.TryParse(model.Rating, out rating) || rating < 0 || rating > 10)
+            {
+                ModelState.AddModelError(nameof(model.Rating), "Rating must be a number between 0.00 and 10.00.");
+
+                return View(model);
+            }
+            if (ModelState.IsValid == false)
+            {
+                return View(model);
+            }
+            
+            await bookService.AddBookAsync(model);
+
+            return RedirectToAction(nameof(All));
+        }
+    }
+
+    
+}
