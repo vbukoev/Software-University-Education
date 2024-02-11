762bf965c5ed2b8d742c6724a59ec96e811d1f75
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-11 12:04:21 +0200

    Create Type.cs

---
+++ CSharp-Web/CSharpWebFund-ExamPrep-Feb2024/Homies/Data/Type.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Homies.Data
+{
+    public class Type
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(DataConstants.TypeNameMaxLength)]
+        public string Name { get; set; } = string.Empty;
+
+        public IEnumerable<Event> Events { get; set; } = new List<Event>();
+    }
+}
