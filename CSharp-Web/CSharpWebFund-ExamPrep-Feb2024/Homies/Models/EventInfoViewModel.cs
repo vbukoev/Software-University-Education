56b3730ab285721999d845f43ac1f505ea158576
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-12 11:11:30 +0200

    Create EventInfoViewModel.cs

---
+++ CSharp-Web/CSharpWebFund-ExamPrep-Feb2024/Homies/Models/EventInfoViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Homies.Data;
+
+namespace Homies.Models
+{
+    public class EventInfoViewModel
+    {
+        public EventInfoViewModel(int id, string name, DateTime start, string type, string organiser)
+        {
+            Id = id;
+            Name = name;
+            Start = start.ToString(DataConstants.DateTimeFormat);
+            Type = type;
+            Organiser = organiser;
+        }
+        public int Id { get; set; }
+
+        public string Name { get; set; } 
+
+        public string Start { get; set; } 
+
+        public string Type { get; set; } 
+
+        public string Organiser { get; set; } 
+    }
+}
