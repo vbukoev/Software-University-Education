9451212eebe8e7733b4d7c916fbd76712d87aeec
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-14 09:52:18 +0200

    Update Login.cshtml.cs

--- CSharp-Web/CSharpWebFund-ExamPrep-Feb2024/Homies/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ CSharp-Web/CSharpWebFund-ExamPrep-Feb2024/Homies/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -71,8 +71,13 @@ namespace Homies.Areas.Identity.Pages.Account
             public string Password { get; set; }
         }
 
-        public async Task OnGetAsync(string returnUrl = null)
+        public async Task<IActionResult> OnGetAsync(string returnUrl = null)
         {
+            if (User != null && User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("All", "Event");
+            }
+
             if (!string.IsNullOrEmpty(ErrorMessage))
             {
                 ModelState.AddModelError(string.Empty, ErrorMessage);
@@ -84,6 +89,8 @@ namespace Homies.Areas.Identity.Pages.Account
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
 
             ReturnUrl = returnUrl;
+
+            return Page();
         }
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
