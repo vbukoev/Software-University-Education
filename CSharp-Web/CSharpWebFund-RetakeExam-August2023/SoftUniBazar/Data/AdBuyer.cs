a55e2a59eedc43830e2791ea7d5c0bea4a4ac0bc
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-14 09:56:28 +0200

    Create AdBuyer.cs

---
+++ CSharp-Web/CSharpWebFund-RetakeExam-August2023/SoftUniBazar/Data/AdBuyer.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Build.Framework;
+
+namespace SoftUniBazar.Data
+{
+    public class AdBuyer
+    {
+        [Required] 
+        public string BuyerId { get; set; } = string.Empty;
+
+        [ForeignKey(nameof(BuyerId))] 
+        public IdentityUser Buyer { get; set; } = null!;
+        [Required]
+        public int AdId { get; set; }
+
+        [ForeignKey(nameof(AdId))] 
+        public Ad Ad { get; set; } = null!;
+    }
+}
