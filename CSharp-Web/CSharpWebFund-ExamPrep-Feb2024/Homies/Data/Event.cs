ce4e4c89db0f4d1b4b696aeb916c1e7e2bc43e72
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-14 09:52:34 +0200

    Create Event.cs

---
+++ CSharp-Web/CSharpWebFund-ExamPrep-Feb2024/Homies/Data/Event.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+using Newtonsoft.Json;
+using static Homies.Data.DataConstants;
+
+namespace Homies.Data
+{
+    public class Event
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(EventNameMaxLength)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(EventDescriptionMaxLength)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public string OrganiserId { get; set; } = string.Empty;
+
+        [ForeignKey(nameof(OrganiserId))]
+        public IdentityUser Organiser { get; set; } = null!;
+
+        [Required]
+        public DateTime CreatedOn { get; set; }
+
+        [Required]
+        public DateTime Start { get; set; }
+
+        [Required]
+        public DateTime End { get; set; }
+
+        [Required] 
+        public int TypeId { get; set; }
+
+        [ForeignKey(nameof(TypeId))]
+        public Type Type { get; set; } = null!;
+
+        public IList<EventParticipant> EventsParticipants { get; set; } = new List<EventParticipant>();
+    }
+}
+
