5d98200dcbf16a52c0fffbb3b3fb3a008a7ecee0
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-14 09:56:30 +0200

    Create Ad.cs

---
+++ CSharp-Web/CSharpWebFund-RetakeExam-August2023/SoftUniBazar/Data/Ad.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+using static SoftUniBazar.Data.DataConstants;
+
+namespace SoftUniBazar.Data
+{
+    public class Ad
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(AdNameMaxLength)]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(AdDescriptionMaxLength)]
+        public string Description { get; set; } = string.Empty;
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public string OwnerId { get; set; } = string.Empty;
+        [ForeignKey(nameof(OwnerId))]
+        public IdentityUser Owner { get; set; } = null!;
+        [Required] 
+        public string ImageUrl { get; set; } = string.Empty;
+        [Required]
+        public DateTime CreatedOn { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; }
+        public IEnumerable<AdBuyer> AdBuyers { get; set; } = new List<AdBuyer>();
+    }
+}
