53f751dd81f1df64ecd03ad71e2ec7bbe869c25f
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-14 09:52:37 +0200

    Create HomiesDbContext.cs

---
+++ CSharp-Web/CSharpWebFund-ExamPrep-Feb2024/Homies/Data/HomiesDbContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Homies.Data
+{
+    public class HomiesDbContext : IdentityDbContext
+    {
+        public HomiesDbContext(DbContextOptions<HomiesDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EventParticipant>()
+                .HasOne(e => e.Event)
+                .WithMany(e => e.EventsParticipants)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<EventParticipant>()
+                .HasKey(ep => new { ep.HelperId, ep.EventId });
+
+            modelBuilder
+                .Entity<Type>()
+                .HasData(new Type()
+                {
+                    Id = 1,
+                    Name = "Animals"
+                },
+                new Type()
+                {
+                    Id = 2,
+                    Name = "Fun"
+                },
+                new Type()
+                {
+                    Id = 3,
+                    Name = "Discussion"
+                },
+                new Type()
+                {
+                    Id = 4,
+                    Name = "Work"
+                });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Event> Events { get; set; } = null!;
+        public DbSet<Type> Types { get; set; } = null!;
+        public DbSet<EventParticipant> EventsParticipants { get; set; } = null!;
+    }
+}
