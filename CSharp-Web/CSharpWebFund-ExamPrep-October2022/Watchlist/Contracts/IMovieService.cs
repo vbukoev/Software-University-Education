a1bc4438534c0b822abac23ae40edd1ba19df5bc
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-14 09:54:56 +0200

    Create IMovieService.cs

---
+++ CSharp-Web/CSharpWebFund-ExamPrep-October2022/Watchlist/Contracts/IMovieService.cs
@@ -0,0 +1,21 @@
+﻿using Watchlist.Data.Models;
+using Watchlist.Models;
+
+namespace Watchlist.Contracts
+{
+    public interface IMovieService
+    {
+        Task<IEnumerable<MovieInfoViewModel>> GetAllAsync();
+
+        Task<IEnumerable<Genre>> GetGenresAsync();
+
+        Task AddMovie(MovieFormViewModel model);
+
+        Task AddToCollection(int movieId, string userId);
+
+        Task RemoveFromCollection(int movieId, string userId);
+
+        Task<IEnumerable<MovieInfoViewModel>> GetWatchedAsync(string userId);
+
+    }
+}
