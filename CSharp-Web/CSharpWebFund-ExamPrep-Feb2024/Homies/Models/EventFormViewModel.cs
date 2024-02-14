f94a17aea510d6dc70c8cff352744841fb92cf19
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-14 09:52:48 +0200

    Create EventFormViewModel.cs

---
+++ CSharp-Web/CSharpWebFund-ExamPrep-Feb2024/Homies/Models/EventFormViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Homies.Data.DataConstants;
+namespace Homies.Models
+{
+    public class EventFormViewModel
+    {
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(EventNameMaxLength, MinimumLength = EventNameMinLength, ErrorMessage = StringLengthErrorMessage)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(EventDescriptionMaxLength, MinimumLength = EventDescriptionMinLength, ErrorMessage = StringLengthErrorMessage)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequireErrorMessage)]
+        public string Start { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequireErrorMessage)]
+        public string End { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequireErrorMessage)]
+        public int TypeId { get; set; }
+
+        public IEnumerable<TypeViewModel> Types { get; set; } = new List<TypeViewModel>();
+    }
+}
