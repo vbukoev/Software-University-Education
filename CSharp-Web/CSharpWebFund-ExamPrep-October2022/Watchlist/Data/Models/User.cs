3f3417d209c4a960bfc9bfa01f60c4b5e54c2628
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-14 09:54:38 +0200

    Create User.cs

---
+++ CSharp-Web/CSharpWebFund-ExamPrep-October2022/Watchlist/Data/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Watchlist.Data.Models
+{
+    public class User : IdentityUser
+    {
+        [Comment("User And Movies Collection (List)")]
+        public List<UserMovie> UsersMovies { get; set; } = new List<UserMovie>();
+    }
+}
