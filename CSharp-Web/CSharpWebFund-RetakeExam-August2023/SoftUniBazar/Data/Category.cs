8442c66be5af409dc9bd0ba32f7b86eadcace4a2
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-14 09:56:33 +0200

    Create Category.cs

---
+++ CSharp-Web/CSharpWebFund-RetakeExam-August2023/SoftUniBazar/Data/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoftUniBazar.Data
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(DataConstants.CategoryNameMaxLength)]
+        public string Name { get; set; } = string.Empty;
+        public IList<Ad> Ads { get; set; } = new List<Ad>();
+    }
+}
