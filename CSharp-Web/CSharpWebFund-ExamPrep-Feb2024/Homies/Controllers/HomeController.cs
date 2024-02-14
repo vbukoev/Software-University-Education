f82425b04103cfd287dbb36f1fa0cd1d11abc32a
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-14 09:52:32 +0200

    Create HomeController.cs

---
+++ CSharp-Web/CSharpWebFund-ExamPrep-Feb2024/Homies/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Homies.Data;
+using Homies.Models;
+using Humanizer;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using static System.Net.Mime.MediaTypeNames;
+namespace Homies.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("All", "Event");
+            }
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
