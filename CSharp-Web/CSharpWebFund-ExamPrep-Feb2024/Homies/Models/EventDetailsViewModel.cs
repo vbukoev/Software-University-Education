7ad6f574b7e46916a363d3ce2265c909914381d4
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-14 09:54:20 +0200

    Create EventDetailsViewModel.cs

---
+++ CSharp-Web/CSharpWebFund-ExamPrep-Feb2024/Homies/Models/EventDetailsViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Homies.Models
+{
+    public class EventDetailsViewModel
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string Type { get; set; }
+
+        public string Start { get; set; } = string.Empty;
+
+        public string End { get; set; } = string.Empty;
+
+        public string Organiser { get; set; } = string.Empty;
+
+        public string CreatedOn { get; set; } = string.Empty;
+    }
+}
