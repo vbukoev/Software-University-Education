f8f4012113f440021db8c2b22a4a1e1f720c7747
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-15 11:45:49 +0200

    Create Program.cs

---
+++ CSharp-Web/CSharpWebFund-MVCIntro-Exercise-January2024/printEvenNumbers/Program.cs
@@ -0,0 +1,43 @@
+﻿float res = 0;
+while (true)
+{
+    Console.WriteLine("Enter a:");
+    float a = float.Parse(Console.ReadLine());
+    Console.WriteLine("Enter b:");
+    float b = float.Parse(Console.ReadLine());
+    Console.WriteLine("Enter operator:");
+    string opperator = Console.ReadLine();
+    if (opperator == "+")
+    {
+        res = a + b;
+        Console.WriteLine($"Result: {res:f2}");
+    }
+    else if (opperator == "-")
+    {
+        res = a - b;
+        Console.WriteLine($"Result: {res:f2}");
+    }
+    else if (opperator == "*")
+    {
+        res = a * b;
+        Console.WriteLine($"Result: {res:f2}");
+    }
+    else if (opperator == "/")
+    {
+        if (b == 0)
+        {
+            Console.WriteLine("Infinity");
+        }
+        else
+        {
+            res = a / b;
+            Console.WriteLine($"Result: {res:f2}");
+        }
+        
+    }
+    else
+    {
+        Console.WriteLine("Invalid operator!");
+    }
+    Console.WriteLine("---------------------");
+}
