bdd28e9e76b542f7cbb9f0514e455227076c24cb
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-15 11:46:10 +0200

    Create TextSplitViewModel.cs

---
+++ CSharp-Web/CSharpWebFund-MVCIntro-Exercise-January2024/TextSplitter/ViewModels/TextSplitViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TextSplitter.ViewModels
+{
+    public class TextSplitViewModel 
+    {
+        public string? TextToSplit { get; set; }
+
+        public string? SplitText { get; set;} 
+    }
+}
