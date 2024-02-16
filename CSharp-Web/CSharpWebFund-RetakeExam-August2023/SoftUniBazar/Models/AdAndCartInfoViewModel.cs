d3d36e4703ab7a8bd9eb8567be33696303916cd6
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-16 11:33:18 +0200

    Create AdAndCartInfoViewModel.cs

---
+++ CSharp-Web/CSharpWebFund-RetakeExam-August2023/SoftUniBazar/Models/AdAndCartInfoViewModel.cs
@@ -0,0 +1,27 @@
+﻿using SoftUniBazar.Data;
+
+namespace SoftUniBazar.Models
+{
+    public class AdAndCartInfoViewModel
+    {
+        public AdAndCartInfoViewModel(int id, string name, string description, string category, decimal price, string imageUrl, string owner, DateTime createdOn)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Category = category;
+            Price = price;
+            ImageUrl = imageUrl;
+            Owner = owner;
+            CreatedOn = createdOn.ToString(DataConstants.DateTimeFormat);
+        }
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string Category { get; set; } = null!;
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; } = null!;
+        public string Owner { get; set; } = null!;
+        public string CreatedOn { get; set; } 
+    }
+}
