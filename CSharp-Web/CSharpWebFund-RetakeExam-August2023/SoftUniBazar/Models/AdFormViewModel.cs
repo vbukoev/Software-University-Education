24346f8c612271f919236301eda3eb5806624e01
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-16 11:33:16 +0200

    Create AdFormViewModel.cs

---
+++ CSharp-Web/CSharpWebFund-RetakeExam-August2023/SoftUniBazar/Models/AdFormViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static SoftUniBazar.Data.DataConstants;
+namespace SoftUniBazar.Models
+{
+    public class AdFormViewModel
+    {
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(AdNameMaxLength, MinimumLength = AdNameMinLength, ErrorMessage = StringLengthErrorMessage)]
+        public string Name { get; set; } = null!;
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(AdDescriptionMaxLength, MinimumLength = AdDescriptionMinLength, ErrorMessage = StringLengthErrorMessage)]
+        public string Description { get; set; } = null!;
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+        [Required]
+        public int CategoryId { get; set; }
+        public List<CategoryViewModel> Categories { get; set; } = new List<CategoryViewModel>();
+    }
+}
