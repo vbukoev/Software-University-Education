1739eb56aa9d87bd2b3cd5b7bc6da1137fc0ced1
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-17 10:46:37 +0200

    Create EventParticipant.cs

---
+++ CSharp-Web/CSharpWebFund-ExamPrep-Feb2024/Homies/Data/EventParticipant.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace Homies.Data
+{
+    public class EventParticipant
+    {
+        [Microsoft.Build.Framework.Required]
+        public string HelperId { get; set; } = string.Empty;
+
+        [ForeignKey(nameof(HelperId))]
+        public IdentityUser Helper { get; set; } = null!;
+
+        [Microsoft.Build.Framework.Required]
+        public int EventId { get; set; }
+
+        [ForeignKey(nameof(EventId))]
+        public Event Event { get; set; } = null!;
+    }
+}
