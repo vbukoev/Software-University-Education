04bdfb5c2214f6e6d7b4c1c1b32ac85c51db57f6
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-17 11:30:15 +0200

    Update EventInfoViewModel.cs

--- CSharp-Web/CSharpWebFund-ExamPrep-Feb2024/Homies/Models/EventInfoViewModel.cs
+++ CSharp-Web/CSharpWebFund-ExamPrep-Feb2024/Homies/Models/EventInfoViewModel.cs
@@ -12,14 +12,29 @@ namespace Homies.Models
             Type = type;
             Organiser = organiser;
         }
+        /// <summary>
+        /// Event Identifier
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// Event Name
+        /// </summary>
         public string Name { get; set; } 
 
+        /// <summary>
+        /// Event Start Date and Time
+        /// </summary>
         public string Start { get; set; } 
 
+        /// <summary>
+        /// Event Type
+        /// </summary>
         public string Type { get; set; } 
 
+        /// <summary>
+        /// Event Organiser
+        /// </summary>
         public string Organiser { get; set; } 
     }
 }
