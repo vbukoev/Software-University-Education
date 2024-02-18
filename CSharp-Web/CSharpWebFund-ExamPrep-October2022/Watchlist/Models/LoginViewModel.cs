47bfe1cdebd562c2ceaa8ccb201f3aafa29f05f1
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-18 10:55:22 +0200

    Create LoginViewModel.cs

---
+++ CSharp-Web/CSharpWebFund-ExamPrep-October2022/Watchlist/Models/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Watchlist.Models
+{
+    public class LoginViewModel
+    {
+        /// <summary>
+        /// Login Username
+        /// </summary>
+        [Required]
+        public string UserName{ get; set; } = null!;
+        /// <summary>
+        /// Login Password
+        /// </summary>
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+    }
+}
