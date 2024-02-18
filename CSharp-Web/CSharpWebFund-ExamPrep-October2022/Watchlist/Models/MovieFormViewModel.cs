54f5248da5bc7cd14cf28c4e64182c0a94838d79
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-18 10:56:34 +0200

    Create MovieFormViewModel.cs

---
+++ CSharp-Web/CSharpWebFund-ExamPrep-October2022/Watchlist/Models/MovieFormViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Primitives;
+using Watchlist.Data.Models;
+using static Watchlist.Data.Models.DataConstants.Genre;
+using static Watchlist.Data.Models.DataConstants.ErrorMessages;
+using static Watchlist.Data.Models.DataConstants.Movie;
+
+namespace Watchlist.Models
+{
+    public class MovieFormViewModel
+    {
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(MovieTitleMaxLength, MinimumLength = MovieTitleMinLength, ErrorMessage = StringLengthErrorMessage)]
+        public string Title{ get; set; }
+
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(MovieDirectorMaxLength, MinimumLength = MovieDirectorMinLength, ErrorMessage = StringLengthErrorMessage)]
+        public string Director { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        [Precision(18,2)]
+        [Range(typeof(decimal), "0", "10")]
+        public decimal Rating { get; set; }
+
+        public int GenreId { get; set; }
+
+        public IEnumerable<Genre> Genres { get; set; } = new List<Genre>();
+    }
+}
