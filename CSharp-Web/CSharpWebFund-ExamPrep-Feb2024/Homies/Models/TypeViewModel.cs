cf019593aeab17b5426e534659b56c232274d6f0
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-18 10:54:59 +0200

    Update TypeViewModel.cs

--- CSharp-Web/CSharpWebFund-ExamPrep-Feb2024/Homies/Models/TypeViewModel.cs
+++ CSharp-Web/CSharpWebFund-ExamPrep-Feb2024/Homies/Models/TypeViewModel.cs
@@ -2,8 +2,14 @@
 {
     public class TypeViewModel
     {
+        /// <summary>
+        /// Type Identifier
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// Type Name
+        /// </summary>
         public string Name { get; set; } = string.Empty;
     }
 }
