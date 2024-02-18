1fad76de7c37d8c322769215af76cb290ec56d12
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-18 10:55:19 +0200

    Create UserMovie.cs

---
+++ CSharp-Web/CSharpWebFund-ExamPrep-October2022/Watchlist/Data/Models/UserMovie.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Watchlist.Data.Models
+{
+    [Comment("Table UserMovies")]
+    public class UserMovie
+    {
+        [Comment("User Identifier")]
+        [Required]
+        public string UserId { get; set; }
+
+        [Comment("User Entity")]
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; }
+
+        [Comment("Movie Identifier")]
+        [Required]
+        public int MovieId { get; set; }
+
+        [Comment("Movie Entity")]
+        [ForeignKey(nameof(MovieId))]
+        public Movie Movie { get; set; }
+    }
+}
