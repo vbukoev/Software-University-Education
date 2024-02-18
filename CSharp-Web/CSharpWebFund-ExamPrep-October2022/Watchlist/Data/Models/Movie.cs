821b8a1c09e5a7a5e080ef4556ede6b5c8158001
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-18 10:55:15 +0200

    Create Movie.cs

---
+++ CSharp-Web/CSharpWebFund-ExamPrep-October2022/Watchlist/Data/Models/Movie.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using static Watchlist.Data.Models.DataConstants.Movie;
+
+namespace Watchlist.Data.Models
+{
+    [Comment("Table Movie")]
+    public class Movie
+    {
+        [Comment("Movie Identifier")]
+        [Key]
+        public int Id { get; set; }
+
+        [Comment("Movie Title")]
+        [Required]
+        [StringLength(MovieTitleMaxLength)]
+        public string Title { get; set; }
+
+        [Comment("Movie Director")]
+        [Required]
+        [StringLength(MovieDirectorMaxLength)]
+        public string Director { get; set; }
+
+        [Comment("Movie Image")]
+        [Required]
+        public string ImageUrl { get; set; }
+
+        [Comment("Movie Rating")]
+        [Required]
+        public decimal Rating { get; set; }
+
+        [Comment("Movie Genre Identifier")]
+        [Required]
+        public int? GenreId { get; set; }
+
+        [Comment("Genre Entity")]
+        [ForeignKey(nameof(GenreId))]
+        public Genre? Genre { get; set; }
+
+        [Comment("UserMovies Collection")]
+        public List<UserMovie> UsersMovies { get; set; } = new List<UserMovie>();
+    }
+}
