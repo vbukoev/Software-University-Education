cf38d037a3c35b355386eee56108093961657c20
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-19 10:18:50 +0200

    Create MovieInfoViewModel.cs

---
+++ CSharp-Web/CSharpWebFund-ExamPrep-October2022/Watchlist/Models/MovieInfoViewModel.cs
@@ -0,0 +1,41 @@
+﻿namespace Watchlist.Models
+{
+    public class MovieInfoViewModel
+    {
+        public MovieInfoViewModel(int id, string title, string director, string imageUrl, decimal rating, string genre)
+        {
+            Id = id;
+            Title = title;
+            Director = director;
+            ImageUrl = imageUrl;
+            Rating = rating;
+            Genre = genre;
+        }
+        /// <summary>
+        /// Movie Identifier
+        /// </summary>
+        public int Id { get; set; } 
+
+        /// <summary>
+        /// Movie Title
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// Movie Director
+        /// </summary>
+        public string Director { get; set; }
+        /// <summary>
+        /// Movie Image path
+        /// </summary>
+        public string ImageUrl { get; set; }
+        /// <summary>
+        ///Movie Rating
+        /// </summary>
+        public decimal Rating { get; set; }
+        /// <summary>
+        /// Movie Genre
+        /// </summary>
+        public string Genre{ get; set; }
+    }
+}
+
