ac02a7dee9d5df025339bc570746b42a0ea177a0
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-19 10:18:33 +0200

    Update HomeController.cs

--- CSharp-Web/CSharpWebFund-ExamPrep-October2022/Watchlist/Controllers/HomeController.cs
+++ CSharp-Web/CSharpWebFund-ExamPrep-October2022/Watchlist/Controllers/HomeController.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Watchlist.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
+        [AllowAnonymous]
         public IActionResult Index()
         {
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("All", "Movies");
+            }
             return View();
         }
     }
