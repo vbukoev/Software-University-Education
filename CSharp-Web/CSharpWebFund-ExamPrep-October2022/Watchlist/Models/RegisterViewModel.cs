9f0cab45e588330108da3d297c09dab12f5a2f63
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-19 10:18:58 +0200

    Create RegisterViewModel.cs

---
+++ CSharp-Web/CSharpWebFund-ExamPrep-October2022/Watchlist/Models/RegisterViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Watchlist.Data.Models.DataConstants.ErrorMessages;
+using static Watchlist.Data.Models.DataConstants.User;
+
+namespace Watchlist.Models
+{
+    public class RegisterViewModel
+    {
+        /// <summary>
+        /// Register Username
+        /// </summary>
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(UserNameMaxLength, MinimumLength = UserNameMinLength, ErrorMessage = StringLengthErrorMessage)]
+        public string UserName { get; set; } = null!;
+        /// <summary>
+        /// Register Email
+        /// </summary>
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [EmailAddress]
+        [StringLength(EmailMaxLength, MinimumLength = EmailMinLength, ErrorMessage = StringLengthErrorMessage)]
+        public string Email { get; set; } = null!; 
+        /// <summary>
+        /// Register Password
+        /// </summary>
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(PasswordMaxLength, MinimumLength = PasswordMinLength, ErrorMessage = StringLengthErrorMessage)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+        /// <summary>
+        /// Register Password Confirmation
+        /// </summary>
+        [Compare(nameof(Password))]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; } = null!;
+
+
+    }
+}
