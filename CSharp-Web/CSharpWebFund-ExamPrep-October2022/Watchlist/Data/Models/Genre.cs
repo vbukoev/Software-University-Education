9bbdc8aa395cf93b336d717e112e9473a306e2a5
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-19 10:18:53 +0200

    Create Genre.cs

---
+++ CSharp-Web/CSharpWebFund-ExamPrep-October2022/Watchlist/Data/Models/Genre.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using static Watchlist.Data.Models.DataConstants.Genre;
+
+namespace Watchlist.Data.Models
+{
+    [Comment("Table Genre")]
+    public class Genre
+    {
+        [Comment("Genre Identifier")]
+        [Key]
+        public int Id { get; set; }
+
+        [Comment("Genre Name")]
+        [Required]
+        [StringLength(GenreNameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        [Comment("Genre Collection")]
+        public ICollection<Movie> Movies { get; set; } = new List<Movie>();
+    }
+}
