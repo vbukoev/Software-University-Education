af0733c5791202ea4a4aa7a5e1d784956c782e8a
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-02-19 10:18:19 +0200

    Update Type.cs

--- CSharp-Web/CSharpWebFund-ExamPrep-Feb2024/Homies/Data/Type.cs
+++ CSharp-Web/CSharpWebFund-ExamPrep-Feb2024/Homies/Data/Type.cs
@@ -1,16 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace Homies.Data
 {
+    [Comment("Type table")]
     public class Type
     {
         [Key]
+        [Comment("Type Identifier")]
         public int Id { get; set; }
 
         [Required]
         [MaxLength(DataConstants.TypeNameMaxLength)]
+        [Comment("Type Name")]
         public string Name { get; set; } = string.Empty;
-
+        [Comment("Collection of Events")]
         public IEnumerable<Event> Events { get; set; } = new List<Event>();
     }
 }
