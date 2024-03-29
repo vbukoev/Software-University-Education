848f49a7cd6e8d50ebdc4ed69aee00a72966f3d3
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-03-29 09:40:33 +0200

    Update EventParticipant.cs

--- CSharp-Web/CSharpWebFund-ExamPrep-Feb2024/Homies/Data/EventParticipant.cs
+++ CSharp-Web/CSharpWebFund-ExamPrep-Feb2024/Homies/Data/EventParticipant.cs
@@ -1,21 +1,27 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace Homies.Data
 {
+    [Comment("EventParticipant table")]
     public class EventParticipant
     {
-        [Microsoft.Build.Framework.Required]
+        [Required]
+        [Comment("EventParticipant Helper Identifier")]
         public string HelperId { get; set; } = string.Empty;
 
         [ForeignKey(nameof(HelperId))]
+        [Comment("EventParticipant Helper")]
         public IdentityUser Helper { get; set; } = null!;
 
-        [Microsoft.Build.Framework.Required]
+        [Required]
+        [Comment("EventParticipant Event Identifier")]
         public int EventId { get; set; }
 
         [ForeignKey(nameof(EventId))]
+        [Comment("EventParticipant Event")]
         public Event Event { get; set; } = null!;
     }
 }
