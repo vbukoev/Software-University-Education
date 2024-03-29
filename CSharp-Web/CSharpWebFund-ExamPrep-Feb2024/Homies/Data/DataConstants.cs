f6b883ab2d82996fcc145c5eb453e058404ead6e
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-03-29 09:40:24 +0200

    Create DataConstants.cs

---
+++ CSharp-Web/CSharpWebFund-ExamPrep-Feb2024/Homies/Data/DataConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace Homies.Data
+{
+    public static class DataConstants
+    {
+        public const int EventNameMinLength = 5;
+        public const int EventNameMaxLength = 20;
+        public const int EventDescriptionMinLength = 15;
+        public const int EventDescriptionMaxLength = 150;
+        public const string DateTimeFormat = "yyyy-MM-dd H:mm";
+
+        public const int TypeNameMinLength = 5;
+        public const int TypeNameMaxLength = 15;
+
+        public const string RequireErrorMessage = "The {0} field is required.";
+        public const string StringLengthErrorMessage = "The field {0} must be at least {2} and at max {1} characters long.";
+    }
+}
