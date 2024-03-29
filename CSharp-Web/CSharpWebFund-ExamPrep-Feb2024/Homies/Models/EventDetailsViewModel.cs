512e10dc81fb94f36fec2f24cca26725edb1d78e
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-03-29 09:40:43 +0200

    Update EventDetailsViewModel.cs

--- CSharp-Web/CSharpWebFund-ExamPrep-Feb2024/Homies/Models/EventDetailsViewModel.cs
+++ CSharp-Web/CSharpWebFund-ExamPrep-Feb2024/Homies/Models/EventDetailsViewModel.cs
@@ -2,19 +2,37 @@
 {
     public class EventDetailsViewModel
     {
+        /// <summary>
+        /// Event Identifier
+        /// </summary>
         public int Id { get; set; } 
+        /// <summary>
+        /// Event Name
+        /// </summary>
         public string Name { get; set; } = string.Empty;
-
+        /// <summary>
+        /// Event Description
+        /// </summary>
         public string Description { get; set; } = string.Empty;
-
+        /// <summary>
+        /// Event Type
+        /// </summary>
         public string Type { get; set; }
-
+        /// <summary>
+        /// Event Start Date and Time
+        /// </summary>
         public string Start { get; set; } = string.Empty;
-
+        /// <summary>
+        /// Event End Date and Time
+        /// </summary>
         public string End { get; set; } = string.Empty;
-
+        /// <summary>
+        /// Event Organiser
+        /// </summary>
         public string Organiser { get; set; } = string.Empty;
-
+        /// <summary>
+        /// Event Creation Date 
+        /// </summary>
         public string CreatedOn { get; set; } = string.Empty;
     }
 }
