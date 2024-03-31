e223e6894c483ceeb7a08305be2b561e615e1aea
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-03-31 11:53:13 +0300

    Update AllBookViewModel.cs

--- CSharp-Web/CSharpWebFund-ExamPrep-May2023/Library/Models/AllBookViewModel.cs
+++ CSharp-Web/CSharpWebFund-ExamPrep-May2023/Library/Models/AllBookViewModel.cs
@@ -4,12 +4,33 @@ namespace Library.Models
 {
     public class AllBookViewModel
     {
+        /// <summary>
+        /// Book Identifier
+        /// </summary>
         public int Id { get; set; }
+        /// <summary>
+        /// Book Title
+        /// </summary>
         public string Title { get; set; } = null!;
+        /// <summary>
+        /// Book Author
+        /// </summary>
         public string Author { get; set; } = null!;
+        /// <summary>
+        /// Book Image URL
+        /// </summary>
         public string ImageUrl { get; set; } = null!;
+        /// <summary>
+        /// Book Rating
+        /// </summary>
         public decimal Rating { get; set; }
+        /// <summary>
+        /// Book Description
+        /// </summary>
         public string? Description { get; set; } 
+        /// <summary>
+        /// Book Category
+        /// </summary>
         public string Category { get; set; } = null!;
     }
 }
