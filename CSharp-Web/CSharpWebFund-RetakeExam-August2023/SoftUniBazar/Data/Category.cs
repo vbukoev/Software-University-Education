da8d8d27e85511af73c39add6593868a8fd533a0
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-04-09 19:20:05 +0300

    Update Category.cs

--- CSharp-Web/CSharpWebFund-RetakeExam-August2023/SoftUniBazar/Data/Category.cs
+++ CSharp-Web/CSharpWebFund-RetakeExam-August2023/SoftUniBazar/Data/Category.cs
@@ -1,14 +1,20 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace SoftUniBazar.Data
 {
+    [Comment("Category table")]
     public class Category
     {
+        [Comment("Category Identifier")]
         [Key]
         public int Id { get; set; }
+
+        [Comment("Category Name")]
         [Required]
         [MaxLength(DataConstants.CategoryNameMaxLength)]
         public string Name { get; set; } = string.Empty;
+        [Comment("Collection of Ads")]
         public IList<Ad> Ads { get; set; } = new List<Ad>();
     }
 }
