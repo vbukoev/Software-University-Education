cd241182b3bdc91c9e59e1442967603254f0f837
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-04-09 19:20:11 +0300

    Update AdBuyer.cs

--- CSharp-Web/CSharpWebFund-RetakeExam-August2023/SoftUniBazar/Data/AdBuyer.cs
+++ CSharp-Web/CSharpWebFund-RetakeExam-August2023/SoftUniBazar/Data/AdBuyer.cs
@@ -1,18 +1,24 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Build.Framework;
+using Microsoft.EntityFrameworkCore;
 
 namespace SoftUniBazar.Data
 {
+    [Comment("AdBuyer table")]
     public class AdBuyer
     {
+        [Comment("AdBuyer Identifier")]
         [Required] 
         public string BuyerId { get; set; } = string.Empty;
 
+        [Comment("Ad Buyer")]
         [ForeignKey(nameof(BuyerId))] 
         public IdentityUser Buyer { get; set; } = null!;
+        [Comment("Ad Identifier")]
         [Required]
         public int AdId { get; set; }
+        [Comment("Ad")]
 
         [ForeignKey(nameof(AdId))] 
         public Ad Ad { get; set; } = null!;
