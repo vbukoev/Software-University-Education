790b2da66d5449f07c8613717cba55ac2238aad7
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-04-18 18:27:17 +0300

    Create SeminarHubDbContext.cs

---
+++ CSharp-Web/CSharpWebFund-RegularExam-Feb2024/SeminarHub/Data/SeminarHubDbContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SeminarHub.Data.Entities;
+
+namespace SeminarHub.Data
+{
+    public class SeminarHubDbContext : IdentityDbContext
+    {
+        public SeminarHubDbContext(DbContextOptions<SeminarHubDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<SeminarParticipant>()
+                .HasOne(sp => sp.Seminar)
+                .WithMany(s => s.SeminarsParticipants)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<SeminarParticipant>()
+                .HasKey(sp => new { sp.SeminarId, sp.ParticipantId });
+
+            builder
+               .Entity<Category>()
+               .HasData(new Category()
+               {
+                   Id = 1,
+                   Name = "Technology & Innovation"
+               },
+               new Category()
+               {
+                   Id = 2,
+                   Name = "Business & Entrepreneurship"
+               },
+               new Category()
+               {
+                   Id = 3,
+                   Name = "Science & Research"
+               },
+               new Category()
+               {
+                   Id = 4,
+                   Name = "Arts & Culture"
+               });
+
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<Seminar> Seminars { get; set; } = null!;
+        public DbSet<SeminarParticipant> SeminarsParticipants { get; set; } = null!;
+    }
+}
