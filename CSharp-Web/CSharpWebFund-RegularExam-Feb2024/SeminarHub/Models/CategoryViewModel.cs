2e69fb84f9adbc4e368a91be954d8265d851b4e0
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-04-19 09:04:51 +0300

    Create CategoryViewModel.cs

---
+++ CSharp-Web/CSharpWebFund-RegularExam-Feb2024/SeminarHub/Models/CategoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace SeminarHub.Models
+{
+    public class CategoryViewModel
+    {
+        /// <summary>
+        /// Category Identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Category Name
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+    }
+}
