8ee9a2951fc896390f91143683621cf5d9c8a39e
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-05-10 18:47:33 +0300

    Update AddBookViewModel.cs

--- CSharp-Web/CSharpWebFund-ExamPrep-May2023/Library/Models/AddBookViewModel.cs
+++ CSharp-Web/CSharpWebFund-ExamPrep-May2023/Library/Models/AddBookViewModel.cs
@@ -4,22 +4,46 @@ namespace Library.Models
 {
     public class AddBookViewModel
     {
+        /// <summary>
+        /// Book Identifier
+        /// </summary>
         public int Id { get; set; }
+        /// <summary>
+        /// Book Title
+        /// </summary>
         [Required]
         [StringLength(50, MinimumLength = 10)]
         public string Title { get; set; } = null!;
+        /// <summary>
+        /// Book Author
+        /// </summary>
         [Required]
         [StringLength(50, MinimumLength = 5)]
         public string Author { get; set; } = null!;
+        /// <summary>
+        /// Book Image URL
+        /// </summary>
         [Required(AllowEmptyStrings = false)]
         public string Url { get; set; } = null!;
+        /// <summary>
+        /// Book Rating
+        /// </summary>
         [Required]
         public string Rating { get; set; } = null!;
+        /// <summary>
+        /// Book Description
+        /// </summary>
         [Required]
         [StringLength(5000, MinimumLength = 5)]
         public string Description { get; set; } = null!;
+        /// <summary>
+        /// Category Identifier
+        /// </summary>
         [Range(1, int.MaxValue)]
         public int CategoryId { get; set; }
+        /// <summary>
+        /// Collection of Categories
+        /// </summary>
 
         public IEnumerable<CategoryViewModel> Categories { get; set; } = new List<CategoryViewModel>();
     }
