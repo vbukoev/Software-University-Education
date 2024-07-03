fe79a3e60b6a364d19f18adced10388afdd26200
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-07-03 18:13:11 +0300

    Create SeminarFormViewModel.cs

---
+++ CSharp-Web/CSharpWebFund-RegularExam-Feb2024/SeminarHub/Models/SeminarFormViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static SeminarHub.Data.DataConstants;
+namespace SeminarHub.Models
+{
+    public class SeminarFormViewModel
+    {
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(SeminarTopicMaxLength, MinimumLength = SeminarTopicMinLength, ErrorMessage = StringLengthErrorMessage)]
+        public string Topic { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(SeminarDetailsMaxLength, MinimumLength = SeminarDetailsMinLength, ErrorMessage = StringLengthErrorMessage)]
+        public string Details { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(SeminarLecturerMaxLength, MinimumLength = SeminarLecturerMinLength, ErrorMessage = StringLengthErrorMessage)]
+        public string Lecturer { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequireErrorMessage)]
+        public string DateAndTime { get; set; } = string.Empty;
+
+        [Range(SeminarDurationMinValue, SeminarDurationMaxValue)]
+        public int Duration { get; set; }
+
+        [Required(ErrorMessage = RequireErrorMessage)]
+        public int CategoryId { get; set; }
+
+        public IEnumerable<CategoryViewModel> Categories { get; set; } = new List<CategoryViewModel>();
+    }
+}
