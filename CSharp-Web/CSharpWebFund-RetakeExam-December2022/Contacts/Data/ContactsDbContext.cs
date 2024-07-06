d175f3a6e2cd3af98547243a5a358f437daa214b
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-07-06 18:52:14 +0300

    Create ContactsDbContext.cs

---
+++ CSharp-Web/CSharpWebFund-RetakeExam-December2022/Contacts/Data/ContactsDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Contacts.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contacts.Data
+{
+    public class ContactsDbContext : IdentityDbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<ApplicationUserContact> ApplicationUserContacts { get; set; }
+        public ContactsDbContext(DbContextOptions<ContactsDbContext> options)
+            : base(options)
+        {
+             this.Database.Migrate();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUserContact>()
+                .HasKey(uc => new { uc.ApplicationUserId, uc.ContactId });
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
