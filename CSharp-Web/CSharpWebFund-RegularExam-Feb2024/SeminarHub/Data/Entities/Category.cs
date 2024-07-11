da27017675d28542c96ebeb9d122dc8c12514703
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-07-11 13:06:26 +0300

    Create Category.cs

---
+++ CSharp-Web/CSharpWebFund-RegularExam-Feb2024/SeminarHub/Data/Entities/Category.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using static SeminarHub.Data.DataConstants;
+
+namespace SeminarHub.Data.Entities
+{
+    [Comment("Category Table")]
+    public class Category
+    {
+        [Comment("Category Identifier")]
+        [Key]
+        public int Id { get; set; }
+
+        [Comment("Category Name")]
+        [Required]
+        [MaxLength(CategoryNameMaxLength)]
+        public string Name { get; set; } = string.Empty;
+
+        [Comment("List of Seminars")]
+        public IEnumerable<Seminar> Seminars { get; set; } = new List<Seminar>();
+    }
+}
