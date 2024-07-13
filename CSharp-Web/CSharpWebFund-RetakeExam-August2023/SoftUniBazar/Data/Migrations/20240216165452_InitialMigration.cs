05caf6912a6c1c7903bd5d1dc6816f55ab7f7d37
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-07-13 09:57:57 +0300

    Create 20240216165452_InitialMigration.cs

---
+++ CSharp-Web/CSharpWebFund-RetakeExam-August2023/SoftUniBazar/Data/Migrations/20240216165452_InitialMigration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SoftUniBazar.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AdId1",
+                table: "AdBuyers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AdBuyers_AdId1",
+                table: "AdBuyers",
+                column: "AdId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AdBuyers_Ads_AdId1",
+                table: "AdBuyers",
+                column: "AdId1",
+                principalTable: "Ads",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AdBuyers_Ads_AdId1",
+                table: "AdBuyers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AdBuyers_AdId1",
+                table: "AdBuyers");
+
+            migrationBuilder.DropColumn(
+                name: "AdId1",
+                table: "AdBuyers");
+        }
+    }
+}
