0bfe4846fdeef47d9749e99d551f74d528708b02
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-07-22 13:22:46 +0300

    Create SeminarDetailsViewModel.cs

---
+++ CSharp-Web/CSharpWebFund-RegularExam-Feb2024/SeminarHub/Models/SeminarDetailsViewModel.cs
@@ -0,0 +1,45 @@
+﻿namespace SeminarHub.Models
+{
+    public class SeminarDetailsViewModel
+    {
+        /// <summary>
+        /// Seminar Identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Seminar Topic
+        /// </summary>
+        public string Topic { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Seminar Lecturer
+        /// </summary>
+        public string Lecturer { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Seminar Date and Time
+        /// </summary>
+        public string DateAndTime { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Seminar Organizer
+        /// </summary>
+        public string Organizer { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Seminar Category
+        /// </summary>
+        public string Category { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Seminar Details
+        /// </summary>
+        public string Details { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Seminar Duration
+        /// </summary>
+        public int Duration { get; set; }
+    }
+}
