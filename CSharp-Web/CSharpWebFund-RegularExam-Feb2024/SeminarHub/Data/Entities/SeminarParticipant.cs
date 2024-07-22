03dbfe2bd77f44e5ce46c4df0084962d027c3c60
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-07-22 13:22:53 +0300

    Create SeminarParticipant.cs

---
+++ CSharp-Web/CSharpWebFund-RegularExam-Feb2024/SeminarHub/Data/Entities/SeminarParticipant.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Build.Framework;
+using Microsoft.EntityFrameworkCore;
+
+namespace SeminarHub.Data.Entities
+{
+    [Comment("Seminar Participant's Table")]
+    public class SeminarParticipant
+    {
+        [Comment("Seminar Identifier")]
+        [Required]
+        public int SeminarId { get; set; }
+
+        [Comment("Seminar")]
+        [ForeignKey(nameof(SeminarId))]
+        public Seminar Seminar { get; set; }
+
+        [Comment("Participant Identifier")]
+        [Required]
+        public string ParticipantId { get; set; }
+
+        [Comment("Participant")]
+        [ForeignKey(nameof(ParticipantId))]
+        public IdentityUser Participant { get; set; }
+    }
+}
+
