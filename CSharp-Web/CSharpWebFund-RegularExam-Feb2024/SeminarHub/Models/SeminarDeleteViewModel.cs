f309a4e93cf34b98b8e008fae607c246cebabd31
Vasil Bukoev <105813259+vbukoev@users.noreply.github.com>
2024-07-22 13:22:48 +0300

    Create SeminarDeleteViewModel.cs

---
+++ CSharp-Web/CSharpWebFund-RegularExam-Feb2024/SeminarHub/Models/SeminarDeleteViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace SeminarHub.Models
+{
+    public class SeminarDeleteViewModel
+    {
+        /// <summary>
+        /// Seminar Identifier
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Seminar Topic
+        /// </summary>
+        public string Topic { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Seminar Date and Time
+        /// </summary>
+        public DateTime DateAndTime { get; set; }
+    }
+}
